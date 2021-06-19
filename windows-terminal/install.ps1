212f54022d02a483819eb4ae05fc5d77255ad045
Chris Pawling <chris-468@outlook.com>
2021-06-19 18:04:12 -0500

    Fix path to windows terminal preview settings

--- windows-terminal/install.ps1
+++ windows-terminal/install.ps1
@@ -1,6 +1,6 @@
 Param([switch]$force=$false, [switch]$quiet=$false)
 
-$settingsFiles = '~\AppData\Local\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\settings.json','C:\Users\Chris\AppData\Local\Packages\Microsoft.WindowsTerminalPreview_8wekyb3d8bbwe\LocalState\settings.json'
+$settingsFiles = '~\AppData\Local\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\settings.json','~\AppData\Local\Packages\Microsoft.WindowsTerminalPreview_8wekyb3d8bbwe\LocalState\settings.json'
 
 if ( ! ($force -or $quiet) ) {
     foreach ($settingsFile in $settingsFiles) {
