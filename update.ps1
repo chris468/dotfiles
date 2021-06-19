6c573273fdf5672649fdd2cf66ede118cea62172
Chris Pawling <chris-468@outlook.com>
2021-06-19 18:04:12 -0500

    update.ps1 allows force

--- update.ps1
+++ update.ps1
@@ -1,3 +1,5 @@
+Param([switch]$force=$false)
+
 function has_updates {
     git -C $PSScriptRoot fetch origin
     git -C $PSScriptRoot status --porcelain -b | select-string -quiet "\[behind"
@@ -5,8 +7,9 @@ function has_updates {
 
 if ( has_updates ) {
     Write-Host "dotfiles are out of date. Updating..."
+    $mode = $force ? "-force" : "-quiet"
     git -C $PSScriptRoot pull
-    & $PSScriptRoot\configure-all.ps1 -quiet
+    & $PSScriptRoot\configure-all.ps1 $mode
 }
 else {
     Write-Host "dotfiles are up to date."
