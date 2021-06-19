0e1c9c086bc1506960d343744ce5a7626b026edb
Chris Pawling <chris-468@outlook.com>
2021-06-19 18:04:12 -0500

    Update script for windows

--- update.ps1
+++ update.ps1
@@ -1,7 +1,13 @@
-
-$git="git -C $PSScriptRoot"
-
 function has_updates {
-    & $git fetch origin
-    & $git status --porcelain -b | 
+    git -C $PSScriptRoot fetch origin
+    git -C $PSScriptRoot status --porcelain -b | select-string -quiet "\[behind"
+}
+
+if ( has_updates ) {
+    Write-Host "dotfiles are out of date. Updating..."
+    git -C $PSScriptRoot pull
+    & $PSScriptRoot\configure-all.ps1 -quiet
+}
+else {
+    Write-Host "dotfiles are up to date."
 }
