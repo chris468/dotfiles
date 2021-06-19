f5c43baaaa0f9176befb4689fd839d5f63450eb0
Chris Pawling <chris-468@outlook.com>
2021-06-19 18:04:11 -0500

    Add dotnet tools to path

--- fish/fish/conf.d/add-local-bin-path.fish
+++ fish/fish/conf.d/add-local-bin-path.fish
@@ -1,4 +1,8 @@
-if test -d ~/.local/bin ;
-    set fish_user_paths ~/.local/bin $fish_user_paths
+set paths ~/.local/bin ~/.dotnet/tools
+
+for path in $paths
+    if test -d $path ;
+        set fish_user_paths $path $fish_user_paths
+    end
 end
 
