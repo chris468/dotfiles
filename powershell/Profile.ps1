24ec8e29c23d6ad235e1d94d2d98045f6baced6d
Chris Pawling <chris-468@outlook.com>
2021-06-19 18:04:12 -0500

    Add posh-git prompt

--- powershell/Profile.ps1
+++ powershell/Profile.ps1
@@ -1,1 +1,10 @@
-Write-Host 'hello'
+Import-Module posh-git
+
+# For performance, disable untracked files in the prompt.
+# Just disabling doesn't hide the +0 - need to set the
+# option to hide 0 statuses as well.
+$GitPromptSettings.UntrackedFilesMode="no"
+$GitPromptSettings.ShowStatusWhenZero=$false
+
+
+
--- powershell/configure.ps1
+++ powershell/configure.ps1
@@ -1,31 +1,39 @@
 Param([switch]$force=$false, [switch]$quiet=$false)
 
-$files = @{
-    $PROFILE.CurrentUserAllHosts = "$PSScriptRoot/Profile.ps1";
-}
+function link-configuration {
+    $files = @{
+        $PROFILE.CurrentUserAllHosts = "$PSScriptRoot/Profile.ps1";
+    }
 
-$fileExists = $false
-if ( ! $force -and ! $quiet ) {
-    foreach ( $destination in $files.keys ) {
-        if ( Test-Path $destination ) {
-            Write-Error "$destination already exists"
-            $fileExists = $true
+    $fileExists = $false
+    if ( ! $force -and ! $quiet ) {
+        foreach ( $destination in $files.keys ) {
+            if ( Test-Path $destination ) {
+                Write-Error "$destination already exists"
+                $fileExists = $true
+            }
+        }
+
+        if ( $fileExists ) {
+            throw "use -Force or -Quiet"
         }
     }
 
-    if ( $fileExists ) {
-        throw "use -Force or -Quiet"
+    if ( $force ) {
+        Remove-Item -Force -Recurse -Path @($files.keys)
+    }
+
+    foreach ( $file in $files.keys ) {
+        if ( !(Test-Path $file) ) {
+            New-Item -ItemType SymbolicLink -Path $file -Target $files[$file]
+        }
     }
 }
 
-if ( $force ) {
-    Remove-Item -Force -Recurse -Path @($files.keys)
+function install-packages {
+    PowerShellGet\Install-Module posh-git -Scope CurrentUser -Force
 }
 
-foreach ( $file in $files.keys ) {
-    if ( !(Test-Path $file) ) {
-        New-Item -ItemType SymbolicLink -Path $file -Target $files[$file]
-    }
-}
-
+install-packages
+link-configuration
 
