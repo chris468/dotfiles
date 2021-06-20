efc8c4563e296b0cd07cb27ff1ccceb531537df4
Chris Pawling <chris-468@outlook.com>
2021-06-20 13:05:22 -0500

    Add update.ps1 option for status file

--- update.ps1
+++ update.ps1
@@ -1,4 +1,4 @@
-Param([switch]$force=$false)
+Param([switch]$force=$false, $statusFile)
 
 function has_updates {
     git -C $PSScriptRoot fetch origin
@@ -10,7 +10,13 @@ if ( has_updates ) {
     $mode = $force ? "-force" : "-quiet"
     git -C $PSScriptRoot pull
     & $PSScriptRoot\configure-all.ps1 $mode
+    if ( $statusFile ) {
+        Set-Content $statusFile "dotfiles updated on $(Get-Date -Format f)"
+    }
 }
 else {
     Write-Host "dotfiles are up to date."
+    if ( $statusFile ) {
+        Set-Content $statusFile "dotfiles were up to date when checked on $(Get-Date -Format f)"
+    }
 }
