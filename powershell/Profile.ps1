6fa83f5372f38187303010a736a1012571965778
Chris Pawling <chris-468@outlook.com>
2021-06-20 15:26:08 -0500

    Add powershell function to auto-update daily

--- powershell/Profile.ps1
+++ powershell/Profile.ps1
@@ -17,3 +17,40 @@ function Update-Dotfiles {
     }
 }
 
+function Auto-Update-Dotfiles {
+    $status_dir = "$HOME\.cache\dotfiles"
+    $logfile = "$status_dir\auto-update.log"
+    $statusfile = "$status_dir\auto-update-status.txt"
+    $interval_days = 1
+
+    function log_exists {
+        Test-Path $logfile
+    }
+
+    function log_updated_since_interval {
+        $age = $(Get-Date) - $(Get-Item $logfile).LastWriteTime
+        $age -lt $(New-TimeSpan -Days $interval_days)
+    }
+
+    function should_update {
+        -not $(log_exists) -or -not $(log_updated_since_interval)
+    }
+
+    function update {
+        New-Item -Type Directory $status_dir > $null
+        Set-Content $logfile "Starting update at $(Get-Date)"
+        $dotfiles = Split-Path (Split-Path (Get-Item $PSCommandPath).Target)
+        Start-Job -ArgumentList $statusfile,$logfile,$dotfiles -ScriptBlock {
+            Param($statusfile, $logfile, $dotfiles)
+            & $dotfiles\update.ps1 -statusFile "$statusfile" 2>&1 >> "$logfile"
+        }
+    }
+
+    if ( should_update ) {
+        Write-Output "Updating dotfiles in background. See $logfile"
+        update
+    }
+    else {
+        Get-Content $statusfile
+    }
+}
