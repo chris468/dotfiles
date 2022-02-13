dd1415a37dd205527d6af81f7387e0798999222f
Chris Pawling <chris-468@outlook.com>
2022-02-13 17:03:16 -0600

    fish: fix error when there is no k8s context

--- dotfiles/fish/fish/functions/k8s_context_prompt.fish
+++ dotfiles/fish/fish/functions/k8s_context_prompt.fish
@@ -1,8 +1,6 @@
 function k8s_context_prompt
     type -q kubectl ; or return
-    set -l context (kubectl config current-context)
+    set -l context (kubectl config current-context 2>/dev/null) ; or return
     test -n $context ; or return
     echo "[k8s:$context]"
 end
-
-
