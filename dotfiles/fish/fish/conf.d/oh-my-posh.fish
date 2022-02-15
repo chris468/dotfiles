d375d16afce5d96e554d81d3f77c6158bd235297
Chris Pawling <chris.pawling@ni.com>
2022-02-14 20:35:02 -0600

    Avoid symlink

--- dotfiles/fish/fish/conf.d/oh-my-posh.fish
+++ dotfiles/fish/fish/conf.d/oh-my-posh.fish
@@ -1,5 +1,6 @@
 type -q oh-my-posh ; or exit
 
-set posh_theme (realpath $__fish_config_dir)/../../oh-my-posh/current-theme.omp.json
+set -l oh_my_posh_path (realpath $__fish_config_dir)/../../oh-my-posh/
+set posh_theme $oh_my_posh_path/(cat $oh_my_posh_path/current-theme)
 
 oh-my-posh --init --shell fish --config $posh_theme | source
