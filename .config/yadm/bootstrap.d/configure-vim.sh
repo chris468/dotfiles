a83b15d54eb020e274191ec5d1be48f6392af1a2
Chris Pawling <chris-468@outlook.com>
2022-03-06 16:36:24 -0600

    Configure vim plugins

---
+++ .config/yadm/bootstrap.d/configure-vim.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# git symbolic link to folder doesn't work on windows, so create it here.
+if grep -qi "win" <<< "$OS" && test ! -e ~/vimfiles ; then
+    pwsh -Command "& { New-Item -Type SymbolicLink -Target ~/.vim -Path ~/vimfiles }"
+fi
+
+vim -c PlugInstall -c qa
+
