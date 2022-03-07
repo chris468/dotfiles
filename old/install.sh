30e42a9ec1989c29d3eb7138059f25fe8417c403
Chris Pawling <chris-468@outlook.com>
2022-03-06 19:13:42 -0600

    Remove the rest of the old manager

--- old/install.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-script_dir="$(cd "$(dirname "$(readlink -e "$0")")" && pwd)"
-
-(cd $script_dir && python3.9 -m manager install $@)
-
--- old/tests/fake_installers/single.py
+++
@@ -1,8 +0,0 @@
-from manager.installer import Installer
-from manager.utils.filesystem import Mode
-from pathlib import Path
-
-
-class InstallerA(Installer):
-    def install(self, home_dir: Path, mode: Mode):
-        pass
