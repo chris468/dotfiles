f0217dd538b519364ed5ea996ca751ea1ea7a886
Chris Pawling <chris-468@outlook.com>
2022-03-10 06:52:05 -0600

    add checking status to prompt

--- .config/fish/conf.d/auto-update-dotfiles.fish
+++ .config/fish/conf.d/auto-update-dotfiles.fish
@@ -2,6 +2,9 @@ if not status is-interactive
     exit
 end
 
+~/.config/yadm/scripts/auto-update.sh -reset
+~/.config/yadm/scripts/auto-update.sh
+
 function _set_auto_update_status --on-event fish_prompt
     set auto_update_log_dir ~/.cache/yadm/auto-update
     set auto_update_status $auto_update_log_dir/status.log
@@ -10,7 +13,3 @@ function _set_auto_update_status --on-event fish_prompt
         and set -gx YADM_AUTO_UPDATE_STATUS (cat $auto_update_status)
         or set -e YADM_AUTO_UPDATE_STATUS
 end
-
-~/.config/yadm/scripts/auto-update.sh reset
-~/.config/yadm/scripts/auto-update.sh
-
