bcd01ec893003dba445ce83215c2817a7ffcb677
Chris Pawling <chris-468@outlook.com>
2022-03-10 05:58:19 -0600

    env needs to be exported globally

--- .config/fish/conf.d/auto-update-dotfiles.fish
+++ .config/fish/conf.d/auto-update-dotfiles.fish
@@ -2,13 +2,12 @@ if not status is-interactive
     exit
 end
 
-
 function _set_auto_update_status --on-event fish_prompt
     set auto_update_log_dir ~/.cache/yadm/auto-update
     set auto_update_status $auto_update_log_dir/status.log
 
     [ -e $auto_update_status ]
-        and set -x YADM_AUTO_UPDATE_STATUS (cat $auto_update_status)
+        and set -gx YADM_AUTO_UPDATE_STATUS (cat $auto_update_status)
         or set -e YADM_AUTO_UPDATE_STATUS
 end
 
