43d9cd4e173fda787cde0b8c9021541a2c982b43
Chris Pawling <chris-468@outlook.com>
2022-03-11 05:56:30 -0600

    yadm: avoid multiple simultaneous updates

--- .config/yadm/scripts/auto-update.sh
+++ .config/yadm/scripts/auto-update.sh
@@ -3,6 +3,7 @@
 auto_update_log_dir=~/.cache/yadm/auto-update
 auto_update_status=$auto_update_log_dir/status.log
 auto_update_last_update=$auto_update_log_dir/last_update
+auto_update_in_progress=$auto_update_log_dir/in_progress
 
 function set-status {
     echo "$@" > $auto_update_status
@@ -23,18 +24,30 @@ function update-dotfiles  {
 
 function update-dotfiles-in-background {
     update-dotfiles || set-status "FAILED"
+    rm -f $auto_update_in_progress
+}
+
+function is-recent {
+    [ -e "$auto_update_last_update" ] && [ -n "$(find $auto_update_last_update -mmin -30)" ]
 }
 
 mkdir -p $auto_update_log_dir
 
+[ ! -e "$auto_update_in_progress" ] || exit
+
 if [ "$1" = "-reset" ] ; then
-    if [ -e "$auto_update_last_update" ] && [ -n "$(find $auto_update_last_update -mmin -30)" ]; then
+    if is-recent ; then
         set-status "RECENT"
+        rm -f $auto_update_in_progress
     else
-        touch $auto_update_last_update
         set-status "CHECKING"
     fi
     exit
 fi
+
+! is-recent || exit
+
+touch $auto_update_in_progress
+touch $auto_update_last_update
 update-dotfiles-in-background 2>&1 > ~/.cache/update-dotfiles.log &
 
