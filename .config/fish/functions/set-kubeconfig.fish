e1f4eb2448308b1d4da751be78c776fbed8c8217
Chris Pawling <chris-468@outlook.com>
2022-06-17 17:48:13 -0500

    fish: detect symlinked kube config files

--- .config/fish/functions/set-kubeconfig.fish
+++ .config/fish/functions/set-kubeconfig.fish
@@ -1,6 +1,6 @@
 function set-kubeconfig
     if test -d ~/.kube
-        set -l files (string join ':' (find ~/.kube/ -maxdepth 1 -type f))
+        set -l files (string join ':' (find ~/.kube/ -maxdepth 1 -type f,l))
         if test -n "$files"
             set -x -g KUBECONFIG "$files"
         end
