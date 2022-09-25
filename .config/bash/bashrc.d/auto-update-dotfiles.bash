f5d1a0c101dba50ee6d1039fe73425ca03ce969d
Chris Pawling <chris-468@outlook.com>
2022-09-25 13:59:38 -0500

    yadm/prompt: add changes indicator to prompt

--- .config/bash/bashrc.d/auto-update-dotfiles.bash
+++ .config/bash/bashrc.d/auto-update-dotfiles.bash
@@ -5,9 +5,19 @@ function _set_auto_update_status {
     auto_update_log_dir=~/.cache/yadm/auto-update
     auto_update_status=$auto_update_log_dir/status.log
 
-    [ -e $auto_update_status ] \
-        && export YADM_AUTO_UPDATE_STATUS=$(cat $auto_update_status) \
-            || unset YADM_AUTO_UPDATE_STATUS
+    if [ -e $auto_update_status ]
+    then
+        export YADM_AUTO_UPDATE_STATUS=$(cat $auto_update_status)
+    else
+        unset YADM_AUTO_UPDATE_STATUS
+    fi
+
+    if [ -n "$(yadm status -s)" ]
+    then
+        export YADM_CHANGES=1
+    else
+        export YADM_CHANGES=0
+    fi
 }
 
 PROMPT_COMMAND="_set_auto_update_status; $PROMPT_COMMAND"
