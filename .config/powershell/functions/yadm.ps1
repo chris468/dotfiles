95e40ba952a542d817e41b0e8bef943bc9e59020
Chris Pawling <chris-468@outlook.com>
2022-10-01 12:09:17 -0500

    move yadm to functions

---
+++ .config/powershell/functions/yadm.ps1
@@ -0,0 +1,3 @@
+function yadm {
+    & 'C:\Program Files\Git\bin\bash.exe' -c "export MSYS=winsymlinks:nativestrict && yadm $args"
+}
--- .config/powershell/profile.ps1
+++ .config/powershell/profile.ps1
@@ -1,12 +1,10 @@
 $config_dir = "$HOME/.config"
 
-function yadm {
-    & 'C:\Program Files\Git\bin\bash.exe' -c "export MSYS=winsymlinks:nativestrict && yadm $args"
-}
-
 foreach ($script in Get-ChildItem $config_dir/powershell/profile.d -Filter '*.ps1') {
     . $script
 }
 
-#. Configure-Prompt
-#. Configure-Completions
+foreach ($script in Get-ChildItem $config_dir/powershell/functions -Filter '*.ps1') {
+    . $script
+}
+
