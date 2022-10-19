28c33b20bf39489261cec62a5d238aa96e593689
Chris Pawling <chris-468@outlook.com>
2022-10-18 22:24:17 -0500

    ahk script detects ergodox

--- .config/ahk/detect-usb-device.ps1
+++ .config/ahk/detect-usb-device.ps1
@@ -1,13 +1,22 @@
 param(
     [Parameter(Mandatory=$true, Position=0)]
-    [string]$Name
+    [string]$Name,
+    [Parameter(Mandatory=$false)]
+    [string]$OutputPath=$null
 )
 
 $keyName="DEVPKEY_Device_BusReportedDeviceDesc"
 
-(Get-PnpDevice -PresentOnly `
+$present=(Get-PnpDevice -PresentOnly `
     | Where-Object { $_.InstanceId -match '^USB' } `
     | ForEach-Object { Get-PnpDeviceProperty -InstanceId $_.InstanceId -KeyName $keyName } `
     | Where-Object { $_.Data -eq $Name } `
     | ForEach-Object { $true } `
-    | Select-Object -First 1) -eq $True
+    | Select-Object -First 1).Count
+
+if ( $OutputPath ) {
+    $parent = New-Item -ItemType Directory -Path (split-path $OutputPath) -Force
+    Set-Content -Path $OutputPath $present
+} else {
+    $present
+}
