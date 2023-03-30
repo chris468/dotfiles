0d70f7287e20c61de384f7fc20b25fd612b45a9e
Chris Pawling <chris-468@outlook.com>
2023-03-30 18:54:39 -0500

    clean up

--- .config/nvim/lua/mappings.lua
+++ .config/nvim/lua/mappings.lua
@@ -1,13 +1,16 @@
-local keymap = vim.api.nvim_set_keymap
 local opts = { noremap = true, silent = true }
 
-keymap("", "<Space>", "<Nop>", opts)
+vim.keymap.set("", "<Space>", "<Nop>", opts)
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
+vim.keymap.set("n", "<leader>h", vim.cmd.tabprevious, opts)
+vim.keymap.set("n", "<leader>l", vim.cmd.tabnext, opts)
+vim.keymap.set("n", "<leader>n", vim.cmd.nohlsearch, opts)
+
 if require 'extensions.nvim-tree' then
   nvim_tree = require('nvim-tree.api')
-  keymap("n", "<leader>ef", ":NvimTreeFocus<CR>", opts)
-  keymap("n", "<leader>eF", ":NvimTreeFindFile<CR>", opts)
-  keymap("n", "<leader>et", ":NvimTreeToggle<CR>", opts)
+  vim.keymap.set("n", "<leader>ef", nvim_tree.tree.open, opts)
+  vim.keymap.set("n", "<leader>eF", function() nvim_tree.tree.open({find_file = true}) end, opts)
+  vim.keymap.set("n", "<leader>et", nvim_tree.tree.toggle, opts)
 end
