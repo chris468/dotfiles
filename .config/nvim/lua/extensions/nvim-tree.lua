f864fb33402ddca7511a025403a5ff2fc1198bc4
Chris Pawling <chris-468@outlook.com>
2023-03-31 07:03:05 -0500

    nvim-tree config

--- .config/nvim/lua/extensions/nvim-tree.lua
+++ .config/nvim/lua/extensions/nvim-tree.lua
@@ -3,4 +3,28 @@ if not status_ok then
   return
 end
 
-nvim_tree.setup()
+nvim_tree.setup {
+  renderer = {
+    group_empty = true,
+    full_name = true,
+    indent_width = 1,
+    indent_markers = {
+      enable = true,
+      inline_arrows = false,
+    },
+    icons = {
+      glyphs = {
+        folder = {
+          default = '\u{25b6}',
+          open = '\u{25bd}',
+          empty = '\u{25b6}',
+          empty_open = '\u{25bd}',
+          symlink = '\u{25b6}',
+          symlink_open = '\u{25bd}',
+        },
+        git = {
+        },
+      },
+    },
+  }
+}
