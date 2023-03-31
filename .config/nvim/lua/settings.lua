e4a5a3d78f22d0d8c5422732e24d9b55233d21a0
Chris Pawling <chris-468@outlook.com>
2023-03-30 19:03:20 -0500

    more settings

--- .config/nvim/lua/settings.lua
+++ .config/nvim/lua/settings.lua
@@ -1,15 +1,24 @@
 local global_options = {
+  dracula_italic = 0,
   loaded_netrw = 1,
-  loaded_netrwPlugin = 1
+  loaded_netrwPlugin = 1,
 }
 
 local options = {
+  background = dark,
   backup = false,
+  cursorline = true,
   directory = vim.fn.stdpath('cache')..'/swp/',
   encoding = 'utf-8',
+  expandtab = true,
   hlsearch = true,
   ignorecase = true,
+  number = true,
+  relativenumber = true,
+  shiftwidth = 4,
+  signcolumn = 'yes',
   smartcase = true,
+  tabstop = 4,
   termguicolors = true,
   wildignore = '**/bin/**,**/obj/**,*.nupkg,**/__pycache__/**,*.tfstate*',
   wrap = false,
