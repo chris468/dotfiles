69550e0179f9f609c2be28583dd7446811fe8d3b
Chris Pawling <chris-468@outlook.com>
2023-03-30 20:32:36 -0500

    add vim-surround, vim-exchange plugins

--- .config/nvim/lua/extensions/packer.lua
+++ .config/nvim/lua/extensions/packer.lua
@@ -24,6 +24,8 @@ return packer.startup(function(use)
   use 'dracula/vim'
   use { 'nvim-telescope/telescope.nvim', requires = {{ 'nvim-lua/plenary.nvim' }}}
   use 'chentoast/marks.nvim'
+  use 'tommcdo/vim-exchange'
+  use 'tpope/vim-surround'
 
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
