5167c7cdec65f537863a1ef03c8ed24bd0c5993a
Chris Pawling <chris-468@outlook.com>
2023-04-01 17:25:12 -0500

    pull out helper that requires all in a dir

--- .config/nvim/lua/chris468/lsp/init.lua
+++ .config/nvim/lua/chris468/lsp/init.lua
@@ -1,16 +1,9 @@
 require 'chris468.util.if-ext' ('lspconfig', function(lspconfig)
 
-  local servers = (function()
-   local settings = {}
-    local server_configs_path = vim.fn.stdpath('config') .. '/lua/chris468/lsp/servers'
-    for f in vim.fs.dir(server_configs_path) do
-      local server = string.gsub(f, '.lua', '')
-      settings[server] = require('chris468.lsp.servers.' .. server)
-    end
-    return settings
-  end)()
+  local require_all = require 'chris468.util.require-all'
+  local servers = require_all 'chris468.lsp.servers'
 
-  local function add_capabilities(settings)
+  local function with_capabilities(settings)
 
     local s = require 'chris468.util.if-ext' ('cmp_nvim_lsp',
       function(cmp_nvim_lsp)
@@ -22,11 +15,12 @@ require 'chris468.util.if-ext' ('lspconfig', function(lspconfig)
     for k, v in pairs(settings) do
       s[k] = v
     end
+
     return s
   end
 
   for server, settings in pairs(servers) do
-    lspconfig[server].setup(add_capabilities(settings))
+    lspconfig[server].setup(with_capabilities(settings))
   end
 
 end)
