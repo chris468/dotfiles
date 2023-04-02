157ff91ebe79b01e131822441ad1bffb4f0e80e7
Chris Pawling <chris-468@outlook.com>
2023-04-02 14:25:11 -0500

    print error if python debug adapter not found

--- .config/nvim/lua/chris468/dap/python.lua
+++ .config/nvim/lua/chris468/dap/python.lua
@@ -1,6 +1,11 @@
 return {
   adapter = function(default)
-    default.command = vim.fn.exepath('debugy-adapter')
+    if not default.command or default.command == '' then
+      default.command = vim.fn.exepath('debugy-adapter')
+    end
+    if not default.command or default.command == '' then
+      vim.cmd("echoe 'debugpy-adapter not found'")
+    end
     return default
   end
 }
