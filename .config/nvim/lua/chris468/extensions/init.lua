851bf97d67b41c7de05435743b836cb63c3ad47b
Chris Pawling <chris-468@outlook.com>
2023-04-02 13:51:45 -0500

    load all from extensions, don't explicitly specify

--- .config/nvim/lua/chris468/extensions/init.lua
+++ .config/nvim/lua/chris468/extensions/init.lua
@@ -1,3 +1,59 @@
-require 'chris468.extensions.packer'
-require 'chris468.extensions.nvim-tree'
-require 'chris468.extensions.mason'
+local fn = vim.fn
+local stdpath = fn.stdpath
+local ensure_packer = function()
+  local install_path = stdpath('data')..'/site/pack/packer/start/packer.nvim'
+  if fn.empty(fn.glob(install_path)) > 0 then
+    fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+    vim.cmd [[packadd packer.nvim]]
+    return true
+  end
+  return false
+end
+
+local packer_bootstrap = ensure_packer()
+
+return require 'chris468.util.if-ext' ('packer', function(packer)
+
+  vim.cmd([[
+    augroup packer_user_config
+      autocmd!
+      autocmd BufWritePost ]] .. stdpath('config') .. [[/lua/chris468/extensions/init.lua source <afile> | PackerSync
+    augroup end
+  ]])
+
+  packer.init()
+
+  packer.startup(function(use)
+    use 'wbthomason/packer.nvim'
+
+    use 'dracula/vim'
+
+    use 'nvim-tree/nvim-tree.lua'
+    use { 'nvim-telescope/telescope.nvim', requires = {{ 'nvim-lua/plenary.nvim' }}}
+
+    use 'tommcdo/vim-exchange'
+    use 'tpope/vim-surround'
+
+    use 'mfussenegger/nvim-dap'
+
+    use { "williamboman/mason.nvim", run = ":MasonUpdate" }
+    use { "williamboman/mason-lspconfig.nvim" }
+    use { "jay-babu/mason-nvim-dap.nvim" }
+
+
+    use 'neovim/nvim-lspconfig'
+    use 'hrsh7th/nvim-cmp'
+    use 'hrsh7th/cmp-nvim-lsp'
+    use 'hrsh7th/cmp-nvim-lua'
+
+    -- Automatically set up your configuration after cloning packer.nvim
+    -- Put this at the end after all plugins
+    if packer_bootstrap then
+      require('packer').sync()
+    end
+  end)
+
+  local require_all = require('chris468.util.require-all')
+  require_all('chris468.extensions')
+
+end)
