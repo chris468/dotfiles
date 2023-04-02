e7bb733ece9dae0bd6c1cbca185a8f5bef094687
Chris Pawling <chris-468@outlook.com>
2023-04-02 13:24:53 -0500

    filter out `init` so that the `init` in a folder can require-all

--- .config/nvim/lua/chris468/util/require-all.lua
+++ .config/nvim/lua/chris468/util/require-all.lua
@@ -1,27 +1,28 @@
-local function filter_libs(files_iter, initial_state)
+local function filter_libs(filter, files_iter, initial_state)
   local function iter(state)
     while true do
       local name, _ = files_iter(state)
       if name == nil then break end
       local lib_name, is_lib = string.gsub(name, '.lua', '')
-      if is_lib > 0 then return lib_name end
+      if is_lib > 0 and filter(lib_name) then return lib_name end
     end
   end
   return iter, initial_state
 end
 
-local function enumerate_libs(parent)
+local function enumerate_libs(parent, filter)
   local libs = {}
+  filter = filter or function (name) return name ~= 'init' end
   local parent_path = vim.fn.stdpath('config') .. '/lua/' .. string.gsub(parent, '%.', '/')
-  for lib in filter_libs(vim.fs.dir(parent_path)) do
+  for lib in filter_libs(filter, vim.fs.dir(parent_path)) do
     table.insert(libs, lib)
   end
   table.sort(libs)
   return libs
 end
 
-local function load_libs(parent)
-  local libs = enumerate_libs(parent)
+local function load_libs(parent, filter)
+  local libs = enumerate_libs(parent, filter)
   local loaded = {}
   for _, lib in ipairs(libs) do
     loaded[lib] = require(parent .. '.' .. lib)
