880bd123a54581149a3eb041198a333e68ec3643
Chris Pawling <chris-468@outlook.com>
2023-04-03 14:53:15 -0500

    look for default debug configuration

--- .config/nvim/lua/chris468/mappings/debugger.lua
+++ .config/nvim/lua/chris468/mappings/debugger.lua
@@ -1,9 +1,41 @@
 return function(dap)
   local first = true
+  local defaults = {_keys = {}}
+
+  local function find_defaults()
+    local new_defaults = {_keys = {}}
+    for filetype, configs in pairs(dap.configurations) do
+      for _, config in ipairs(configs) do
+        if config.default then
+          new_defaults[filetype] = config
+          new_defaults._keys[#new_defaults._keys + 1] = {filetype=filetype, config=config}
+          break
+        end
+      end
+    end
+    print('new defaults', vim.inspect(new_defaults))
+    defaults = new_defaults
+  end
+
+  local function load_configs()
+    require('dap.ext.vscode').load_launchjs()
+    find_defaults()
+  end
+
+  local function select_default()
+    load_configs()
+    find_defaults()
+    if #defaults._keys == 1 then
+      return defaults._keys[1].config
+    end
+
+    local ft = vim.bo.filetype
+    return defaults[ft]
+  end
 
   local function prompt()
     first = false
-    require('dap.ext.vscode').load_launchjs()
+    load_configs()
     dap.continue()
   end
 
@@ -19,10 +51,15 @@ return function(dap)
     if dap.session() then
       dap.continue()
     else
-      if first then
-        prompt()
+      local default = select_default()
+      if default then
+        dap.run(default)
       else
-        dap.run_last()
+        if first then
+          prompt()
+        else
+          dap.run_last()
+        end
       end
     end
   end
