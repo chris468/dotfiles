c51f8ce2e9f2b73d8a62c24e150c4b262ab25be0
Chris Pawling <chris-468@outlook.com>
2023-04-03 10:10:54 -0500

    remove dap-virtual0text

--- .config/nvim/lua/chris468/extensions/50-nvim-treesitter.lua
+++
@@ -1,3 +0,0 @@
-require 'chris468.util.if-ext' ('nvim-treesitter', function(ts)
-  ts.setup()
-end)
--- .config/nvim/lua/chris468/extensions/init.lua
+++ .config/nvim/lua/chris468/extensions/init.lua
@@ -51,9 +51,6 @@ return require 'chris468.util.if-ext' ('packer', function(packer)
     use 'lewis6991/gitsigns.nvim'
     use { 'TimUntersberger/neogit', requires = 'nvim-lua/plenary.nvim' }
 
-    use { 'nvim-treesitter/nvim-treesitter', tag = 'v0.8.3', disable = true }
-    use { 'theHamsta/nvim-dap-virtual-text', disable = true }
-
     use { "rcarriga/nvim-dap-ui", requires = {"mfussenegger/nvim-dap"} }
 
     -- Automatically set up your configuration after cloning packer.nvim
