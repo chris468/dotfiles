f76e38efdc37d35623c8e2384a48391528e3218a
Chris Pawling <chris-468@outlook.com>
2023-04-03 10:10:44 -0500

    configure dap-ui

--- .config/nvim/lua/chris468/extensions/50-nvim-dap-ui.lua
+++ .config/nvim/lua/chris468/extensions/50-nvim-dap-ui.lua
@@ -1,4 +1,15 @@
-require 'chris468.util.if-ext' ('dapui', function(dapui)
+require 'chris468.util.if-ext' ({'dap', 'dapui'}, function(exts)
+  local dap, dapui = exts.dap, exts.dapui
   dapui.setup()
-end)
 
+  dap.listeners.after.event_initialized["dapui_config"] = function()
+    dapui.open()
+  end
+
+  for _, close_event in pairs({'event_terminated', 'event_exited'}) do
+    dap.listeners.before[close_event]["dapui_config"] = function()
+      dapui.close()
+    end
+  end
+
+end)
--- .config/nvim/lua/chris468/util/if-ext.lua
+++ .config/nvim/lua/chris468/util/if-ext.lua
@@ -1,8 +1,20 @@
-return function(ext_name, callback, fail_callback)
-  local status_ok, ext = pcall(require, ext_name)
-  if not status_ok then
-    local f = fail_callback or function(_) end
-    return f()
+return function(ext_names, callback, fail_callback)
+
+  local single = type(ext_names) == 'string'
+  if single then ext_names = { ext_names } end
+
+  local exts = {}
+
+  for _, ext_name in ipairs(ext_names) do
+    local status_ok, ext = pcall(require, ext_name)
+    if not status_ok then
+      local f = fail_callback or function(_) end
+      return f()
+    end
+    exts[ext_name] = ext
   end
-  return callback(ext)
+
+  if single then exts = exts[ext_names[1]] end
+
+  return callback(exts)
 end
