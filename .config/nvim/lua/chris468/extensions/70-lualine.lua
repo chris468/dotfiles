cdadd92bfb5d9a461fd90b48d2838bd51e69e101
Chris Pawling <chris-468@outlook.com>
2023-04-06 09:42:56 -0500

    shorten user branch names

--- .config/nvim/lua/chris468/extensions/70-lualine.lua
+++ .config/nvim/lua/chris468/extensions/70-lualine.lua
@@ -4,7 +4,14 @@ require('chris468.util.if-ext')('lualine', function(lualine)
     theme = 'dracula',
     sections = {
       lualine_b = {
-        'branch',
+        {
+          'branch',
+          fmt = function(branch)
+            return branch
+              :gsub('^users/cpawling', 'u')
+              :gsub('^users', 'u')
+          end,
+        },
         'diff',
         { 'diagnostics',
           sections = { 'error', 'warn' },
