6d5e91564286824ddae80a8c18137e6aec6a672a
Chris Pawling <chris-468@outlook.com>
2023-04-06 16:41:30 -0500

    clean up cmp mappings

--- .config/nvim/lua/chris468/extensions/50-cmp.lua
+++ .config/nvim/lua/chris468/extensions/50-cmp.lua
@@ -8,48 +8,54 @@ if_ext({'cmp', 'cmp_nvim_lsp'}, function(cmp, _)
     return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
   end
 
-  local mappings = {
-    {
-      keys = { '<Tab>', '<C-N>', '<C-J>', },
-      mapping = cmp.mapping(function(fallback)
+  -- set invisible to false to fallback even if nothing to call if invisible
+  -- if left null, the callback will be called unconditionally
+  local function mapping(callback, invisible, modes)
+    local always = invisible == nil
+    modes = modes or { 'i', 's', 'c' }
+
+    if always then
+      return cmp.mapping(function(_) callback() end, modes)
+    else
+      return cmp.mapping(function(fallback)
         if cmp.visible() then
-          cmp.select_next_item()
-        elseif has_words_before() then
-          cmp.complete()
-        else
-          fallback() -- The fallback function sends a already mapped key. In this case, it's probably `<Tab>`.
-        end
-      end, { "i", "s" }),
-    },
-    {
-      keys = { '<S-Tab>', '<C-P>', '<C-K>' },
-      mapping = cmp.mapping(function(fallback)
-        if cmp.visible() then
-          cmp.select_prev_item()
+          callback()
+        elseif invisible and has_words_before() then
+          invisible()
         else
           fallback()
         end
-      end, { "i", "s" }),
+      end, modes)
+    end
+  end
+
+
+  local mappings = {
+    {
+      keys = { '<C-Space>', }, mapping = mapping(cmp.complete) },
+    {
+      keys = { '<Tab>', '<C-N>', '<C-J>', },
+      mapping = mapping(cmp.select_next_item, cmp.complete)
+    },
+    {
+      keys = { '<Down>', },
+      mapping = mapping(cmp.select_next_item, cmp.complete, { 'i', 's' })
+    },
+    {
+      keys = { '<S-Tab>', '<C-P>', '<C-K>', },
+      mapping = mapping(cmp.select_prev_item, cmp.complete)
+    },
+    {
+      keys = { '<Up>', },
+      mapping = mapping(cmp.select_prev_item, cmp.complete, { 'i', 's' })
     },
     {
       keys = { '<CR>' },
-      mapping = cmp.mapping(function(fallback)
-        if cmp.visible() then
-          cmp.confirm()
-        else
-          fallback()
-        end
-      end)
+      mapping = mapping(cmp.confirm, false, {'i', 's'})
     },
     {
       keys = { '<C-E>' },
-      mapping = cmp.mapping(function(fallback)
-        if cmp.visible() then
-          cmp.abort()
-        else
-          fallback()
-        end
-      end),
+      mapping = mapping(cmp.abort, false)
     },
   }
 
@@ -76,14 +82,12 @@ if_ext({'cmp', 'cmp_nvim_lsp'}, function(cmp, _)
   cmp.setup(options)
 
   cmp.setup.cmdline({ '/', '?' }, {
-    mapping = cmp.mapping.preset.cmdline(),
     sources = {
       { name = 'buffer' },
     }
   })
 
   cmp.setup.cmdline(':', {
-    mapping = cmp.mapping.preset.cmdline(),
     sources = {
       { name = 'path' },
       { name = 'cmdline' },
