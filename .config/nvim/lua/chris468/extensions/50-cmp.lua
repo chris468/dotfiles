254d4b1c93abee8ebaa31a2fedab911d11945355
Chris Pawling <chris-468@outlook.com>
2023-04-05 19:55:16 -0500

    fix cmp sources

--- .config/nvim/lua/chris468/extensions/50-cmp.lua
+++ .config/nvim/lua/chris468/extensions/50-cmp.lua
@@ -3,7 +3,7 @@ local if_ext = require 'chris468.util.if-ext'
 if_ext('nvim-cmp', function(cmp) if_ext('cmp_nvm_lsp', function(_)
   cmp.setup {
     sources = {
-      name = 'nvim_lsp'
+      { name = 'nvim_lsp' }
     }
   }
 end) end)
