d77599c69faa45ef55b052141790394d3b17ed20
Chris Pawling <chris-468@outlook.com>
2023-04-05 20:03:39 -0500

    add tab/shift-tab cmp mappings

--- .config/nvim/lua/chris468/extensions/50-cmp.lua
+++ .config/nvim/lua/chris468/extensions/50-cmp.lua
@@ -1,6 +1,13 @@
 local if_ext = require 'chris468.util.if-ext'
 
 if_ext('nvim-cmp', function(cmp) if_ext('cmp_nvm_lsp', function(_)
+
+  local has_words_before = function()
+    unpack = unpack or table.unpack
+    local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+    return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+  end
+
   cmp.setup {
     snippet = {
       expand = function(args)
@@ -10,6 +17,25 @@ if_ext('nvim-cmp', function(cmp) if_ext('cmp_nvm_lsp', function(_)
     sources = {
       { name = 'luasnip' },
       { name = 'nvim_lsp' },
+    },
+    mappings = {
+      ["<Tab>"] = cmp.mapping(function(fallback)
+        if cmp.visible() then
+          cmp.select_next_item()
+        elseif has_words_before() then
+          cmp.complete()
+        else
+          fallback() -- The fallback function sends a already mapped key. In this case, it's probably `<Tab>`.
+        end
+      end, { "i", "s" }),
+
+      ["<S-Tab>"] = cmp.mapping(function(fallback)
+        if cmp.visible() then
+          cmp.select_prev_item()
+        else
+          fallback()
+        end
+      end, { "i", "s" }),
     }
   }
 end) end)
