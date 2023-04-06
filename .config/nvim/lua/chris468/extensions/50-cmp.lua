234016b2cdf400f98251e481e837b7f7f06c5354
Chris Pawling <chris-468@outlook.com>
2023-04-05 20:00:58 -0500

    add snippet engine

--- .config/nvim/lua/chris468/extensions/50-cmp.lua
+++ .config/nvim/lua/chris468/extensions/50-cmp.lua
@@ -2,8 +2,14 @@ local if_ext = require 'chris468.util.if-ext'
 
 if_ext('nvim-cmp', function(cmp) if_ext('cmp_nvm_lsp', function(_)
   cmp.setup {
+    snippet = {
+      expand = function(args)
+        require 'luasnip'.lsp_expand(args.body)
+      end
+    },
     sources = {
-      { name = 'nvim_lsp' }
+      { name = 'luasnip' },
+      { name = 'nvim_lsp' },
     }
   }
 end) end)
