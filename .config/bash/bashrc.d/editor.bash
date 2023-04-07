60e0ae85a5d6eb9a02fd12cb70d2ffd88f2dd4ad
Chris Pawling <chris-468@outlook.com>
2023-04-07 12:25:03 -0500

    bash: look for the best editor

--- .config/bash/bashrc.d/editor.bash
+++ .config/bash/bashrc.d/editor.bash
@@ -1,5 +1,17 @@
 function __find_editor {
-
+    editors=('nvim' 'vim.bat' 'gvim.bat' 'vim' 'vi' )
+    for e in ${editors[@]}
+    do
+        if command -v "$e"
+        then
+            return
+        fi
+    done
 }
 
-export EDITOR=$(__find_editor)
+__editor=$(__find_editor)
+if [ -n "$__editor" ]
+then
+    export EDITOR="$__editor"
+fi
+unset __editor
