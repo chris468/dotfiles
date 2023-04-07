f72dd44c427e55a80300f08cc3f6801662fcefa9
Chris Pawling <chris-468@outlook.com>
2023-04-06 19:05:12 -0500

    enable manual mode in project.nvim
    This prevents changing directory when just opening file. But when selecting project, it still changes

--- .config/nvim/lua/chris468/extensions/50-project.lua
+++ .config/nvim/lua/chris468/extensions/50-project.lua
@@ -1,6 +1,6 @@
 local if_ext = require 'chris468.util.if-ext'
 if_ext ({'project_nvim', 'telescope'}, function(project, telescope)
-  project.setup {}
+  project.setup { manual_mode = true }
   telescope.load_extension('projects')
 end)
 
