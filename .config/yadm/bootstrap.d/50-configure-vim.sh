97b8fe595cd4ed89626a2c5995263b6654f3f67e
Chris Pawling <chris-468@outlook.com>
2023-04-06 22:57:01 -0500

    just disable vim bootstrap
    nvim does it automatically anyway

--- .config/yadm/bootstrap.d/50-configure-vim.sh
+++ .config/yadm/bootstrap.d/50-configure-vim.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-if command -v nvim &> /dev/null
-then
-    nvim -c 'autocmd User PackerComplete quitall' -c PackerSync
-elif command -v vim &> /dev/null
-then
-    vim -c PlugInstall -c qa
-fi
+# if command -v nvim &> /dev/null
+# then
+#     nvim -c 'autocmd User PackerComplete quitall' -c PackerSync
+# elif command -v vim &> /dev/null
+# then
+#     vim -c PlugInstall -c qa
+# fi
