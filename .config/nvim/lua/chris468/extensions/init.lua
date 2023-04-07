8a7e2cf752e4e5735ae8f2e991642e5a42ff88b1
Chris Pawling <chris-468@outlook.com>
2023-04-06 20:27:25 -0500

    set package tag or branch, or comment why not

--- .config/nvim/lua/chris468/extensions/init.lua
+++ .config/nvim/lua/chris468/extensions/init.lua
@@ -30,59 +30,68 @@ return require 'chris468.util.if-ext' ('packer', function(packer)
   packer.init()
 
   packer.startup(function(use)
-    use 'wbthomason/packer.nvim'
-
-    use 'dracula/vim'
-
-    use 'nvim-tree/nvim-tree.lua'
-    use { 'nvim-telescope/telescope.nvim', requires = {{ 'nvim-lua/plenary.nvim' }}}
-
-    use 'tommcdo/vim-exchange'
-    use 'tpope/vim-surround'
-
-    use 'mfussenegger/nvim-dap'
-
-    use "williamboman/mason.nvim"
-    use { "williamboman/mason-lspconfig.nvim" }
-    use { "jay-babu/mason-nvim-dap.nvim" }
-
-
-    use 'neovim/nvim-lspconfig'
-    use 'L3MON4D3/LuaSnip'
-    use 'saadparwaiz1/cmp_luasnip'
-    use 'hrsh7th/cmp-buffer'
-    use 'hrsh7th/cmp-cmdline'
-    use 'hrsh7th/cmp-nvim-lsp'
-    use 'hrsh7th/cmp-nvim-lua'
-    use 'hrsh7th/cmp-path'
-    use 'hrsh7th/nvim-cmp'
-
-    use 'rafcamlet/nvim-luapad'
-
-    use 'lewis6991/gitsigns.nvim'
-    use { 'TimUntersberger/neogit', requires = 'nvim-lua/plenary.nvim' }
-
-    use { 'nvim-treesitter/nvim-treesitter', tag = 'v0.8.3' }
-    use { 'theHamsta/nvim-dap-virtual-text', requires = 'nvim-treesitter/nvim-treesitter' }
-
-    use { "rcarriga/nvim-dap-ui", requires = {"mfussenegger/nvim-dap"} }
-
     use {
-      'nvim-neotest/neotest',
-      requires = {
-        'nvim-treesitter/nvim-treesitter',
-        'nvim-lua/plenary.nvim'
-      }
+      { 'wbthomason/packer.nvim' }, -- no tags
+      { 'nvim-lua/plenary.nvim', tag='v0.*' },
+
+      { 'dracula/vim', branch = 'master' },
+
+      { 'nvim-tree/nvim-tree.lua' }, -- only has `nightly` tag
+
+      {
+        'nvim-telescope/telescope.nvim',
+        -- tag = 'v0.*' , *lots* of changes since tag, fear bug fixes.
+        requires = { 'nvim-lua/plenary.nvim' }
+      },
+
+      { 'tommcdo/vim-exchange' }, -- no tags
+
+      { 'tpope/vim-surround' }, -- tags are pretty old
+
+      { 'mfussenegger/nvim-dap', tag = '0.*' },
+
+      { "williamboman/mason.nvim" }, -- no tgs
+      { "williamboman/mason-lspconfig.nvim" }, -- no tags
+      { "jay-babu/mason-nvim-dap.nvim", tag = 'v1.*' },
+
+      { 'neovim/nvim-lspconfig' }, -- bug fixes since last tag
+      { 'L3MON4D3/LuaSnip', tag = 'v1.*' },
+      { 'saadparwaiz1/cmp_luasnip' }, -- no tags
+      { 'hrsh7th/cmp-buffer' },  -- no tags
+      { 'hrsh7th/cmp-cmdline' }, -- no tags
+      { 'hrsh7th/cmp-nvim-lsp' }, -- no tags
+      { 'hrsh7th/cmp-nvim-lua' }, -- no tags
+      { 'hrsh7th/cmp-path' }, -- no tags
+      { 'hrsh7th/nvim-cmp' }, -- no tags
+
+      { 'rafcamlet/nvim-luapad' }, -- outdated tags
+
+      { 'lewis6991/gitsigns.nvim', tag = 'v0.*' },
+      { 'TimUntersberger/neogit', requires = 'nvim-lua/plenary.nvim' }, -- no tags
+
+      { 'nvim-treesitter/nvim-treesitter', tag = 'v0.8.*' },
+      { 'theHamsta/nvim-dap-virtual-text', requires = 'nvim-treesitter/nvim-treesitter' }, -- no tags
+
+      { "rcarriga/nvim-dap-ui", tag = 'v3.*', requires = {"mfussenegger/nvim-dap"} },
+
+      {
+        'nvim-neotest/neotest',
+        tag = 'v2.*',
+        requires = {
+          'nvim-treesitter/nvim-treesitter',
+          'nvim-lua/plenary.nvim'
+        }
+      },
+
+      { 'nvim-neotest/neotest-python', requires = { 'nvim-neotest/neotest' } }, -- no tags
+      { 'Issafalcon/neotest-dotnet', tag = 'v1.*', requires = { 'nvim-neotest/neotest' } },
+      { 'lukas-reineke/indent-blankline.nvim', tag = 'v2.*' },
+
+      { 'nvim-lualine/lualine.nvim' }, -- tags look like only compat
+      { 'ahmedkhalf/project.nvim', requires = 'nvim-telescope/telescope.nvim' }, -- no tags
+
+      { 'chrisbra/unicode.vim' }, -- unicode.vim tags are really old
     }
-
-    use { 'nvim-neotest/neotest-python', requires = { 'nvim-neotest/neotest' } }
-    use { 'Issafalcon/neotest-dotnet', requires = { 'nvim-neotest/neotest' } }
-    use 'lukas-reineke/indent-blankline.nvim'
-
-    use 'nvim-lualine/lualine.nvim'
-    use { 'ahmedkhalf/project.nvim', requires = 'nvim-telescope/telescope.nvim' }
-
-    use 'chrisbra/unicode.vim'
 
     -- Automatically set up your configuration after cloning packer.nvim
     -- Put this at the end after all plugins
