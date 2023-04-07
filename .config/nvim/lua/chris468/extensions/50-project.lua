8ccf93d8d987cb0c986bebea79c419b9ed39c98b
Chris Pawling <chris-468@outlook.com>
2023-04-07 18:57:24 -0500

    nvim: let project change dir, but scope to tab and ignore site

--- .config/nvim/lua/chris468/extensions/50-project.lua
+++ .config/nvim/lua/chris468/extensions/50-project.lua
@@ -1,6 +1,11 @@
 local if_ext = require 'chris468.util.if-ext'
 if_ext ({'project_nvim', 'telescope'}, function(project, telescope)
-  project.setup { manual_mode = true }
+  project.setup {
+    scope_chdir = 'tab',
+    exclude_dirs = {
+      vim.fn.stdpath('data') .. '/*'
+    }
+  }
   telescope.load_extension('projects')
 end)
 
