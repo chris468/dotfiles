c846f58c6ad7cb0101cc94949ad026be0e4526ee
Chris Pawling <chris-468@outlook.com>
2023-04-15 12:38:57 -0500

    lvim: load launch.json

--- .config/lvim/lua/chris468/dap.lua
+++ .config/lvim/lua/chris468/dap.lua
@@ -1,6 +1,10 @@
-local mason_dap = require("mason-nvim-dap")
-
 local M = {}
+
+local function load_launchjs()
+  local dap_vscode = require("dap.ext.vscode")
+  local filetypes = require("mason-nvim-dap.mappings.filetypes")
+  dap_vscode.load_launchjs(nil, filetypes.adapter_to_configs)
+end
 
 function M.config()
   lvim.chris468.dap = {
@@ -14,10 +18,31 @@ function M.config()
 end
 
 function M.setup(_)
+  local mason_dap = require("mason-nvim-dap")
   mason_dap.setup({
     ensure_installed = lvim.chris468.dap.ensure_installed or {},
     handlers = {}
   })
+
+  local group = vim.api.nvim_create_augroup("chris468.dap", {clear = false})
+  vim.api.nvim_create_autocmd(
+    "DirChanged",
+    {
+      callback = load_launchjs,
+      group = group,
+    }
+  )
+  vim.api.nvim_create_autocmd(
+    "BufWritePost",
+    {
+      pattern = "*/.vscode/launch.json",
+      callback = load_launchjs,
+      group = group,
+    }
+  )
+
+  load_launchjs()
+
 end
 
 return M
