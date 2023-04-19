d35959502ff8bee44c38fe1ab92feca819a79df2
Chris Pawling <chris-468@outlook.com>
2023-04-18 21:43:38 -0500

    remaining yadm scripts

--- yadm/bootstrap.d/50-include-yadm-git-config.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-script=$(readlink -e "${BASH_SOURCE[0]}")
-script_dir="$(cd "$(dirname "$script")" && pwd)"
-git_config_dir="$(dirname $(dirname $script_dir))"/git
-include_file_name=yadm-config
-include_file_path=$git_config_dir/$include_file_name
-
-git config --get-all include.path | grep -q $include_file_name || git config --global --add include.path $include_file_path
