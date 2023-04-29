bf7751b717e5e726e8bf034327be8ad34b9ff6d2
Chris Pawling <chris-468@outlook.com>
2023-04-29 18:22:54 -0500

    Use `Set-PoshContext` instead of hijacking prompt function

--- dotfiles/private_dot_config/powershell/profile.d/40-prompt.ps1
+++ dotfiles/private_dot_config/powershell/profile.d/40-prompt.ps1
@@ -1,43 +1,29 @@
-function Configure-Prompt {
-    function On-VIModeChange {
-        if ($args[0] -eq 'Command') {
-            # Set the cursor to a blinking block.
-            Write-Host -NoNewLine "`e[1 q"
-        } else {
-            # Set the cursor to a blinking line.
-            Write-Host -NoNewLine "`e[5 q"
-        }
+function Script:On-VIModeChange {
+    if ($args[0] -eq 'Command') {
+        # Set the cursor to a blinking block.
+        Write-Host -NoNewLine "`e[1 q"
+    } else {
+        # Set the cursor to a blinking line.
+        Write-Host -NoNewLine "`e[5 q"
     }
-
-    function Initialize-InteractiveSession {
-
-        function Configure-PSReadline {
-            Set-PSReadLineOption -Editmode vi
-            Set-PSReadLineOption -ViModeIndicator Script -ViModeChangeHandler ${Function:On-VIModeChange}
-            Set-PSReadLineOption -HistorySavePath "$HOME/.cache/pwsh/history.txt"
-        }
-
-        if (! $global:InteractiveSession ) {
-            $global:InteractiveSession = $true
-
-            Configure-PSReadLine
-        }
-    }
-
-    # oh my posh replaces the prompt function. To be able to make sure Initialize-InteractiveSession,
-    # configure the posh prompt inside the initial prompt function.
-    $theme = "$config_dir/oh-my-posh/current-theme.omp.json"
-    oh-my-posh init pwsh --config $theme | Invoke-Expression
-
-    Set-Item function:OhMyPoshPrompt (Get-Item function:Prompt).ScriptBlock -Force
-
-    [ScriptBlock]$PromptWrapper = {
-
-        Initialize-InteractiveSession
-        $(OhMyPoshPrompt)
-    }
-
-    Set-Item function:Prompt $PromptWrapper -Force
 }
 
-. Configure-Prompt
+function Script:Configure-PSReadline {
+    Set-PSReadLineOption -Editmode vi
+    Set-PSReadLineOption -ViModeIndicator Script -ViModeChangeHandler ${Function:On-VIModeChange}
+    Set-PSReadLineOption -HistorySavePath "$HOME/.cache/pwsh/history.txt"
+}
+
+
+function Initialize-InteractiveSession {
+    if (! $global:InteractiveSession ) {
+        $global:InteractiveSession = $true
+
+        Configure-PSReadLine
+    }
+}
+
+New-Alias -Name 'Set-PoshContext' -Value 'Initialize-InteractiveSession' -Scope Global -Force
+
+$theme = "$config_dir/oh-my-posh/current-theme.omp.json"
+oh-my-posh init pwsh --config $theme | Invoke-Expression
