51d1f4aedc01edba732fe28c542c4d1b1765f07e
Chris Pawling <chris-468@outlook.com>
2023-04-30 12:11:00 -0500

    update chezmoi daily (pwsh)

---
+++ dotfiles/private_dot_config/powershell/profile.d/50-upgrade-chezmoi.ps1
@@ -0,0 +1,12 @@
+function Upgrade-Chezmoi {
+    $local:last_chezmoi_upgrade="$HOME/.cache/dotfiles/last-chezmoi-upgrade"
+    $local:yesterday = ((Get-Date).Date - (New-TimeSpan -Days 1))
+
+    if (! (Test-Path $last_chezmoi_upgrade -NewerThan $yesterday)) {
+        chezmoi upgrade `
+         && New-Item -Force -Type Directory -Path (Split-Path $last_chezmoi_upgrade) | Out-Null `
+         && Set-Content -Force -Path $last_chezmoi_upgrade -Value ""
+    }
+}
+
+Upgrade-Chezmoi
