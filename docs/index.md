5b3651aa6edbfe0f1a293e34cb9768f70a5ebb33
Chris Pawling <chris-468@outlook.com>
2023-05-05 15:33:05 -0500

    pull yadm removal out to separate script

--- docs/index.md
+++ docs/index.md
@@ -19,3 +19,13 @@ sh -c "$(curl -fsLS chris468.github.io/dotfiles/install.sh)"
 ```
 (Invoke-RestMethod -UseBasicParsing https://chris468.github.io/dotfiles/install.ps1) | pwsh -c -
 ```
+
+
+# Back up and remove up previous yadm files
+
+```
+curl -fsLS https://chris468.github.io/dotfiles/remove-yadm-files.sh | sh -
+```
+
+Moves any files managed by yadm to ~/.local/share/dotfiles.bkp, and renames
+the yadm repo folder in the same directory. Requires git to be installed.
--- docs/remove-yadm-files.sh
+++ docs/remove-yadm-files.sh
@@ -1,4 +1,3 @@
-{{- if lookPath "git" -}}
 #!/usr/bin/env bash
 
 set -e
@@ -7,6 +6,13 @@ set -o pipefail
 YADM_ROOT="$HOME/.local/share/yadm"
 YADM_REPO="$YADM_ROOT/repo.git"
 YADM_BACKUP="$YADM_ROOT/dotfiles.bkp"
+
+if ! command -v git &> /dev/null
+then
+  echo "Git not found, cannot clean yadm files." >&2
+  exit 1
+fi
+
 if [ -d "$YADM_REPO" ]
 then
   echo "Moving yadm dotfiles to $YADM_BACKUP..."
@@ -21,6 +27,6 @@ then
     fi
   done
   mv "$YADM_REPO" "$YADM_REPO.bkp"
+else
+  echo "$YADM_REPO not found, skipping yadm cleanup"
 fi
-
-{{- end -}}
