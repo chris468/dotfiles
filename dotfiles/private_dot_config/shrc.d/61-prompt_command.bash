fae04eff0d7cfa1af2211733103d9df947f17c70
Chris Pawling <chris-468@outlook.com>
2023-05-14 21:16:25 -0500

    better name - shrc.d

--- dotfiles/private_dot_config/shrc.d/61-prompt_command.bash
+++ dotfiles/private_dot_config/shrc.d/61-prompt_command.bash
@@ -1,6 +1,6 @@
 function _prompt_commands {
     ret=$?
-    for c in $(find ~/.config/shellrc.d/prompt_command.d -maxdepth 1 -type f -name '*.bash')
+    for c in $(find ~/.config/shrc.d/prompt_command.d -maxdepth 1 -type f -name '*.bash')
     do
         . $c
     done
