1b9ef7e4dd5e25e58cb1fa1adfa74d8ecd781285
Chris Pawling <chris-468@outlook.com>
2023-05-19 18:39:24 -0500

    pwsh: load completions

--- dotfiles/private_dot_config/powershell/profile.ps1
+++ dotfiles/private_dot_config/powershell/profile.ps1
@@ -3,3 +3,7 @@ $config_dir = "$HOME/.config"
 foreach ($script in Get-ChildItem $config_dir/powershell/profile.d -Filter '*.ps1') {
     . $script
 }
+
+foreach ($script in Get-ChildItem $config_dir/powershell/completion.d -Filter '*.ps1') {
+    . $script
+}
