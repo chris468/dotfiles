246c524af0e0f7e12da1083d7bae0719d52ce6f1
Chris Pawling <chris-468@outlook.com>
2023-06-04 12:01:10 -0500

    install git w/ winget

--- docs/install.ps1
+++ docs/install.ps1
@@ -1,12 +1,59 @@
-$env:SCOOP="$HOME/.local/opt/scoop"
-if (!(Get-Command scoop 2>$null)) {
-  Write-Host "Installing scoop..."
-  Set-ExecutionPolicy RemoteSigned -Scope CurrentUser
-  Invoke-RestMethod https://get.scoop.sh | Invoke-Expression
+function Install-Git {
+  function Create-Inf {
+    $inf = New-TemporaryFile
+    Set-Content -Path "$inf" -Value @"
+[Setup]
+Lang=default
+Dir=C:\Program Files\Git
+Group=Git
+NoIcons=0
+SetupType=default
+Components=ext,ext\shellhere,ext\guihere,gitlfs,assoc,assoc_sh,autoupdate,windowsterminal,scalar
+Tasks=
+EditorOption=VIM
+CustomEditorPath=
+DefaultBranchOption=main
+PathOption=Cmd
+SSHOption=OpenSSH
+TortoiseOption=false
+CURLOption=OpenSSL
+CRLFOption=CRLFAlways
+BashTerminalOption=ConHost
+GitPullBehaviorOption=FFOnly
+UseCredentialManager=Enabled
+PerformanceTweaksFSCache=Enabled
+EnableSymlinks=Enabled
+EnablePseudoConsoleSupport=Disabled
+EnableFSMonitor=Disabled
+
+"@
+    $inf
+  }
+
+  $git_install_status=(winget list Git.Git)
+  if ($?) {
+      $git_install_status
+  } else {
+    $inf = Create-Inf
+    winget install Git.Git --override "/LoadInf=$inf /Silent"
+  }
 }
 
-Write-Host "Installing git..."
-& $env:SCOOP\shims\scoop.ps1 install git
+function Install-Scoop {
+  $env:SCOOP="$HOME/.local/opt/scoop"
+  if (!(Get-Command scoop 2>$null)) {
+    Write-Host "Installing scoop..."
+    Set-ExecutionPolicy RemoteSigned -Scope CurrentUser
+    Invoke-RestMethod https://get.scoop.sh | Invoke-Expression
+  }
+}
 
-'$params="-BinDir `"$HOME\.local\bin`" init --apply chris468 $params"', `
-  (Invoke-RestMethod -UseBasicParsing https://get.chezmoi.io/ps1) | pwsh -c -
+function Install-Dotfiles {
+  '$params="-BinDir `"$HOME\.local\bin`" init --apply chris468 $params"', `
+    (Invoke-RestMethod -UseBasicParsing https://get.chezmoi.io/ps1) | pwsh -c -
+}
+
+
+Install-Git
+#Install-Scoop
+#Install-Dotfiles
