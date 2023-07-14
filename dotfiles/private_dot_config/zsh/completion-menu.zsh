694e6fd1ba86ef2f82c61190bacd14bc32dabbbe
Chris Pawling <chris@airbook.home.arpa>
2023-07-13 19:56:18 -0500

    fixes for mac

--- dotfiles/private_dot_config/zsh/completion-menu.zsh
+++ dotfiles/private_dot_config/zsh/completion-menu.zsh
@@ -4,7 +4,9 @@ zstyle ':completion:*:*:*:*:corrections' format '%B%F{yellow}%d%f%b'
 zstyle ':completion:*:*:*:*:descriptions' format '%B%F{blue}%d%f%b'
 zstyle ':completion:*' group-name ''
 zstyle ':completion:*' menu select=2
-eval "$(dircolors -b)"
+if command -v dircolors >/dev/null ; then
+    eval "$(dircolors -b)"
+fi
 zstyle ':completion:*:default' list-colors ${(s.:.)LS_COLORS}
 zstyle ':completion:*' list-colors ''
 zstyle ':completion:*' list-prompt %SAt %p: Hit TAB for more, or the character to insert%s
