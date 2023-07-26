6324deac9544ee86768df454e4e22d818a703b70
Chris Pawling <chris-468@outlook.com>
2023-07-26 18:55:31 -0500

    some more basic configuration

--- dotfiles/private_dot_config/nvim/init.lua
+++ dotfiles/private_dot_config/nvim/init.lua
@@ -8,4 +8,11 @@ vim.o.backup = false
 vim.o.number = true
 vim.o.relativenumber = true
 
+vim.o.expandtab = true
+vim.o.tabstop = 2
+vim.o.shiftwidth = 2
+vim.o.autoindent = true
+vim.o.wrap = false
+vim.o.signcolumn = "yes"
+
 require "chris468.load-plugins"
