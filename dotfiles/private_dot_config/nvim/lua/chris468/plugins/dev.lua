e13b8eeff75b92ca3f3ea89bb4c48b040870c070
Chris Pawling <chris-468@outlook.com>
2023-07-26 18:57:52 -0500

    add luapad plugin

--- dotfiles/private_dot_config/nvim/lua/chris468/plugins/dev.lua
+++ dotfiles/private_dot_config/nvim/lua/chris468/plugins/dev.lua
@@ -1,14 +1,17 @@
 return {
-  "williamboman/mason.nvim",
-  tag = "stable",
-  build = ":MasonUpdate",
-  config = function (_) require("mason").setup() end,
-  cmd = {
-    "Mason",
-    "MasonUpdate",
-    "MasonInstall",
-    "MasonUninstall",
-    "MasonUninstallAll",
-    "MasonLog",
+  { 'rafcamlet/nvim-luapad' },
+  {
+    "williamboman/mason.nvim",
+    tag = "stable",
+    build = ":MasonUpdate",
+    config = function (_) require("mason").setup() end,
+    cmd = {
+      "Mason",
+      "MasonUpdate",
+      "MasonInstall",
+      "MasonUninstall",
+      "MasonUninstallAll",
+      "MasonLog",
+    },
   },
 }
