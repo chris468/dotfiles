96b0cec0c1de5b2b442c2913fa6767f13a3ca30c
Chris Pawling <chris-468@outlook.com>
2023-07-26 20:04:14 -0500

    start on completion support

--- dotfiles/private_dot_config/nvim/lua/chris468/config/lsp.lua
+++ dotfiles/private_dot_config/nvim/lua/chris468/config/lsp.lua
@@ -1,5 +1,10 @@
 local lspconfig = require("lspconfig")
+local cmp_nvim_lsp = require("cmp_nvim_lsp")
+
+local capabilities = cmp_nvim_lsp.default_capabilities()
+
 lspconfig.lua_ls.setup {
+  capabilities = capabilities,
   settings = {
     Lua = {
       diagnostics = {
--- dotfiles/private_dot_config/nvim/lua/chris468/plugins/dev.lua
+++ dotfiles/private_dot_config/nvim/lua/chris468/plugins/dev.lua
@@ -28,4 +28,54 @@ return {
     end,
   },
   { "neovim/nvim-lspconfig" },
+  { "hrsh7th/nvim-cmp" },
+  { "hrsh7th/cmp-buffer" },
+  { "hrsh7th/cmp-cmdline" },
+  { "hrsh7th/cmp-path" },
+  {
+    "hrsh7th/cmp-nvim-lsp",
+    config = function()
+      local cmp = require("cmp")
+      cmp.setup({
+        snippet = {
+          expand = function(args)
+            require('luasnip').lsp_expand(args.body)
+          end,
+        },
+        mapping = cmp.mapping.preset.insert({
+          ['<C-d>'] = cmp.mapping.scroll_docs(-4),
+          ['<C-f>'] = cmp.mapping.scroll_docs(4),
+          ['<C-Space>'] = cmp.mapping.complete(),
+          ['<CR>'] = cmp.mapping.confirm({ select = true }),
+        }),
+        sources = cmp.config.sources({
+          { name = 'nvim_lsp' },
+          { name = 'luasnip' },
+          { name = 'buffer' },
+          { name = 'path' },
+        }),
+      })
+
+      cmp.setup.cmdline('/', {
+        mapping = cmp.mapping.preset.cmdline(),
+        sources = {
+          { name = 'buffer' }
+        }
+      })
+
+      cmp.setup.cmdline(':', {
+        mapping = cmp.mapping.preset.cmdline(),
+        sources = cmp.config.sources(
+        {
+          { name = 'path' }
+        },
+        {
+          { name = 'cmdline' }
+        })
+      })
+    end,
+  },
+  { "L3MON4D3/LuaSnip" },
+  { "saadparwaiz1/cmp_luasnip" },
+  { "rafamadriz/friendly-snippets" },
 }
