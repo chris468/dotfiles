6a1d65646800a618ad9540193079c4329fd22b97
Chris Pawling <chris-468@outlook.com>
2023-07-26 21:49:10 -0500

    add treesitter

--- dotfiles/private_dot_config/nvim/lua/chris468/plugins/dev.lua
+++ dotfiles/private_dot_config/nvim/lua/chris468/plugins/dev.lua
@@ -106,4 +106,18 @@ return {
 			})
 		end,
 	},
+	{
+		"nvim-treesitter/nvim-treesitter",
+		build = ":TSUpdate",
+		config = function()
+			require("nvim-treesitter.configs").setup({
+				ensure_installed = {
+					"lua",
+				},
+				sync_install = false,
+				auto_install = true,
+				highlight = { enable = true },
+			})
+		end,
+	},
 }
