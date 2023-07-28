11029f75145cb70945d4bfe97472ca78078f3613
Chris Pawling <chris-468@outlook.com>
2023-07-28 17:20:57 -0500

    lsp cleanup towards separat config

--- dotfiles/private_dot_config/nvim/lua/chris468/config/lsp.lua
+++ dotfiles/private_dot_config/nvim/lua/chris468/config/lsp.lua
@@ -1,22 +1,14 @@
-local lspconfig = require("lspconfig")
-local cmp_nvim_lsp = require("cmp_nvim_lsp")
-
-local capabilities = cmp_nvim_lsp.default_capabilities()
-
-lspconfig.lua_ls.setup({
-	capabilities = capabilities,
-	settings = {
-		Lua = {
-			workspace = {
-				checkThirdParty = false,
+return function(capabilities)
+	return {
+		lua_ls = {
+			capabilities = capabilities,
+			settings = {
+				Lua = {
+					workspace = {
+						checkThirdParty = false,
+					},
+				},
 			},
 		},
-	},
-})
-
-vim.api.nvim_create_autocmd("BufWritePre", {
-	buffer = buffer,
-	callback = function()
-		vim.lsp.buf.format({ async = false })
-	end,
-})
+	}
+end
