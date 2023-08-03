3cfee5541bdcdca0f6354f3ec1732f9eb2ac6004
Chris Pawling <chris-468@outlook.com>
2023-08-03 18:34:28 -0500

    default tabs to 4 spaces, override for specific filetypes

---
+++ dotfiles/private_dot_config/nvim/lua/config/autocmds.lua
@@ -0,0 +1,30 @@
+vim.api.nvim_create_autocmd("FileType", {
+  group = vim.api.nvim_create_augroup("chris468_tabsize", {}),
+  callback = function(args)
+    local tabs = require("chris468.config.tabs")
+    local bo = vim.bo[args.buf]
+    local ft = bo.filetype
+    local setting = tabs[ft]
+
+    if type(setting) == "number" then
+      bo.shiftwidth = setting
+      bo.tabstop = setting
+      bo.expandtab = true
+    elseif type(setting) == "table" then
+      if setting[1] then
+        bo.shiftwidth = setting[1]
+        bo.tabstop = setting[1]
+      end
+
+      if setting.shiftwidth then
+        bo.shiftwidth = setting.shiftwidth
+      end
+      if setting.tabstop then
+        bo.tabstop = setting.tabstop
+      end
+      if setting.expandtab ~= nil then
+        bo.expandtab = setting.expandtab
+      end
+    end
+  end,
+})
