4470d1d456cc57db3c7d50d460b29e33e7444559
Chris Pawling <chris-468@outlook.com>
2023-08-06 16:49:37 -0500

    Revert "nvim: call lazy bootstrap helper"
    This reverts commit 39d76148d479ee670e1c1a2543479cad266d21dd.

--- dotfiles/private_dot_config/nvim/init.lua
+++ dotfiles/private_dot_config/nvim/init.lua
@@ -1,7 +1,17 @@
-local lazy = require("lazy")
-lazy.bootstrap()
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
 
-lazy.setup({
+require("lazy").setup({
   spec = {
     { "LazyVim/LazyVim", import = "lazyvim.plugins" },
     { import = "lazyvim.plugins.extras.formatting.prettier" },
