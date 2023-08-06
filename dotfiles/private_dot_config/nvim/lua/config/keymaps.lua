4fd942c9ed61d6e01d201b6cbbba94d57c559f4f
Chris Pawling <chris-468@outlook.com>
2023-08-06 16:50:15 -0500

    nvim: fix comment

--- dotfiles/private_dot_config/nvim/lua/config/keymaps.lua
+++ dotfiles/private_dot_config/nvim/lua/config/keymaps.lua
@@ -2,5 +2,5 @@ vim.keymap.set("n", "<leader>h", "<cmd>noh<cr>", { desc = "No highlight" })
 vim.keymap.del({ "n", "i", "v" }, "<A-j>")
 vim.keymap.del({ "n", "i", "v" }, "<A-k>")
 
-# load here to be sure it overwrites the LazyVim keymaps
+-- load here to be sure it overwrites the LazyVim keymaps
 vim.cmd("Lazy load vim-tmux-navigator")
