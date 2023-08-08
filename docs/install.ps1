f9902e35cdaccf43ea687568983ffc91ce73c7fa
Chris Pawling <chris-468@outlook.com>
2023-08-08 18:42:26 -0500

    move scoop install to script, add essentials

--- docs/install.ps1
+++ docs/install.ps1
@@ -50,22 +50,11 @@ EnableFSMonitor=Disabled
   }
 }
 
-function Install-Scoop {
-  $env:SCOOP="$HOME/.local/opt/scoop"
-  if (!(Get-Command scoop 2>$null)) {
-    Write-Host "Installing scoop..."
-    Set-ExecutionPolicy RemoteSigned -Scope CurrentUser
-    Invoke-RestMethod https://get.scoop.sh | Invoke-Expression
-  }
-}
-
 function Install-Dotfiles {
   '$params="-BinDir `"$HOME\.local\bin`" init --apply chris468 $params"', `
     (Invoke-RestMethod -UseBasicParsing https://get.chezmoi.io/ps1) | pwsh -c -
 }
 
-
 Check-SymbolicLinks
 Install-Git
-Install-Scoop
 Install-Dotfiles
