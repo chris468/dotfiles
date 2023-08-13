eeab320a83f195f9f182dd5519272d95765d4014
Chris Pawling <chris-468@outlook.com>
2023-08-13 10:30:42 -0500

    nvim: more terminal configuration

--- dotfiles/private_dot_config/nvim/lua/config/keymaps.lua
+++ dotfiles/private_dot_config/nvim/lua/config/keymaps.lua
@@ -1,7 +1,33 @@
-vim.keymap.set("n", "<leader>h", "<cmd>noh<cr>", { desc = "No highlight" })
-vim.keymap.del({ "n", "i", "v" }, "<A-j>")
-vim.keymap.del({ "n", "i", "v" }, "<A-k>")
-vim.keymap.del("n", "<leader>ft")
-vim.keymap.del("n", "<leader>fT")
-vim.keymap.del({ "n", "t" }, "<C-/>")
-vim.keymap.del({ "n", "t" }, "<C-_>")
+local function map_hide_highlight()
+  vim.keymap.set("n", "<leader>h", "<cmd>noh<cr>", { desc = "No highlight" })
+end
+
+local function map_terminals() end
+
+local function clear_line_movement()
+  -- was accidentally triggering when leaving insert mode and trying to move
+  vim.keymap.del({ "n", "i", "v" }, "<A-j>")
+  vim.keymap.del({ "n", "i", "v" }, "<A-k>")
+end
+
+local function clear_terminals()
+  -- replacing with toggleterm
+  vim.keymap.del("n", "<leader>ft")
+  vim.keymap.del("n", "<leader>fT")
+  vim.keymap.del({ "n", "t" }, "<C-/>")
+  vim.keymap.del({ "n", "t" }, "<C-_>")
+
+  -- don't unmap <leader>gg because it is reconfigured in toggleterm plugin configuration
+end
+
+local function clear()
+  clear_line_movement()
+  clear_terminals()
+end
+
+local function map()
+  map_hide_highlight()
+end
+
+clear()
+map()
--- dotfiles/private_dot_config/nvim/lua/plugins/toggleterm.lua
+++ dotfiles/private_dot_config/nvim/lua/plugins/toggleterm.lua
@@ -1,11 +1,68 @@
+local function create_terminal(opts)
+  local Terminal = require("toggleterm.terminal").Terminal
+  return Terminal:new(opts)
+end
+
+local function map_keys(map_normal)
+  map_normal = map_normal == nil or map_normal
+  local function unmap_window_navigation(bufnr)
+    for _, key in pairs({ "<C-h>", "<C-j>", "<C-k>", "<C-l>" }) do
+      vim.keymap.set({ "n", "i", "t" }, key, key, { buffer = bufnr })
+    end
+  end
+
+  local function unmap_normal(bufnr)
+    vim.keymap.set({ "n", "i", "t" }, "<Esc>", "<Esc>", { buffer = bufnr, nowait = true })
+  end
+
+  return function(terminal)
+    if terminal.direction == "float" then
+      unmap_window_navigation(terminal.bufnr)
+    end
+
+    if not map_normal then
+      unmap_normal(terminal.bufnr)
+    end
+  end
+end
+
+local function horizontal()
+  print("there")
+  return create_terminal({
+    count = 2,
+    direction = "horizontal",
+  })
+end
+
+local function lazygit()
+  print("hi")
+  return create_terminal({
+    hidden = true,
+    direction = "float",
+    cmd = "lazygit",
+    on_create = map_keys(false),
+  })
+end
+
 return {
   "akinsho/toggleterm.nvim",
   opts = {
     open_mapping = "<C-\\>",
+    direction = "float",
+    float_opts = {
+      border = "rounded",
+    },
+    on_create = map_keys(),
   },
+  -- stylua: ignore
   keys = {
     { "<C-\\>" },
+    { "<leader>ft", "<cmd>TermSelect<CR>", desc = { "Terminal" } },
+    { "<leader>tf", "<cmd>1ToggleTerm<cr>", { "n", "t" }, desc = "Floating terminal" },
+    { "<leader>tt", function() horizontal():toggle() end, { "n", "t" }, desc = "Horizontal terminal" },
+    { "<leader>gg", function() lazygit():toggle() end, { "n", "t" }, desc = "Git" },
   },
+  -- stylua: ignore end
   cmd = {
     "ToggleTerm",
     "ToggleTermToggleAll",
