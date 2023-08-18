4f65f9b2e5cbf41e327b256f3aece6d8ff78fc6f
Chris Pawling <chris.pawling@ni.com>
2023-08-18 08:29:25 -0500

    nvim: configure powershell

--- dotfiles/private_dot_config/nvim/lua/chris468/profiles/base/config/options.lua
+++ dotfiles/private_dot_config/nvim/lua/chris468/profiles/base/config/options.lua
@@ -7,3 +7,13 @@ local opt = vim.opt
 opt.tabstop = 4
 opt.shiftwidth = 4
 opt.conceallevel = 0
+
+if vim.fn.has("win32") == 1 then
+  opt.shell = vim.fn.executable("pwsh") == 1 and "pwsh" or "powershell"
+  opt.shellcmdflag =
+    "-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.Encoding]::UTF8;"
+  opt.shellredir = "-RedirectStandardOutput %s -NoNewWindow -Wait"
+  opt.shellpipe = "2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode"
+  opt.shellquote = ""
+  opt.shellxquote = ""
+end
