cea616253ef42718a457773ce597d4f9441f7442
Chris Pawling <chris-468@outlook.com>
2023-08-27 17:40:13 -0500

    nvim: complete on <tab> and commitCharacters

--- dotfiles/private_dot_config/nvim/lua/chris468/profiles/base/config/autocmds.lua
+++ dotfiles/private_dot_config/nvim/lua/chris468/profiles/base/config/autocmds.lua
@@ -32,3 +32,32 @@ vim.api.nvim_create_autocmd("FileType", {
     end
   end,
 })
+
+-- According to hrsh7th/nvim-cmp/issues/835, cmp intentionally does not select
+-- on completion via a commit character. Adapted this based on hrsh7th/nvim-cmp/discussions/1618.
+vim.api.nvim_create_autocmd("InsertEnter", {
+  group = vim.api.nvim_create_augroup("chris468_register_complete_on_commit_char", {}),
+  callback = function()
+    local cmp = require("cmp")
+    cmp.event:on("menu_opened", function()
+      vim.api.nvim_create_autocmd("InsertCharPre", {
+        group = vim.api.nvim_create_augroup("chris468_complete_on_commit_char", {}),
+        callback = function(_)
+          local entry = cmp.get_selected_entry() or {}
+          local completion_item = entry.completion_item or {}
+          local commit_chars = completion_item.commitCharacters or {}
+          local c = vim.v.char
+          if vim.tbl_contains(commit_chars, c) then
+            vim.v.char = ""
+            vim.schedule(function()
+              cmp.confirm()
+              vim.api.nvim_feedkeys(c, "n", false)
+            end)
+          end
+        end,
+        once = true,
+      })
+    end)
+  end,
+  once = true,
+})
--- dotfiles/private_dot_config/nvim/lua/chris468/profiles/base/plugins/cmp.lua
+++ dotfiles/private_dot_config/nvim/lua/chris468/profiles/base/plugins/cmp.lua
@@ -1,10 +1,44 @@
 return {
-  "nvim-cmp",
-  event = "CmdlineEnter",
-  opts = function(_, opts)
-    local cmp = require("cmp")
-    local mapping = opts.mapping
-    mapping["<C-k>"] = cmp.mapping(opts.mapping["<C-P>"], { "i", "c" })
-    mapping["<C-j>"] = cmp.mapping(opts.mapping["<C-N>"], { "i", "c" })
-  end,
+  {
+    "L3MON4D3/LuaSnip",
+    keys = function(_, keys)
+      local updated = {}
+      for _, k in ipairs(keys) do
+        if string.lower(k[1]) ~= "<tab>" then
+          updated[#updated + 1] = k
+        end
+      end
+
+      return updated
+    end,
+  },
+  {
+    "hrsh7th/nvim-cmp",
+    dependencies = {
+      "hrsh7th/cmp-emoji",
+      "L3MON4D3/LuaSnip",
+    },
+    event = "CmdlineEnter",
+    opts = function(_, opts)
+      local cmp = require("cmp")
+      local luasnip = require("luasnip")
+
+      local mapping = opts.mapping
+      opts.mapping = vim.tbl_extend("force", mapping, {
+        ["<C-k>"] = cmp.mapping(mapping["<C-P>"], { "i", "c" }),
+        ["<C-j>"] = cmp.mapping(mapping["<C-N>"], { "i", "c" }),
+        ["<Tab>"] = cmp.mapping(function(fallback)
+          if cmp.visible() then
+            cmp.confirm()
+          -- You could replace the expand_or_jumpable() calls with expand_or_locally_jumpable()
+          -- this way you will only jump inside the snippet region
+          elseif luasnip.expand_or_jumpable() then
+            luasnip.expand_or_jump()
+          else
+            fallback()
+          end
+        end, { "i", "s" }),
+      })
+    end,
+  },
 }
