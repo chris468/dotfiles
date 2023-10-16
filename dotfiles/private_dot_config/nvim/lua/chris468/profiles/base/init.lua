7dceadb34a6c7b0cf39a9c8ef272103ec2996016
Chris Pawling <chris-468@outlook.com>
2023-10-16 18:59:59 -0500

    update lazyvim to 10.4.1

--- dotfiles/private_dot_config/nvim/lua/chris468/profiles/base/init.lua
+++ dotfiles/private_dot_config/nvim/lua/chris468/profiles/base/init.lua
@@ -3,6 +3,7 @@ return {
     "LazyVim/LazyVim",
     opts = { colorscheme = "nord" },
   },
+  { import = "lazyvim.plugins.extras.formatting.black" },
   { import = "lazyvim.plugins.extras.formatting.prettier" },
   { import = "lazyvim.plugins.extras.lang.docker" },
   { import = "lazyvim.plugins.extras.lang.json" },
