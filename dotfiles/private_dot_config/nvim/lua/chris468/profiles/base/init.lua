af4982d48ae548a30ff41b0bc90be66f3a8d283d
Chris Pawling <chris-468@outlook.com>
2023-12-26 09:21:11 -0600

    nvim: add go

--- dotfiles/private_dot_config/nvim/lua/chris468/profiles/base/init.lua
+++ dotfiles/private_dot_config/nvim/lua/chris468/profiles/base/init.lua
@@ -6,6 +6,7 @@ return {
   { import = "lazyvim.plugins.extras.formatting.black" },
   { import = "lazyvim.plugins.extras.formatting.prettier" },
   { import = "lazyvim.plugins.extras.lang.docker" },
+  { import = "lazyvim.plugins.extras.lang.go" },
   { import = "lazyvim.plugins.extras.lang.json" },
   { import = "lazyvim.plugins.extras.lang.python" },
   { import = "lazyvim.plugins.extras.lang.terraform" },
