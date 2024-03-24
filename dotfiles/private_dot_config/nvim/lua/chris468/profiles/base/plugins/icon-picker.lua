53c7d64a74e4b7f1482707758d5ee8d2fa811e40
Chris Pawling <chris-468@outlook.com>
2024-03-24 13:01:10 -0500

    nvim: icon picker use current nerd font icons

--- dotfiles/private_dot_config/nvim/lua/chris468/profiles/base/plugins/icon-picker.lua
+++ dotfiles/private_dot_config/nvim/lua/chris468/profiles/base/plugins/icon-picker.lua
@@ -1,9 +1,10 @@
+local icons = "nerd_font_v3 symbols alt_font emoji html_colors"
 return {
   {
     "ziontee113/icon-picker.nvim",
     keys = {
-      { "<leader>ii", "<cmd>IconPickerNormal<CR>", mode = "n", desc = "Insert icon" },
-      { "<leader>iy", "<cmd>IconPickerYank<CR>", mode = "n", desc = "Yank icon" },
+      { "<leader>ii", "<cmd>IconPickerNormal " .. icons .. "<CR>", mode = "n", desc = "Insert icon" },
+      { "<leader>iy", "<cmd>IconPickerYank " .. icons .. "<CR>", mode = "n", desc = "Yank icon" },
     },
     cmd = {
       "IconPickerNormal",
