07d9865420dd6042b6ab2a1a066685ba1314821d
Chris Pawling <chris.pawling@ni.com>
2024-04-01 18:16:56 +0000

    oh-my-posh: environment variable for a simple prompt

--- dotfiles/private_dot_config/zsh/oh-my-posh.zsh
+++ dotfiles/private_dot_config/zsh/oh-my-posh.zsh
@@ -1,5 +1,9 @@
-eval "$(oh-my-posh init zsh)"
+if [ -z "$SIMPLE_PROMPT" ] ; then
+    eval "$(oh-my-posh init zsh)"
 
-function set_poshcontext {
-    [ "$COLUMNS" -ge 90 ] && export POSH_WIDE=1 || export POSH_WIDE=0
-}
+    function set_poshcontext {
+        [ "$COLUMNS" -ge 90 ] && export POSH_WIDE=1 || export POSH_WIDE=0
+    }
+else
+    PS1="%% "
+fi
