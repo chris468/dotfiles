870a115b958b8b928ecedc3de0145b3bcbc923b1
Chris Pawling <chris-468@outlook.com>
2024-05-06 19:07:44 -0500

    allow multiple fallbacks

--- dotfiles/private_dot_config/tmux/theme/scripts/util.sh
+++ dotfiles/private_dot_config/tmux/theme/scripts/util.sh
@@ -1,20 +1,15 @@
-function check_arg {
-	local arg="$1"
-	local opt="$2"
-
-	[[ "$opt" != -* ]] || (
-		echo "missing value for '$arg'" >&2
-		return 1
-	)
-}
-
 function get_option {
 	local option=$1
-	local fallback=$2
+	shift
 
-	local result=$(tmux show-option -gqv "$option")
-	if [[ -z "$result" ]] && [[ -n "$fallback" ]]; then
-		result=$(tmux show-option -gqv "$fallback")
+	local result
+	if [[ "$option" == @* ]]; then
+		result=$(tmux show-option -gqv "$option")
+		if [[ -z "$result" ]]; then
+			result=$(get_option "$@")
+		fi
+	else
+		result="$option"
 	fi
 
 	echo "$result"
