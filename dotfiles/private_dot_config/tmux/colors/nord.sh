e868e3d3b30b25ec0bd3ac4917d648db4419abcc
Chris Pawling <chris-468@outlook.com>
2024-05-07 20:11:52 -0500

    pull colors into nord script

--- dotfiles/private_dot_config/tmux/colors/nord.sh
+++ dotfiles/private_dot_config/tmux/colors/nord.sh
@@ -1,52 +1,99 @@
-nord0="#2e3440"
-nord1="#3b4252"
-nord2="#434c5e"
-nord3="#4c566a"
-nord4="#d8dee9"
-nord5="#e5e9f0"
-nord6="#eceff4"
-nord7="#8fbcbb"
-nord8="#88c0d0"
-nord9="#81a1c1"
-nord10="#5e81ac"
-nord11="#bf616a"
-nord12="#d08770"
-nord13="#ebcb8b"
-nord14="#a3be8c"
-nord15="#b48ead"
+function nord_colors {
+	nord0="#2e3440"
+	nord1="#3b4252"
+	nord2="#434c5e"
+	nord3="#4c566a"
+	nord4="#d8dee9"
+	nord5="#e5e9f0"
+	nord6="#eceff4"
+	nord7="#8fbcbb"
+	nord8="#88c0d0"
+	nord9="#81a1c1"
+	nord10="#5e81ac"
+	nord11="#bf616a"
+	nord12="#d08770"
+	nord13="#ebcb8b"
+	nord14="#a3be8c"
+	nord15="#b48ead"
 
-status_foreground=$nord4
-status_background=$nord1
+	status_foreground=$nord4
+	status_background=$nord1
 
-status_segment_foreground=$nord8
-status_segment_background=$nord3
-status_outer_foreground=$nord0
-status_outer_background=$nord8
-status_outer_suspended_background=$nord3
+	status_segment_foreground=$nord8
+	status_segment_background=$nord3
+	status_outer_foreground=$nord0
+	status_outer_background=$nord8
+	status_outer_suspended_background=$nord3
 
-status_outer_prefix_foreground=$nord0
-status_outer_prefix_background=$nord4
+	status_outer_prefix_foreground=$nord0
+	status_outer_prefix_background=$nord4
 
-window_foreground=$nord8
-window_background=$nord3
-window_current_foreground=$nord4
-window_current_background=$nord10
-window_current_suspended_background=$nord3
+	window_foreground=$nord8
+	window_background=$nord3
+	window_current_foreground=$nord4
+	window_current_background=$nord10
+	window_current_suspended_background=$nord3
 
-display_panes_color=$nord3
-display_panes_active_color=$nord10
+	display_panes_color=$nord3
+	display_panes_active_color=$nord10
 
-message_style_foreground=$nord8
-message_style_background=$nord0
-message_command_style_foreground=$nord10
-message_command_style_background=$nord0
+	message_style_foreground=$nord8
+	message_style_background=$nord0
+	message_command_style_foreground=$nord10
+	message_command_style_background=$nord0
 
-copy_mode_match_style_foreground=$nord0
-copy_mode_match_style_background=$nord10
-copy_mode_current_match_style_foreground=$nord0
-copy_mode_current_match_style_background=$nord8
-copy_mode_mark_style_foreground=$nord0
-copy_mode_mark_style_background=$nord8
+	copy_mode_match_style_foreground=$nord0
+	copy_mode_match_style_background=$nord10
+	copy_mode_current_match_style_foreground=$nord0
+	copy_mode_current_match_style_background=$nord8
+	copy_mode_mark_style_foreground=$nord0
+	copy_mode_mark_style_background=$nord8
 
-mode_style_foreground=$nord0
-mode_style_background=$nord10
+	mode_style_foreground=$nord0
+	mode_style_background=$nord10
+
+	tmux set -g @theme468-status-foreground "$status_foreground"
+	tmux set -g @theme468-status-background "$status_background"
+	tmux set -g @theme468-mode-foreground "$mode_foreground"
+	tmux set -g @theme468-mode-background "$mode_background"
+
+	tmux set -g @theme468-window-foreground "$window_foreground"
+	tmux set -g @theme468-window-background "$window_background"
+	tmux set -g @theme468-window-current-foreground "$window_current_foreground"
+	tmux set -g @theme468-window-current-background \
+		"$(dim_when_suspended $window_current_background $window_current_suspended_background)"
+
+	tmux set -g @theme468-segment-session-foreground "#{?client_prefix,$status_outer_prefix_foreground,$status_outer_foreground}"
+	tmux set -g @theme468-segment-session-background \
+		"#{?client_prefix,$status_outer_prefix_background,$(dim_when_suspended $status_outer_background $status_outer_suspended_background)}"
+
+	tmux set -g @theme468-segment-host-foreground "#{?client_prefix,$status_outer_prefix_foreground,$status_outer_foreground}"
+	tmux set -g @theme468-segment-host-background \
+		"#{?client_prefix,$status_outer_prefix_background,$(dim_when_suspended $status_outer_background $status_outer_suspended_background)}"
+
+	tmux set -g @theme468-segment-date "#($modules_path/date.tmux) "
+	tmux set -g @theme468-segment-date-icon " 󰃰 "
+	tmux set -g @theme468-segment-date-foreground "$status_segment_foreground"
+	tmux set -g @theme468-segment-date-background "$status_segment_background"
+
+	tmux set -g @theme468-message-style-foreground "$message_style_foreground"
+	tmux set -g @theme468-message-style-background "$message_style_background"
+	tmux set -g @theme468-message-command-style-foreground "$message_command_style_foreground"
+	tmux set -g @theme468-message-command-style-background "$message_command_style_background"
+	tmux set -g @theme468-copy-mode-match-style-foreground "$copy_mode_match_style_foreground"
+	tmux set -g @theme468-copy-mode-match-style-background "$copy_mode_match_style_background"
+	tmux set -g @theme468-copy-mode-current-match-style-foreground "$copy_mode_current_match_style_foreground"
+	tmux set -g @theme468-copy-mode-current-match-style-background "$copy_mode_current_match_style_background"
+	tmux set -g @theme468-copy-mode-mark-style-foreground "$copy_mode_mark_style_foreground"
+	tmux set -g @theme468-copy-mode-mark-style-background "$copy_mode_mark_style_background"
+
+	tmux set -g @theme468-display-panes-color "$display_panes_color"
+	tmux set -g @theme468-display-panes-active-color "$display_panes_active_color"
+	tmux set -g @theme468-pane-border-foreground "$window_background"
+	tmux set -g @theme468-pane-border-background "default"
+	tmux set -g @theme468-pane-active-border-foreground \
+		"$(dim_when_suspended $window_current_background $window_current_suspended_background)"
+	tmux set -g @theme468-pane-active-border-background "default"
+
+	unset -f nord_colors
+} && nord_colors
--- dotfiles/private_dot_config/tmux/executable_theme.tmux
+++ dotfiles/private_dot_config/tmux/executable_theme.tmux
@@ -5,7 +5,6 @@ modules_path="$script_path/modules"
 colors_path="$script_path/colors"
 
 theme=$(tmux show-option -gv @chris468-theme)
-source "$colors_path/$theme.sh"
 
 function dim_when_suspended() {
 	default="$1"
@@ -14,10 +13,22 @@ function dim_when_suspended() {
 	echo "#{?@theme468-suspended,$suspended,$default}"
 }
 
+tmux set -g @theme468-window "#I#W "
+tmux set -g @theme468-segment-session "#S"
+tmux set -g @theme468-segment-session-icon " "
+tmux set -g @theme468-segment-session-attr "bold"
+
+tmux set -g @theme468-segment-host "#H"
+tmux set -g @theme468-segment-host-icon " 󰒋 "
+tmux set -g @theme468-segment-host-attr "bold"
+
 tmux set -g status-interval 1
 tmux set -g default-terminal xterm-256color
 tmux set -sa terminal-overrides ",xterm*:Tc"
 tmux set -g display-time 4000
+
+tmux set -g @theme468-status-left-modules "session"
+tmux set -g @theme468-status-right-modules "date host"
 
 tmux set -g @theme468-status-left-separator-outer "█"
 tmux set -g @theme468-status-left-separator-left ""
@@ -28,56 +39,4 @@ tmux set -g @theme468-status-right-separator-right ""
 tmux set -g @theme468-window-separator-left ""
 tmux set -g @theme468-window-separator-right ""
 
-tmux set -g @theme468-status-foreground "$status_foreground"
-tmux set -g @theme468-status-background "$status_background"
-tmux set -g @theme468-mode-foreground "$mode_foreground"
-tmux set -g @theme468-mode-background "$mode_background"
-
-tmux set -g @theme468-window "#I#W "
-tmux set -g @theme468-window-foreground "$window_foreground"
-tmux set -g @theme468-window-background "$window_background"
-tmux set -g @theme468-window-current-foreground "$window_current_foreground"
-tmux set -g @theme468-window-current-background \
-	"$(dim_when_suspended $window_current_background $window_current_suspended_background)"
-
-tmux set -g @theme468-segment-session "#S"
-tmux set -g @theme468-segment-session-icon " "
-tmux set -g @theme468-segment-session-foreground "#{?client_prefix,$status_outer_prefix_foreground,$status_outer_foreground}"
-tmux set -g @theme468-segment-session-background \
-	"#{?client_prefix,$status_outer_prefix_background,$(dim_when_suspended $status_outer_background $status_outer_suspended_background)}"
-tmux set -g @theme468-segment-session-attr "bold"
-
-tmux set -g @theme468-segment-host "#H"
-tmux set -g @theme468-segment-host-icon " 󰒋 "
-tmux set -g @theme468-segment-host-foreground "#{?client_prefix,$status_outer_prefix_foreground,$status_outer_foreground}"
-tmux set -g @theme468-segment-host-background \
-	"#{?client_prefix,$status_outer_prefix_background,$(dim_when_suspended $status_outer_background $status_outer_suspended_background)}"
-
-tmux set -g @theme468-segment-host-attr "bold"
-
-tmux set -g @theme468-segment-date "#($modules_path/date.tmux) "
-tmux set -g @theme468-segment-date-icon " 󰃰 "
-tmux set -g @theme468-segment-date-foreground "$status_segment_foreground"
-tmux set -g @theme468-segment-date-background "$status_segment_background"
-
-tmux set -g @theme468-status-left-modules "session"
-tmux set -g @theme468-status-right-modules "date host"
-
-tmux set -g @theme468-message-style-foreground "$message_style_foreground"
-tmux set -g @theme468-message-style-background "$message_style_background"
-tmux set -g @theme468-message-command-style-foreground "$message_command_style_foreground"
-tmux set -g @theme468-message-command-style-background "$message_command_style_background"
-tmux set -g @theme468-copy-mode-match-style-foreground "$copy_mode_match_style_foreground"
-tmux set -g @theme468-copy-mode-match-style-background "$copy_mode_match_style_background"
-tmux set -g @theme468-copy-mode-current-match-style-foreground "$copy_mode_current_match_style_foreground"
-tmux set -g @theme468-copy-mode-current-match-style-background "$copy_mode_current_match_style_background"
-tmux set -g @theme468-copy-mode-mark-style-foreground "$copy_mode_mark_style_foreground"
-tmux set -g @theme468-copy-mode-mark-style-background "$copy_mode_mark_style_background"
-
-tmux set -g @theme468-display-panes-color "$display_panes_color"
-tmux set -g @theme468-display-panes-active-color "$display_panes_active_color"
-tmux set -g @theme468-pane-border-foreground "$window_background"
-tmux set -g @theme468-pane-border-background "default"
-tmux set -g @theme468-pane-active-border-foreground \
-	"$(dim_when_suspended $window_current_background $window_current_suspended_background)"
-tmux set -g @theme468-pane-active-border-background "default"
+source "$colors_path/$theme.sh"
