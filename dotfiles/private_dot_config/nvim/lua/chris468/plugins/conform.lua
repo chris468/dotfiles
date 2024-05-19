1e1773f7c0ceb084e443d751ad81fbedfd0362df
Chris Pawling <chris-468@outlook.com>
2024-05-19 12:11:48 -0500

    formatting and lintin

---
+++ dotfiles/private_dot_config/nvim/lua/chris468/plugins/conform.lua
@@ -0,0 +1,33 @@
+local function format()
+  local conform = require("conform")
+  conform.format({ lsp_fallback = true, timeout_ms = 1500 })
+end
+
+local formatters_by_ft = {
+  lua = { "stylua" },
+  python = { "black" },
+  javascript = { { "prettierd", "prettier" } },
+}
+
+return {
+  "stevearc/conform.nvim",
+  dependencies = {
+    {
+      "williamboman/mason.nvim",
+      opts = {
+        install_for_filetype = { formatter = formatters_by_ft },
+      },
+    },
+  },
+  keys = {
+    { "<leader>cf", format, desc = "Format" },
+    { "<leader>cif", "<cmd>ConformInfo<cr>", desc = "Formatter" },
+  },
+  opts = {
+    formatters_by_ft = formatters_by_ft,
+    format_on_save = {
+      lsp_fallback = true,
+      timeout_ms = 500,
+    },
+  },
+}
