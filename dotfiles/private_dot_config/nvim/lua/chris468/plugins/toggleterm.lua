3f434d8b0ce558477944db72781e278f33ac565d
Chris Pawling <chris-468@outlook.com>
2024-05-19 14:45:36 -0500

    default terminal, float/horizontal/vertical

--- dotfiles/private_dot_config/nvim/lua/chris468/plugins/toggleterm.lua
+++ dotfiles/private_dot_config/nvim/lua/chris468/plugins/toggleterm.lua
@@ -1,3 +1,20 @@
+local function size(term)
+  if term.direction == "horizontal" then
+    return vim.o.lines * 0.25
+  elseif term.direction == "vertical" then
+    return vim.o.columns * 0.4
+  end
+end
+
+local function default(direction)
+  local function toggle()
+    local tt = require("toggleterm")
+    tt.toggle(1, nil, nil, direction, "Terminal")
+  end
+
+  return toggle
+end
+
 return {
   "akinsho/toggleterm.nvim",
   cmd = {
@@ -11,14 +28,18 @@ return {
     "ToggleTermSetName",
   },
   keys = {
-    { "<C-\\>" },
-    { "<leader>ft", "<cmd>TermSelect<CR>", desc = { "Terminal" } },
+    { "<C-\\>", default(), mode = { "n", "t" }, desc = "Toggle terminal" },
+    { "<leader>ft", "<cmd>TermSelect<CR>", desc = "Terminal" },
+    { "<leader>mf", default("float"), desc = "Float" },
+    { "<leader>mh", default("horizontal"), desc = "Horizontal" },
+    { "<leader>mm", default(), desc = "Toggle" },
+    { "<leader>mv", default("vertical"), desc = "Vertical" },
   },
   opts = {
-    open_mapping = "<C-\\>",
     direction = "float",
     float_opts = {
       border = "rounded",
     },
+    size = size,
   },
 }
