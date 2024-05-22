943ed1bf26016f3d4de722cd9cde1e0b73ac7457
Chris Pawling <chris-468@outlook.com>
2024-05-21 23:02:11 -0500

    nvim: don't show mode

--- dotfiles/private_dot_config/nvim/lua/chris468/config/options.lua
+++ dotfiles/private_dot_config/nvim/lua/chris468/config/options.lua
@@ -7,6 +7,7 @@ return {
     vim.opt.expandtab = true
     vim.opt.number = true
     vim.opt.relativenumber = true
+    vim.opt.showmode = false
     vim.opt.shiftwidth = 4
     vim.opt.signcolumn = "yes"
     vim.opt.splitbelow = true
