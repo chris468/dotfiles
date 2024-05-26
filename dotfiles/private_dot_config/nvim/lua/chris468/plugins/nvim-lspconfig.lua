45575509e10f3cc65701b1a47c9b05afba204f37
Chris Pawling <chris-468@outlook.com>
2024-05-25 19:06:41 -0500

    key for line diagnostics

--- dotfiles/private_dot_config/nvim/lua/chris468/plugins/nvim-lspconfig.lua
+++ dotfiles/private_dot_config/nvim/lua/chris468/plugins/nvim-lspconfig.lua
@@ -90,6 +90,13 @@ return {
     for severity, sign in pairs(opts.signs) do
       vim.fn.sign_define("DiagnosticSign" .. severity, { text = sign, texthl = "Diagnostic" .. severity, numhl = "" })
     end
+
+    vim.diagnostic.config({
+      float = {
+        severity_sort = true,
+      },
+      severity_sort = true,
+    })
   end,
   dependencies = {
     { "williamboman/mason-lspconfig.nvim" },
