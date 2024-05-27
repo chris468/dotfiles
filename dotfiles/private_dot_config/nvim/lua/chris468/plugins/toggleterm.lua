eac70ce465944da98c331dfd7f90a614f0d4980b
Chris Pawling <chris-468@outlook.com>
2024-05-27 10:59:25 -0500

    refactor terminal create to take opts table

--- dotfiles/private_dot_config/nvim/lua/chris468/plugins/toggleterm.lua
+++ dotfiles/private_dot_config/nvim/lua/chris468/plugins/toggleterm.lua
@@ -37,22 +37,30 @@ local function map_keys(term, allow_normal)
   end
 end
 
+--- @class TermOpts
+--- @field map_keys_once boolean?
+--- @field allow_normal boolean?
+
+--- @type TermOpts
+local term_defaults = {
+  map_keys_once = false,
+  allow_normal = true,
+}
+
 --- @param id number
 --- @param display_name string
 --- @param cmd string?
---- @param map_keys_once boolean?
---- @param allow_normal boolean?
-local function create(id, display_name, cmd, map_keys_once, allow_normal)
+--- @param opts TermOpts?
+local function create(id, display_name, cmd, opts)
   local Terminal = require("toggleterm.terminal").Terminal
 
-  map_keys_once = map_keys_once or false
-  allow_normal = allow_normal == nil or allow_normal
+  opts = vim.tbl_extend("keep", opts or {}, term_defaults)
 
   local on_map_keys = function(term)
-    map_keys(term, allow_normal)
+    map_keys(term, opts.allow_normal)
   end
 
-  local create_keys_when = map_keys_once and "on_create" or "on_open"
+  local create_keys_when = opts.map_keys_once and "on_create" or "on_open"
 
   return Terminal:new({
     id = id,
@@ -84,7 +92,10 @@ end
 
 local function lazygit()
   local function toggle()
-    local term = create(terminal_id.lazygit, "Lazygit", "lazygit", true, false)
+    local term = create(terminal_id.lazygit, "Lazygit", "lazygit", {
+      map_keys_once = true,
+      allow_normal = false,
+    })
     term:toggle(nil, "float")
     if any_modified_buffers() then
       vim.notify("Some files have unsaved changes", vim.log.levels.WARN)
@@ -119,11 +130,11 @@ return {
   keys = {
     { "<C-\\>", toggle, mode = { "n", "t" }, desc = "Toggle last terminal" },
     { "<leader>ft", "<cmd>TermSelect<CR>", desc = "Terminal" },
+    { "<leader>gg", lazygit(), desc = "Lazygit" },
     { "<leader>mf", default("float"), desc = "Float deafult terminal" },
     { "<leader>mh", default("horizontal"), desc = "Horizontal deafult terminal" },
     { "<leader>mm", default(), desc = "Toggle default terminal" },
     { "<leader>mv", default("vertical"), desc = "Vertical deafult terminal" },
-    { "<leader>gg", lazygit(), desc = "Lazygit" },
   },
   opts = {
     direction = "float",
