e7fc7b5331ff559b10e30950cff51e622e1025cf
Chris Pawling <chris-468@outlook.com>
2024-06-02 12:51:12 -0500

    Revert "cmdline popup at bottom"
    This reverts commit 7ffaf71f68dd6165179e9b5749b82c4d7f1a9c41.

--- dotfiles/private_dot_config/nvim/lua/chris468/plugins/noice.lua
+++ dotfiles/private_dot_config/nvim/lua/chris468/plugins/noice.lua
@@ -30,55 +30,56 @@ return {
     { "<S-CR>", redirect_cmdline, mode = "c", desc = "Redirect command line" },
   },
   lazy = false, -- not lazy to try and get set up before any notifcations can be raised
-  opts = {
-    cmdline = {
-      format = {
-        IncRename = {
-          pattern = "^:%s*IncRename%s+",
-          icon = "",
-          conceal = true,
+  opts = function()
+    local format = vim.deepcopy(require("noice.config").defaults().cmdline.format)
+    format.IncRename = {
+      pattern = "^:%s*IncRename%s+",
+      icon = "",
+      icon_hl_group = "lualine_a_command",
+      conceal = true,
+    }
+
+    for _, f in pairs(format) do
+      f.icon_hl_group = "lualine_a_command"
+      f.icon = " " .. (f.icon or " ") .. " "
+    end
+
+    return {
+      cmdline = {
+        view = "cmdline",
+        format = format,
+      },
+      lsp = {
+        -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
+        override = {
+          ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+          ["vim.lsp.util.stylize_markdown"] = true,
+          ["cmp.entry.get_documentation"] = true, -- requires hrsh7th/nvim-cmp
         },
       },
-    },
-    lsp = {
-      -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
-      override = {
-        ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
-        ["vim.lsp.util.stylize_markdown"] = true,
-        ["cmp.entry.get_documentation"] = true, -- requires hrsh7th/nvim-cmp
+      -- you can enable a preset for easier configuration
+      presets = {
+        bottom_search = true, -- use a classic bottom cmdline for search
+        command_palette = false, -- position the cmdline and popupmenu together
+        inc_rename = false, -- enables an input dialog for inc-rename.nvim
+        long_message_to_split = true, -- long messages will be sent to a split
+        lsp_doc_border = false, -- add a border to hover docs and signature help
       },
-    },
-    -- you can enable a preset for easier configuration
-    presets = {
-      bottom_search = true, -- use a classic bottom cmdline for search
-      command_palette = false, -- position the cmdline and popupmenu together
-      inc_rename = false, -- enables an input dialog for inc-rename.nvim
-      long_message_to_split = true, -- long messages will be sent to a split
-      lsp_doc_border = false, -- add a border to hover docs and signature help
-    },
-    routes = {
-      {
-        filter = {
-          any = {
-            { event = "msg_show", find = "%d+L, %d+B" },
-            { event = "msg_show", find = "; after #%d+" },
-            { event = "msg_show", find = "; before #%d+" },
-            { event = "msg_show", find = "^Already at oldest change" },
-            { event = "msg_show", find = "^search hit .+, continuing at .+$" },
-            { event = "msg_show", find = "^E486" },
+      routes = {
+        {
+          filter = {
+            any = {
+              { event = "msg_show", find = "%d+L, %d+B" },
+              { event = "msg_show", find = "; after #%d+" },
+              { event = "msg_show", find = "; before #%d+" },
+              { event = "msg_show", find = "^Already at oldest change" },
+              { event = "msg_show", find = "^search hit .+, continuing at .+$" },
+              { event = "msg_show", find = "^E486" },
+            },
           },
-        },
-        view = "mini",
-      },
-    },
-    views = {
-      cmdline_popup = {
-        position = { row = "95%", col = "50%" },
-        size = {
-          width = "95%",
-          height = "auto",
+          view = "mini",
         },
       },
-    },
-  },
+    }
+  end,
 }
