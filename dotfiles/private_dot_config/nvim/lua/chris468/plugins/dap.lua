7da8b114302ad568ca7417c9f160d31674656e33
Chris Pawling <chris-468@outlook.com>
2024-06-03 19:14:28 -0500

    mappings

--- dotfiles/private_dot_config/nvim/lua/chris468/plugins/dap.lua
+++ dotfiles/private_dot_config/nvim/lua/chris468/plugins/dap.lua
@@ -2,8 +2,135 @@ local adapters_for_ft = {
   python = { "debugpy" },
 }
 
+local mappings = {
+  ["<F5>"] = {
+    function()
+      require("dap").continue()
+    end,
+    desc = "Run",
+  },
+  ["<S-F5>"] = {
+    function()
+      require("dap").terminate()
+    end,
+    desc = "Stop debugging",
+  },
+  ["<C-S-F5>"] = {
+    function()
+      require("dap").restart()
+    end,
+    desc = "Restart debugging",
+  },
+  ["<F10>"] = {
+    function()
+      require("dap").step_over()
+    end,
+    desc = "Step over",
+  },
+  ["<C-F10>"] = {
+    function()
+      require("dap").run_to_cursor()
+    end,
+    desc = "Run to cursor",
+  },
+  ["<C-S-F10>"] = {
+    function()
+      require("dap").goto_()
+    end,
+    desc = "Set next statement",
+  },
+  ["<F11>"] = {
+    function()
+      require("dap").step_into()
+    end,
+    desc = "Step into",
+  },
+  ["<S-F11>"] = {
+    function()
+      require("dap").step_out()
+    end,
+    desc = "Step out",
+  },
+  ["<F9>"] = {
+    function()
+      require("dap").toggle_breakpoint()
+    end,
+    desc = "Toggle breakpoint",
+  },
+  ["<C-S-F9>"] = {
+    function()
+      require("dap").clear_breakpoints()
+    end,
+    desc = "Clear all breakpoints",
+  },
+  ["<leader>dlp"] = {
+    function()
+      require("dap").set_breakpoint(nil, nil, vim.fn.input("Log point message: "))
+    end,
+    "Set logpoint",
+  },
+  ["<leader>dr"] = {
+    function()
+      require("dap").repl.open()
+    end,
+    desc = "Open REPL",
+  },
+  ["<leader>dl"] = {
+    function()
+      require("dap").run_last()
+    end,
+    desc = "Run last configuration",
+  },
+  ["<leader>dh"] = {
+    function()
+      require("dap.ui.widgets").hover()
+    end,
+    mode = { "n", "v" },
+    desc = "Hover",
+  },
+  ["<leader>dp"] = {
+    function()
+      require("dap.ui.widgets").preview()
+    end,
+    mode = { "n", "v" },
+    desc = "Preview",
+  },
+  ["<leader>df"] = {
+    function()
+      local widgets = require("dap.ui.widgets")
+      widgets.centered_float(widgets.frames)
+    end,
+    desc = "Frames",
+  },
+  ["<leader>ds"] = {
+    function()
+      local widgets = require("dap.ui.widgets")
+      widgets.centered_float(widgets.scopes)
+    end,
+    desc = "Scopes",
+  },
+}
+
+local function attach()
+  local dapui = require("dapui")
+  dapui.open()
+end
+
+local function detach()
+  local dapui = require("dapui")
+  dapui.close()
+end
+
 return {
   "mfussenegger/nvim-dap",
+  config = function()
+    local dap = require("dap")
+
+    dap.listeners.before.attach.dapui_config = attach
+    dap.listeners.before.launch.dapui_config = attach
+    dap.listeners.before.event_terminated.dapui_config = detach
+    dap.listeners.before.event_exited.dapui_config = detach
+  end,
   dependencies = {
     {
       "williamboman/mason.nvim",
@@ -14,25 +141,8 @@ return {
     },
     {
       "rcarriga/nvim-dap-ui",
-      config = function()
-        local dap, dapui = require("dap"), require("dapui")
-
-        dapui.setup()
-
-        dap.listeners.before.attach.dapui_config = function()
-          dapui.open()
-        end
-        dap.listeners.before.launch.dapui_config = function()
-          dapui.open()
-        end
-        dap.listeners.before.event_terminated.dapui_config = function()
-          dapui.close()
-        end
-        dap.listeners.before.event_exited.dapui_config = function()
-          dapui.close()
-        end
-      end,
       dependencies = { "nvim-neotest/nvim-nio" },
+      opts = {},
     },
     {
       "theHamsta/nvim-dap-virtual-text",
@@ -81,4 +191,12 @@ return {
       dependencies = { "williamboman/mason.nvim" },
     },
   },
+  keys = function(_, keys)
+    keys = keys or {}
+    for key, mapping in pairs(mappings) do
+      local m = vim.deepcopy(mapping)
+      table.insert(m, 1, key)
+      table.insert(keys, m)
+    end
+  end,
 }
