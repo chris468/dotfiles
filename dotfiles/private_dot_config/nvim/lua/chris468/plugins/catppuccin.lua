760832ba30e193699ce58263f1b162fefb80072b
Chris Pawling <chris-468@outlook.com>
2024-06-06 22:45:54 -0500

    extract highlight creation and add to cat

--- dotfiles/private_dot_config/nvim/lua/chris468/plugins/catppuccin.lua
+++ dotfiles/private_dot_config/nvim/lua/chris468/plugins/catppuccin.lua
@@ -8,6 +8,30 @@ end
 
 return {
   "catppuccin/nvim",
+  init = function()
+    local catppuccin = require("catppuccin.palettes").get_palette("mocha")
+
+    local function create_highlights()
+      vim.api.nvim_set_hl(0, "chris468.IndentGuide", {
+        force = true,
+        fg = catppuccin.surface0,
+      })
+      vim.api.nvim_set_hl(0, "chris468.ScopeGuide", {
+        force = true,
+        fg = catppuccin.lavender,
+      })
+    end
+
+    if is_default_theme then
+      create_highlights()
+    end
+
+    vim.api.nvim_create_autocmd("ColorScheme", {
+      group = vim.api.nvim_create_augroup("catppuccin highlights", {}),
+      pattern = "catppuccin*",
+      callback = create_highlights,
+    })
+  end,
   config = function()
     if is_default_theme then
       vim.cmd("colorscheme " .. theme)
