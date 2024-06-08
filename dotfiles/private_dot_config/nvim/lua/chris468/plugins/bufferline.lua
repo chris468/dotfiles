cb0bcadd303b18b7c625098ff72cbd2dbbc8f6d9
Chris Pawling <chris-468@outlook.com>
2024-06-07 21:28:27 -0500

    use mini.bufremove to handle buffer delete behavior

--- dotfiles/private_dot_config/nvim/lua/chris468/plugins/bufferline.lua
+++ dotfiles/private_dot_config/nvim/lua/chris468/plugins/bufferline.lua
@@ -5,7 +5,6 @@ return {
   event = "VeryLazy",
   keys = {
     { "<leader>bb", "<cmd>e #<cr>", desc = "Last" },
-    { "<leader>bd", "<cmd>bd<cr>", desc = "Delete current" },
     { "<leader>bo", "<cmd>BufferLineCloseOthers<cr>", desc = "Delete others" },
     { "<S-h>", "<cmd>BufferLineCyclePrev<cr>", desc = "Previous buffer" },
     { "<S-l>", "<cmd>BufferLineCycleNext<cr>", desc = "Next buffer" },
