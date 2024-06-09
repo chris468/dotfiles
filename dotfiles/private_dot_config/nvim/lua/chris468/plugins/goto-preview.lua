4230505635b0a95d8571cfcf1be475e1727bbc52
Chris Pawling <chris-468@outlook.com>
2024-06-09 09:45:33 -0500

    start adjusting laziness

--- dotfiles/private_dot_config/nvim/lua/chris468/plugins/goto-preview.lua
+++ dotfiles/private_dot_config/nvim/lua/chris468/plugins/goto-preview.lua
@@ -1,5 +1,13 @@
 return {
   "rmagatti/goto-preview",
+  keys = {
+    "gpd",
+    "gpt",
+    "gpi",
+    "gpD",
+    "gP",
+    "gpr",
+  },
   opts = {
     default_mappings = true,
   },
--- dotfiles/private_dot_config/nvim/lua/chris468/plugins/mini-comment.lua
+++ dotfiles/private_dot_config/nvim/lua/chris468/plugins/mini-comment.lua
@@ -1,4 +1,4 @@
 return {
   "echasnovski/mini.comment",
-  opts = {},
+  config = true,
 }
