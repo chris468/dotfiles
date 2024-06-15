b93e5b0201175758fb1eca8d26be8a30c0e27a27
Chris Pawling <chris-468@outlook.com>
2024-06-14 22:23:18 -0500

    nvim: pyright: detect venv

--- dotfiles/private_dot_config/nvim/lua/chris468/plugins/nvim-lspconfig.lua
+++ dotfiles/private_dot_config/nvim/lua/chris468/plugins/nvim-lspconfig.lua
@@ -1,5 +1,31 @@
 local icons = require("chris468.config.icons").diagnostic
 
+local function detect_venv(root_dir)
+  local Job = require("plenary.job")
+  local python = vim.loop.os_uname().sysname == "Windows_NT" and "python.exe" or "python"
+  local dot_venv_python = root_dir .. "/.venv/bin/" .. python
+  if vim.fn.filereadable(dot_venv_python) == 1 then
+    return dot_venv_python
+  end
+
+  local pyproject = root_dir .. "/pyproject.toml"
+  if vim.fn.executable("poetry") == 1 and vim.fn.filereadable(pyproject) == 1 then
+    local ok, venv = pcall(function()
+      return Job:new({
+        command = "poetry",
+        cwd = root_dir,
+        args = { "env", "info", "-p" },
+      })
+        :sync()[1]
+    end)
+    if ok then
+      return venv .. "/bin/" .. python
+    end
+  end
+
+  return false
+end
+
 local servers = {
   angularls = {},
   ansiblels = {},
@@ -23,7 +49,19 @@ local servers = {
   nil_ls = {}, -- nix
   omnisharp = require("chris468.plugins.config.lsp.omnisharp"),
   powershell_es = {},
-  pyright = {},
+  pyright = {
+    on_new_config = function(new_config, new_root_dir)
+      local defaults = require("lspconfig.server_configurations.pyright")
+      if defaults.on_new_config then
+        defaults.on_new_config(new_config, new_root_dir)
+      end
+
+      local venv_python = detect_venv(new_root_dir)
+      if venv_python then
+        new_config.settings.python.pythonPath = venv_python
+      end
+    end,
+  },
   rust_analyzer = {},
   ruff = {}, -- python
   spectral = {}, -- OpenAPI
@@ -177,6 +215,7 @@ return {
   end,
   event = "FileType",
   dependencies = {
+    { "nvim-lua/plenary.nvim" },
     { "williamboman/mason-lspconfig.nvim" },
     {
       "williamboman/mason.nvim",
