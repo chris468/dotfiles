40ea50ca03a9d6d12d53515686e1e79f150f7b94
Chris Pawling <chris-468@outlook.com>
2024-06-30 14:22:47 -0500

    nvim: CS-H/L to navigate buffers

--- dotfiles/private_dot_config/nvim/lua/chris468/plugins/bufferline.lua
+++ dotfiles/private_dot_config/nvim/lua/chris468/plugins/bufferline.lua
@@ -7,7 +7,7 @@ return {
   keys = {
     { "<leader>bb", "<cmd>e #<cr>", desc = "Last" },
     { "<leader>bo", "<cmd>BufferLineCloseOthers<cr>", desc = "Delete others" },
-    { "<leader>h", "<cmd>BufferLineCyclePrev<cr>", desc = "Previous buffer" },
-    { "<leader>l", "<cmd>BufferLineCycleNext<cr>", desc = "Next buffer" },
+    { "<C-S-H>", "<cmd>BufferLineCyclePrev<cr>", desc = "Previous buffer" },
+    { "<C-S-L>", "<cmd>BufferLineCycleNext<cr>", desc = "Next buffer" },
   },
 }
