f61dc7e283255affaac635d688c5186fc5e9d1ca
Chris Pawling <chris-468@outlook.com>
2024-07-17 17:46:50 -0500

    add cterm

--- dotfiles/private_dot_config/nvim/lua/chris468/themes/base16.lua
+++ dotfiles/private_dot_config/nvim/lua/chris468/themes/base16.lua
@@ -1,7 +1,7 @@
 local base16 = require("base16-colorscheme").colors
 
 --- @type chris468.Theme
-return {
+local M = {
   highlights = {
     IblIndent = { fg = base16.base05 },
     IblScope = { fg = base16.base05 },
@@ -10,3 +10,26 @@ return {
     StoppedIcon = { fg = base16.base05 },
   },
 }
+
+function M.add_cterm(colors)
+  return vim.tbl_extend("keep", colors, {
+    cterm00 = 0,
+    cterm01 = 8,
+    cterm02 = 11,
+    cterm03 = 10,
+    cterm04 = 13,
+    cterm05 = 14,
+    cterm06 = 12,
+    cterm07 = 5,
+    cterm08 = 3,
+    cterm09 = 8,
+    cterm0A = 11,
+    cterm0B = 10,
+    cterm0C = 13,
+    cterm0D = 14,
+    cterm0E = 12,
+    cterm0F = 7,
+  })
+end
+
+return M
