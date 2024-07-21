f8590219202cee6b561ba531109bd1ee6efb5ed6
Chris Pawling <chris-468@outlook.com>
2024-07-21 12:54:43 -0500

    fix dircolors

--- dotfiles/private_dot_config/bashrc.d/dircolors.sh
+++ dotfiles/private_dot_config/bashrc.d/dircolors.sh
@@ -1,13 +1,11 @@
-if [ -x /usr/bin/dircolors ]; then
-	eval "$(/usr/bin/env bash ~/.config/dircolors/dircolors.sh)"
+source ~/.config/dircolors/dircolors.sh
 
-	if command -v gls >/dev/null 2>&1; then
-		alias ls='gls --color=auto'
-	else
-		alias ls='ls --color=auto'
-	fi
-
-	alias grep='grep --color=auto'
-	alias fgrep='fgrep --color=auto'
-	alias egrep='egrep --color=auto'
+if command -v gls >/dev/null 2>&1; then
+	alias ls='gls --color=auto'
+else
+	alias ls='ls --color=auto'
 fi
+
+alias grep='grep --color=auto'
+alias fgrep='fgrep --color=auto'
+alias egrep='egrep --color=auto'
