156da257ad0bf9f845b3c96194981f653e8e0b25
Chris Pawling <chris-468@outlook.com>
2024-09-21 12:44:54 -0500

    nvim: local config

---
+++ dotfiles/private_dot_config/nvim/lua/chris468/config/create_local.lua
@@ -0,0 +1,9 @@
+-- This file may be updated to modify configuration for this location.
+-- Changes will be ignored by chezmoi.
+
+--- Return locally customized configuration.
+--- @param config chris468Config Default configuration
+--- @return chris468Config config Customized configuration
+return function(config)
+  return config
+end
--- dotfiles/private_dot_config/nvim/lua/chris468/config/init.lua
+++ dotfiles/private_dot_config/nvim/lua/chris468/config/init.lua
@@ -3,9 +3,16 @@
 --- | '"omnisharp"'
 
 --- @class chris468Config
---- @field enable_nice boolean
+--- @field enable_noice boolean
 --- @field csharp_lsp chris468CsharpLsp | chris468CsharpLsp[]
-return {
+
+--- @type chris468Config
+local default_config = {
   enable_noice = true,
   csharp_lsp = "roslyn",
 }
+
+--- @type boolean, fun(config: chris468Config) : chris468Config
+local ok, local_config = pcall(require, "chris468.config.local", default_config)
+
+return ok and local_config(default_config) or default_config
