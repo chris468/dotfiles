245c22db15a4252069992848bbd8914ba538aae4
Chris Pawling <chris-468@outlook.com>
2024-09-21 19:04:40 -0500

    disable codeium for work

--- dotfiles/private_dot_config/nvim/lua/chris468/plugins/codeium.lua
+++ dotfiles/private_dot_config/nvim/lua/chris468/plugins/codeium.lua
@@ -1,6 +1,15 @@
 return {
   "Exafunction/codeium.nvim",
-  cond = require("chris468.config").ai_assistant == "codeium",
+  cond = function()
+    local enable = require("chris468.config").ai_assistant == "codeium"
+    local work = require("chris468.config.settings").work
+    if enable and work then
+      enable = false
+      vim.notify("only copilot at work", vim.log.levels.ERROR)
+    end
+
+    return enable
+  end,
   config = function()
     require("codeium").setup({})
   end,
