ef8864e3d471c839985e3a67c558b5617e6cadae
Chris Pawling <chris-468@outlook.com>
2024-10-27 16:17:30 -0500

    telescope binding to show lazy plugin's options

--- dotfiles/private_dot_config/exact_lazyvim/lua/plugins/telescope.lua
+++ dotfiles/private_dot_config/exact_lazyvim/lua/plugins/telescope.lua
@@ -1,7 +1,29 @@
 return {
   "nvim-telescope/telescope.nvim",
   dependencies = {
-    "tsakirist/telescope-lazy.nvim",
+    {
+      "tsakirist/telescope-lazy.nvim",
+      config = function()
+        local actions = require("telescope._extensions.lazy.actions")
+
+        actions.show_configuration = function()
+          local state = require("telescope.actions.state")
+          local util = require("chris468.util")
+          local util_lazy = require("chris468.util.lazy")
+
+          local entry = state.get_selected_entry()
+          if not entry then
+            return
+          end
+
+          local opts = util_lazy.get_plugin_opts(entry.value.name)
+          util.show_in_readonly_popup(vim.inspect(opts))
+        end
+      end,
+    },
+  },
+  keys = {
+    { "<leader>sL", "<cmd>Telescope lazy<CR>", desc = "Lazy plugins" },
   },
   opts = {
     extensions = {
@@ -9,9 +31,13 @@ return {
         mappings = {
           change_cwd_to_plugin = "<C-w>",
           open_plugins_picker = "<C-o>",
+          show_configuration = "<C-c>",
         },
         actions_opts = {
           change_cwd_to_plugin = {
+            auto_close = true,
+          },
+          show_configuration = {
             auto_close = true,
           },
         },
