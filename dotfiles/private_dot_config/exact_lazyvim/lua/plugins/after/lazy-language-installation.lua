ac6fb5ab21e68c1b8c00539eabf1c33ac2fa0efe
Chris Pawling <chris-468@outlook.com>
2024-10-27 22:49:17 -0500

    attempt to lazy-load by wrapping the setup

--- dotfiles/private_dot_config/exact_lazyvim/lua/plugins/after/lazy-language-installation.lua
+++ dotfiles/private_dot_config/exact_lazyvim/lua/plugins/after/lazy-language-installation.lua
@@ -2,11 +2,64 @@ return {
   {
     "neovim/nvim-lspconfig",
     opts = function(_, opts)
-      opts._servers = opts.servers
-      opts.servers = {}
+      -- opts._servers = opts.servers
+      -- opts.servers = {}
+      --
+      -- opts._setup = opts.setup
+      -- opts.setup = {}
 
-      opts._setup = opts.setup
-      opts.setup = {}
+      -- for each server, server_opts in servers
+      --   server_opts.mason = false -- to prevent LazyVim plugin config from calling mason install.
+      --                        -- Will immediately set up instead.
+      --   original_setup = setup[server] or function() return false end
+      --   setup[server] = function(s, o)
+      --     on filetype o.filetypes or lspconfig[s].default_config.filetypes {
+      --        install the server
+      --        if not original_setup(s, o) then
+      --          lspconfig[s].setup(o)
+      --        end
+      --     }
+      --     return true
+      --   end
+      -- end
+
+      local function wrap(setup, opts)
+        if opts.wrapped then
+          return true
+        end
+
+        opts.wrapped = true
+
+        return function(name, opts)
+          local lspconfig = require("lspconfig")
+
+          setup = setup or function()
+            return false
+          end
+          local desc = "Install and configure " .. name .. "lsp server"
+          vim.api.nvim_create_autocmd("FileType", {
+            callback = function()
+              -- TODO: install
+              vim.notify("TODO: Install " .. name)
+              if setup(name, opts) ~= true then
+                vim.notify("seting up " .. name)
+                lspconfig[name].setup(opts)
+              end
+              return true
+            end,
+            desc = desc,
+            group = vim.api.nvim_create_augroup(desc, { clear = true }),
+            pattern = opts.filetypes or lspconfig[name].config_def.default_config.filetypes,
+          })
+
+          return true
+        end
+      end
+
+      for server, server_opts in pairs(opts.servers) do
+        server_opts.mason = false
+        opts.setup[server] = wrap(opts.setup[server], server_opts)
+      end
     end,
   },
   {
