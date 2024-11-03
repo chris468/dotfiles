638f749647c6ca63f3e5bd7c4deade0ad47abf9f
Chris Pawling <chris-468@outlook.com>
2024-11-03 14:27:24 -0600

    clear _ in formatters_by_ft

--- dotfiles/private_dot_config/exact_lazyvim/lua/chris468/lazy-mason-install/formatters.lua
+++ dotfiles/private_dot_config/exact_lazyvim/lua/chris468/lazy-mason-install/formatters.lua
@@ -13,6 +13,7 @@ function M.to_filetypes()
 
   local formatters_by_ft = lazyvim_util.opts("conform.nvim").formatters_by_ft
   formatters_by_ft["*"] = vim.list_extend(formatters_by_ft["*"] or {}, formatters_by_ft["_"] or {})
+  formatters_by_ft["_"] = nil
   return util.invert_list_map(formatters_by_ft)
 end
 
