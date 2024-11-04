a1a98c26158dc508ed105c9b5600654badcdb8df
Chris Pawling <chris-468@outlook.com>
2024-11-03 18:05:59 -0600

    Less intrusive notifications

---
+++ dotfiles/private_dot_config/exact_lazyvim/lua/plugins/notifications.lua
@@ -0,0 +1,17 @@
+return {
+  "folke/noice.nvim",
+  opts = {
+    messages = {
+      enabled = true,
+      view = "mini",
+      view_error = "mini",
+      view_warn = "mini",
+      view_history = "messages",
+      view_search = "virtualtext",
+    },
+    notify = {
+      enabled = true,
+      view = "mini",
+    },
+  },
+}
