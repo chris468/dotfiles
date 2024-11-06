54f6d4e28e4da37696da68e4791063b592d6fba5
Chris Pawling <chris-468@outlook.com>
2024-11-05 20:43:20 -0600

    remove keymaps

--- dotfiles/private_dot_config/exact_lazyvim/lua/config/keymaps.lua
+++ dotfiles/private_dot_config/exact_lazyvim/lua/config/keymaps.lua
@@ -1,3 +1,10 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- was accidentally triggering line movement when leaving insert mode and trying to move
+vim.keymap.del({ "n", "i", "v" }, "<A-j>")
+vim.keymap.del({ "n", "i", "v" }, "<A-k>")
+
+-- I never increment on purpose
+vim.keymap.set("n", "<C-a>", "<nop>")
