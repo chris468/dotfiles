d823547d0d56f6329246cc744e51025fcac4d176
Chris Pawling <chris-468@outlook.com>
2024-11-09 18:10:52 -0600

    Add dotfiles shortcuts

--- dotfiles/private_dot_config/exact_lazyvim/lua/config/keymaps.lua
+++ dotfiles/private_dot_config/exact_lazyvim/lua/config/keymaps.lua
@@ -1,6 +1,9 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+local lazyvim = {
+  util = require("lazyvim.util"),
+}
 
 -- was accidentally triggering line movement when leaving insert mode and trying to move
 vim.keymap.del({ "n", "i", "v" }, "<A-j>")
@@ -10,3 +13,32 @@ vim.keymap.del({ "n", "i", "v" }, "<A-k>")
 vim.keymap.set("n", "<C-a>", "<nop>")
 
 vim.keymap.set("n", "<leader>f<C-T>", "<cmd>TermSelect<cr>", { desc = "Terminals" })
+
+lazyvim.util.on_load("which-key.nvim", function()
+  local wk = require("which-key")
+  wk.add({
+    { "<leader>fd", group = "Chezmoi Dotfiles" },
+  })
+end)
+
+vim.keymap.set("n", "<leader>fda", "", {
+  desc = "Apply chezmoi dotfiles",
+  callback = function()
+    require("chris468.terminal").open(
+      "chezmoi apply",
+      { direction = "horizontal", display_name = "Chezmoi", remain_on_error = true, warn_on_unsaved = true }
+    )
+  end,
+})
+vim.keymap.set("n", "<leader>fdA", "", {
+  desc = "Add current file to dotfiles",
+  callback = function()
+    local cmd = function()
+      return { "chezmoi", "add", vim.api.nvim_buf_get_name(vim.api.nvim_get_current_buf()) }
+    end
+    require("chris468.terminal").open(
+      cmd,
+      { direction = "horizontal", display_name = "Chezmoi", remain_on_error = true, warn_on_unsaved = true }
+    )
+  end,
+})
