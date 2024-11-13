1bf6ce1a63b389f91bf914370c7b83fbf4d5c466
Chris Pawling <chris-468@outlook.com>
2024-11-12 21:16:01 -0600

    don't replace Luapad cmd, move better keymap to keymaps

--- dotfiles/private_dot_config/exact_lazyvim/lua/plugins/luapad.lua
+++ dotfiles/private_dot_config/exact_lazyvim/lua/plugins/luapad.lua
@@ -5,19 +5,6 @@ return {
     "LuaRun",
     "Lua",
   },
-  config = function(_, opts)
-    require("luapad").setup(opts)
-    vim.api.nvim_create_user_command("Luapad", function()
-      require("chris468.luapad").toggle()
-    end, { force = true })
-  end,
-  keys = {
-    {
-      "<leader>ml",
-      "<cmd>Luapad<cr>",
-      desc = "Lua REPL",
-    },
-  },
   opts = {
     eval_on_change = false,
     eval_on_move = false,
