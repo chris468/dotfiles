745942e0838376a2d66eb00547eb29b1b36e42d2
Chris Pawling <chris-468@outlook.com>
2024-11-16 08:13:40 -0600

    disable snacks notifier

--- dotfiles/private_dot_config/exact_lazyvim/lua/plugins/notifications.lua
+++ dotfiles/private_dot_config/exact_lazyvim/lua/plugins/notifications.lua
@@ -1,17 +1,25 @@
 return {
-  "folke/noice.nvim",
-  opts = {
-    messages = {
-      enabled = true,
-      view = "mini",
-      view_error = "mini",
-      view_warn = "mini",
-      view_history = "messages",
-      view_search = "virtualtext",
+  {
+    "folke/snacks.nvim",
+    opts = {
+      notifier = { enabled = false },
     },
-    notify = {
-      enabled = true,
-      view = "mini",
+  },
+  {
+    "folke/noice.nvim",
+    opts = {
+      messages = {
+        enabled = true,
+        view = "mini",
+        view_error = "mini",
+        view_warn = "mini",
+        view_history = "messages",
+        view_search = "virtualtext",
+      },
+      notify = {
+        enabled = true,
+        view = "mini",
+      },
     },
   },
 }
