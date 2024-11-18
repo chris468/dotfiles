cc4ed6080da23f22b55499c2de0f981cdfe18515
Chris Pawling <chris-468@outlook.com>
2024-11-17 18:27:03 -0600

    Move functionality to config/{autocmds,keymaps}.lua

--- dotfiles/private_dot_config/exact_lazyvim/lua/config/autocmds.lua
+++ dotfiles/private_dot_config/exact_lazyvim/lua/config/autocmds.lua
@@ -1,3 +1,16 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+
+-- Prevent save prompts on modified prompt buffers (for example telescope)
+vim.api.nvim_create_autocmd("QuitPre", {
+  group = vim.api.nvim_create_augroup("qp", { clear = true }),
+  callback = function(args)
+    local buf = args.buf
+    if vim.api.nvim_buf_is_valid(buf) then
+      if vim.bo[buf].buftype == "prompt" then
+        vim.bo[buf].modified = false
+      end
+    end
+  end,
+})
--- dotfiles/private_dot_config/exact_lazyvim/lua/config/keymaps.lua
+++ dotfiles/private_dot_config/exact_lazyvim/lua/config/keymaps.lua
@@ -87,3 +87,29 @@ if lazyvim.util.has("vim-tmux-navigator") then
   vim.keymap.set("n", "<C-k>", "<cmd>TmuxNavigateUp<cr>", { desc = "Move to window above" })
   vim.keymap.set("n", "<C-l>", "<cmd>TmuxNavigateRight<cr>", { desc = "Move to window on right" })
 end
+
+local function F(n)
+  return "F" .. n
+end
+
+local function Shift(k)
+  return "S-" .. k
+end
+
+local function Control(k)
+  return "C-" .. k
+end
+
+local function Wrap(k)
+  return "<" .. k .. ">"
+end
+
+for k = 1, 12 do
+  -- s-f10: <F22>
+  -- c-f10: <F34>
+  -- c-s-f10: <F46>
+
+  vim.api.nvim_set_keymap("", Wrap(F(k + 12)), Wrap(Shift(F(k))), { desc = "which_key_ignore" })
+  vim.api.nvim_set_keymap("", Wrap(F(k + 24)), Wrap(Control(F(k))), { desc = "which_key_ignore" })
+  vim.api.nvim_set_keymap("", Wrap(F(k + 36)), Wrap(Control(Shift(F(k)))), { desc = "which_key_ignore" })
+end
