692e20643a0e86f3940ff965ada4b8c259cfd87a
Chris Pawling <chris-468@outlook.com>
2024-11-22 19:46:17 -0600

    Better completion kinds

--- dotfiles/private_dot_config/nvim/lua/plugins/completion.lua
+++ dotfiles/private_dot_config/nvim/lua/plugins/completion.lua
@@ -1,6 +1,44 @@
+local format = {}
+
+function format.path(entry, item)
+  local ok, mi = pcall(require, "mini.icons")
+  if not ok then
+    return
+  end
+
+  local data = entry.completion_item.data or {}
+  local path, type = data.path, data.type
+  if not path or not type then
+    return item
+  end
+
+  local icon, hl = mi.get(type, path)
+
+  item.kind = icon .. " " .. type:gsub("^%a", string.upper)
+  item.kind_hl_group = hl
+  return item
+end
+
+function format.cmdline(_, item)
+  item.kind = " Command"
+  return item
+end
+
+function format.cmdline_history(_, item)
+  item.kind = " History"
+  return item
+end
+
+function format.buffer(_, item)
+  item.kind = " Buffer"
+end
+
 return {
   "nvim-cmp",
-  dependencies = { "chrisgrieser/cmp-nerdfont" },
+  dependencies = {
+    "chrisgrieser/cmp-nerdfont",
+    "plenary.nvim",
+  },
   opts = function(_, opts)
     local cmp = require("cmp")
 
@@ -18,5 +56,12 @@ return {
         winhighlight = "FloatBorder:NoiceCmdlinePopupBorder",
       },
     })
+
+    local original_format = opts.formatting.format
+    opts.formatting.format = function(entry, item)
+      item = original_format(entry, item)
+      item = format[entry.source.name] and format[entry.source.name](entry, item) or item
+      return item
+    end
   end,
 }
