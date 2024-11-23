c5b9d74b1b2891f47f1bd05db09cd2fd5566408d
Chris Pawling <chris-468@outlook.com>
2024-11-22 18:21:02 -0600

    use bordered cmp window

--- dotfiles/private_dot_config/nvim/lua/plugins/completion.lua
+++ dotfiles/private_dot_config/nvim/lua/plugins/completion.lua
@@ -2,9 +2,21 @@ return {
   "nvim-cmp",
   dependencies = { "chrisgrieser/cmp-nerdfont" },
   opts = function(_, opts)
+    local cmp = require("cmp")
+
     table.insert(opts.sources, {
       name = "nerdfont",
       group_index = 1,
+    })
+    opts.window = vim.tbl_deep_extend("force", opts.window or {}, {
+      completion = {
+        border = "rounded",
+        winhighlight = "FloatBorder:NoiceCmdlinePopupBorder",
+      },
+      documentation = {
+        border = "rounded",
+        winhighlight = "FloatBorder:NoiceCmdlinePopupBorder",
+      },
     })
   end,
 }
