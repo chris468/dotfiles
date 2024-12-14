9041c7b15b832913caa5e2760fe227528dd14d64
Chris Pawling <chris-468@outlook.com>
2024-12-14 12:32:15 -0600

    wezterm: silence curl when installing terminfo

--- dotfiles/.chezmoiscripts/run_install_wezterm_terminfo.sh
+++ dotfiles/.chezmoiscripts/run_install_wezterm_terminfo.sh
@@ -3,6 +3,6 @@
 [ -n "$(find ~/.terminfo -name wezterm)" ] ||
   (echo "Installing wezterm terminfo" &&
     tempfile=$(mktemp) &&
-    curl -o $tempfile https://raw.githubusercontent.com/wez/wezterm/main/termwiz/data/wezterm.terminfo &&
+    curl -s -o $tempfile https://raw.githubusercontent.com/wez/wezterm/main/termwiz/data/wezterm.terminfo &&
     tic -x -o ~/.terminfo $tempfile &&
     rm $tempfile)
