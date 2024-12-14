96930e596d570fd3f3f487b6b1a9bd67c139f96c
Chris Pawling <chris-468@outlook.com>
2024-12-14 12:30:32 -0600

    wezterm: typo

--- dotfiles/.chezmoiscripts/run_install_wezterm_terminfo.sh
+++ dotfiles/.chezmoiscripts/run_install_wezterm_terminfo.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-[ -n "$(find ~/.terminfo -name wezter)" ] ||
+[ -n "$(find ~/.terminfo -name wezterm)" ] ||
   (echo "Installing wezterm terminfo" &&
     tempfile=$(mktemp) &&
     curl -o $tempfile https://raw.githubusercontent.com/wez/wezterm/main/termwiz/data/wezterm.terminfo &&
