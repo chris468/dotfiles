68518b854b1d486a2e737181085de7156bc3bd86
Chris Pawling <chris-468@outlook.com>
2024-12-22 14:49:14 -0600

    shell: aliases for bat, lsd when available

--- dotfiles/private_dot_config/bashrc.d/dircolors.sh
+++ dotfiles/private_dot_config/bashrc.d/dircolors.sh
@@ -1,11 +1,1 @@
 source ~/.config/dircolors/dircolors.sh
-
-if command -v gls >/dev/null 2>&1; then
-	alias ls='gls --color=auto'
-else
-	alias ls='ls --color=auto'
-fi
-
-alias grep='grep --color=auto'
-alias fgrep='fgrep --color=auto'
-alias egrep='egrep --color=auto'
---
+++ dotfiles/private_dot_config/bashrc.d/zz_aliases.sh
@@ -0,0 +1,19 @@
+function _chk() {
+  command -v "$1" &>/dev/null
+}
+
+alias grep='grep --color=auto'
+alias fgrep='fgrep --color=auto'
+alias egrep='egrep --color=auto'
+
+if _chk lsd; then
+  alias ls='lsd --color=auto'
+elif _chk gls; then
+  alias ls='gls --color=auto'
+else
+  alias ls='ls --color=auto'
+fi
+
+! _chk bat || alias cat=bat
+
+unset -f _chk
