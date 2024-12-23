82369dc4f0c62cd4ffc586283670f938ff7082ca
Chris Pawling <chris-468@outlook.com>
2024-12-23 10:29:34 -0600

    tmux: add btop as possible monitor

--- dotfiles/private_dot_config/tmux/scripts/executable_monitor.sh
+++ dotfiles/private_dot_config/tmux/scripts/executable_monitor.sh
@@ -1,19 +1,19 @@
 #!/usr/bin/env bash
 #
-APPS=(glances htop top)
+APPS=(btop glances htop top)
 
 for app in ${APPS[@]}; do
-	if command -v $app &>/dev/null; then
-		$app
-		exit
-	fi
+  if command -v $app &>/dev/null; then
+    $app
+    exit
+  fi
 done
 
 message="No monitoring app found: ${APPS[@]}"
 if [ -z "$TMUX" ]; then
-	echo "$message" >2
+  echo "$message" >2
 else
-	tmux display-message "#[fg=##d08770]$message"
+  tmux display-message "#[fg=##d08770]$message"
 fi
 
 exit 1
