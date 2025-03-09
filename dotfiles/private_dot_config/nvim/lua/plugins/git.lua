1dd644444a719e6590ec03cc0944e693eae2b77a
Chris Pawling <chris-468@outlook.com>
2025-03-09 11:36:56 -0500

    Add diffview

--- dotfiles/private_dot_config/nvim/lua/plugins/git.lua
+++ dotfiles/private_dot_config/nvim/lua/plugins/git.lua
@@ -1,9 +1,16 @@
 return {
   {
-    "NeogitOrg/neogit",
+    "sindrets/diffview.nvim",
+    cmd = { "DiffviewOpen", "DiffviewClose", "DiffviewToggleFiles", "DiffviewFocusFiles" },
     lazy = true,
     opts = {},
+  },
+  {
+    "NeogitOrg/neogit",
+    dependencies = { "diffview.nvim" },
     cmd = "Neogit",
+    lazy = true,
+    opts = {},
     keys = {
       { "<leader>gg", "<cmd>Neogit<cr>", "Neogit" },
     },
