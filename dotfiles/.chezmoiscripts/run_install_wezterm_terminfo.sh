9d53cee4ca584aa7e0ac19d2452d9e7575ad5cbe
Chris Pawling <chris-468@outlook.com>
2025-04-26 15:40:17 -0500

    wezterm: download terminfo from latest release and react on change

--- dotfiles/.chezmoiscripts/run_install_wezterm_terminfo.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-[ -n "$(find ~/.terminfo -name wezterm)" ] ||
-  (echo "Installing wezterm terminfo" &&
-    tempfile=$(mktemp) &&
-    curl -s -o $tempfile https://raw.githubusercontent.com/wez/wezterm/main/termwiz/data/wezterm.terminfo &&
-    tic -x -o ~/.terminfo $tempfile &&
-    rm $tempfile)
