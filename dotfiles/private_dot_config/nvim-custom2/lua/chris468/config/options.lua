4268468369a51c7a224e5d092ae7d6f16c0e0851
Chris Pawling <chris-468@outlook.com>
2025-05-14 20:32:52 -0500

    nord and catppuccin

--- dotfiles/private_dot_config/nvim-custom2/lua/chris468/config/options.lua
+++ dotfiles/private_dot_config/nvim-custom2/lua/chris468/config/options.lua
@@ -1,2 +1,9 @@
 vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
+
+local chezmoi = require("chris468.config.chezmoi")
+local chris468 = {}
+local has_overrides, overrides = pcall(require, "chris468.config.local")
+
+_G.Chris468 =
+  vim.tbl_deep_extend("error", vim.tbl_deep_extend("force", chris468, has_overrides and overrides or {}), chezmoi)
--- dotfiles/private_dot_config/nvim-custom2/lua/chris468/plugins/colors.lua
+++ dotfiles/private_dot_config/nvim-custom2/lua/chris468/plugins/colors.lua
@@ -1,8 +1,46 @@
+local theme = {
+  catppuccin = Chris468.options.theme:find("^catppuccin") and true or false,
+  nord = Chris468.options.theme == "nord",
+  tokyonight = Chris468.options.theme:find("^tokyonight") and true or false,
+}
+
 return {
-  "folke/tokyonight.nvim",
-  lazy = false,
-  config = function()
-    require("tokyonight").setup()
-    vim.cmd.colorscheme("tokyonight")
-  end,
+  {
+    "folke/tokyonight.nvim",
+    config = function()
+      require("tokyonight").setup()
+      if theme.tokyonight then
+        vim.cmd.colorscheme(Chris468.options.theme)
+      end
+    end,
+    event = "VeryLazy",
+    lazy = not theme.tokyonight,
+  },
+  {
+    "catppuccin/nvim",
+    config = function()
+      require("catppuccin").setup()
+      if theme.catppuccin then
+        vim.cmd.colorscheme(Chris468.options.theme)
+      end
+    end,
+    event = "VeryLazy",
+    lazy = not theme.catppuccin,
+    name = "catppuccin",
+  },
+  {
+    "nordtheme/vim",
+    config = function()
+      if theme.nord then
+        vim.cmd.colorscheme(Chris468.options.theme)
+      end
+    end,
+    event = "VeryLazy",
+    init = function()
+      vim.g.nord_cursor_line_number_background = 1
+    end,
+    lazy = not theme.nord,
+    name = "nord",
+    version = false,
+  },
 }
