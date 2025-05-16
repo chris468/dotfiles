0c8d718d0e72a7cb932eed18e7b150a1a1fef3b6
Chris Pawling <chris-468@outlook.com>
2025-05-15 19:35:16 -0500

    mason

---
+++ dotfiles/private_dot_config/nvim-custom2/lua/chris468/plugins/tools.lua
@@ -0,0 +1,18 @@
+return {
+  {
+    "mason-org/mason.nvim",
+    cmd = { "Mason", "MasonInstall", "MasonLog", "MasonUninstall", "MasonUninstallAll", "MasonUpdate" },
+    keys = {
+      { "<leader>m", "<cmd>Mason<CR>", desc = "Mason" },
+    },
+    opts = {
+      ui = {
+        icons = {
+          package_installed = "✓",
+          package_pending = "➜",
+          package_uninstalled = "✗",
+        },
+      },
+    },
+  },
+}
