b7c113c5416a1fbe37727ad7e35953c96a3c57ef
Chris Pawling <chris-468@outlook.com>
2025-05-16 10:56:44 -0500

    lazier load for lspconfig, etc

--- dotfiles/private_dot_config/nvim-custom2/plugin/tools.lua
+++ dotfiles/private_dot_config/nvim-custom2/plugin/tools.lua
@@ -1,7 +1,9 @@
 vim.api.nvim_create_autocmd("FileType", {
   callback = function(arg)
     local filetype = arg.match
-    local buf = arg.buf
-    return require("chris468.tools").install_tools(buf, filetype)
+    if not vim.list_contains(Chris468.tools.disable, filetype) then
+      local buf = arg.buf
+      return require("chris468.tools").install_tools(buf, filetype)
+    end
   end,
 })
