169a96a4dd1e183952d6c0b8b97e89ba2a16fda8
Chris Pawling <chris-468@outlook.com>
2025-05-16 12:32:28 -0500

    format on save, with option to disable

--- dotfiles/private_dot_config/nvim-custom2/lua/chris468/types.lua
+++ dotfiles/private_dot_config/nvim-custom2/lua/chris468/types.lua
@@ -1,3 +1,12 @@
 ---@alias chris468.config.CheckPrequisite fun(): boolean, string
----@alias chris468.config.ToolsByFiletype { [string]: string[]|{[1]: string, prerequisite?: chris468.config.CheckPrequisite}[] }
+
+---@class chris468.config.ToolSpec
+---@field [1] string package name
+---@field prerequisite? chris468.config.CheckPrequisite
+
+---@class chris468.config.FormatterSpec: chris468.config.ToolSpec
+---@field format_on_save? boolean
+
+---@alias chris468.config.ToolsByFiletype { [string]: string[]|chris468.config.ToolSpec[] }
+---@alias chris468.config.FormattersByFiletype { [string]: string[]|chris468.config.FormatterSpec[] }
 ---@alias chris468.config.Lsps  table<string, { config: vim.lsp.Config, prerequisite?: chris468.config.CheckPrequisite }>
