74a1562912f7235934afcc898c20f521e9ccdabe
Chris Pawling <chris-468@outlook.com>
2025-05-17 09:00:55 -0500

    enable inlay hints

--- dotfiles/private_dot_config/nvim-custom2/plugin/tools.lua
+++ dotfiles/private_dot_config/nvim-custom2/plugin/tools.lua
@@ -1,4 +1,6 @@
+local group = vim.api.nvim_create_augroup("chris468.tools", { clear = true })
 vim.api.nvim_create_autocmd("FileType", {
+  group = group,
   callback = function(arg)
     local filetype = arg.match
     if not vim.list_contains(Chris468.tools.disable, filetype) then
