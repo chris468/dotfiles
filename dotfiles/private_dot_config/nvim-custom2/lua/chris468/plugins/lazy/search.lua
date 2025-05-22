ee3a1317f7fe3c075be4bf0f43f0e86c72db15c4
Chris Pawling <chris-468@outlook.com>
2025-05-22 17:41:31 -0500

    switch search to telescope

--- dotfiles/private_dot_config/nvim-custom2/lua/chris468/plugins/lazy/search.lua
+++ dotfiles/private_dot_config/nvim-custom2/lua/chris468/plugins/lazy/search.lua
@@ -1,26 +1,28 @@
 return {
   {
-    "ibhagwan/fzf-lua",
-    cmd = "FzfLua",
-    dependencies = { "mini.icons" },
+    "nvim-telescope/telescope.nvim",
+    cmd = "Telescope",
+    dependencies = "plenary.nvim",
     keys = {
-      { "<leader>/", "<cmd>FzfLua live_grep<CR>", desc = "Live grep" },
-      { "<leader><leader>", "<cmd>FzfLua files<CR>", desc = "Files" },
-      { "<leader>fb", "<cmd>FzfLua buffers<CR>", desc = "Buffers" },
-      { "<leader>fr", "<cmd>FzfLua oldfiles<CR>", desc = "Recent files" },
-      { "<leader>fz", "<cmd>FzfLua zoxide<CR>", desc = "Zoxide" },
-      { "<leader>gs", "<cmd>FzfLua git_status<CR>", desc = "Git status" },
-      { "<leader>r", "<cmd>FzfLua resume<CR>", desc = "Resume last search" },
-      { "<leader>sh", "<cmd>FzfLua highlights<CR>", desc = "Highlights" },
-      { "<leader>sk", "<cmd>FzfLua keymaps<CR>", desc = "Key maps" },
-      { "<leader>uC", "<cmd>FzfLua colorschemes<CR>", desc = "Change color scheme" },
+      { "<leader>/", "<cmd>Telescope live_grep<CR>", desc = "Live grep" },
+      { "<leader><leader>", "<cmd>Telescope find_files<CR>", desc = "Files" },
+      { "<leader>fb", "<cmd>Telescope buffers<CR>", desc = "Buffers" },
+      { "<leader>fr", "<cmd>Telescope oldfiles<CR>", desc = "Recent files" },
+      -- { "<leader>fz", "<cmd>Telescope zoxide<CR>", desc = "Zoxide" },
+      { "<leader>gs", "<cmd>Telescope git_status<CR>", desc = "Git status" },
+      { "<leader>r", "<cmd>Telescope resume<CR>", desc = "Resume last search" },
+      { "<leader>sh", "<cmd>Telescope highlights<CR>", desc = "Highlights" },
+      { "<leader>sk", "<cmd>Telescope keymaps<CR>", desc = "Key maps" },
+      { "<leader>uC", "<cmd>Telescope colorschemes<CR>", desc = "Change color scheme" },
     },
     opts = {
-      { "hide" },
-      winopts = {
-        preview = {
-          layout = "vertical",
+      defaults = {
+        layout_config = {
+          prompt_position = "top",
+          mirror = true,
         },
+        layout_strategy = "vertical",
+        sorting_strategy = "ascending",
       },
     },
   },
