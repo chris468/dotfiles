9ceb7d2fae53d2e056fc8877eb7e84eb5e23d524
Chris Pawling <chris-468@outlook.com>
2025-05-23 18:32:41 -0500

    refactor grep

--- dotfiles/private_dot_config/nvim-custom2/lua/chris468/plugins/config/search.lua
+++ dotfiles/private_dot_config/nvim-custom2/lua/chris468/plugins/config/search.lua
@@ -6,34 +6,78 @@ local actions_state = require("telescope.actions.state")
 
 local M = {}
 
-local defaults = {
-  search_hidden_files = false,
-  search_ignored_files = false,
-  default_globs = {
-    "!**/.git/**",
-    "!**/node_modules/**",
-    "!**/bin/**",
-    "!**/obj/**",
-    "!**/.venv/**",
-  },
+local default_globs = {
+  "!**/.git/**",
+  "!**/node_modules/**",
+  "!**/bin/**",
+  "!**/obj/**",
+  "!**/.venv/**",
 }
 
+---@param args string[]
+---@param new_args string|string[]
+---@param opts? { append?: boolean, flag?: string }
+local function append_args(args, new_args, opts)
+  opts = opts or {}
+  if opts.append == false then
+    return
+  end
+
+  new_args = type(new_args) == "table" and new_args or { new_args }
+  vim.list_extend(
+    args,
+    vim
+      .iter(new_args)
+      :filter(function(v)
+        return v and v ~= ""
+      end)
+      :map(function(v)
+        return opts.flag and { opts.flag, v } or v
+      end)
+      :flatten()
+      :totable()
+  )
+end
+
+---@param find fun(opts: table)
+---@param update_opts fun(): table opts
+---@return fun()
+function M.change_opts(find, update_opts)
+  return function()
+    local opts = update_opts()
+    opts.default_text = actions_state.get_current_line()
+    find(opts)
+  end
+end
+
 function M.grep(opts)
+  local defaults = {
+    search_hidden_files = false,
+    search_ignored_files = false,
+    default_globs = default_globs,
+  }
+
   opts = vim.tbl_deep_extend("keep", opts or {}, defaults)
   opts.cwd = opts.cwd or vim.uv.cwd()
 
   opts.attach_mappings = function(_, map)
-    map({ "i", "n" }, "<C-E>i", function()
-      opts.search_ignored_files = not opts.search_ignored_files
-      opts.default_text = actions_state.get_current_line()
-      M.grep(opts)
-    end)
+    map(
+      { "i", "n" },
+      "<C-E>i",
+      M.change_opts(M.grep, function()
+        opts.search_ignored_files = not opts.search_ignored_files
+        return opts
+      end)
+    )
 
-    map({ "i", "n" }, "<C-e>h", function()
-      opts.search_hidden_files = not opts.search_hidden_files
-      opts.default_text = actions_state.get_current_line()
-      M.grep(opts)
-    end)
+    map(
+      { "i", "n" },
+      "<C-E>h",
+      M.change_opts(M.grep, function()
+        opts.search_hidden_files = not opts.search_hidden_files
+        return opts
+      end)
+    )
 
     return true
   end
@@ -44,38 +88,24 @@ function M.grep(opts)
         return nil
       end
 
-      local args = { "rg" }
+      local args =
+        { "rg", "--color=never", "--no-heading", "--with-filename", "--line-number", "--column", "--smart-case" }
 
-      for _, glob in ipairs(opts.default_globs) do
-        vim.list_extend(args, { "--glob", glob })
-      end
+      append_args(args, opts.default_globs, { flag = "--glob" })
 
       local pieces = vim.split(prompt, "  ")
-      if pieces[1] then
-        vim.list_extend(args, { "-e", pieces[1] })
-      end
+      append_args(args, pieces[1])
 
       if pieces[2] and pieces[2] ~= "" then
         local globs = vim.split(pieces[2], "|")
-        for _, glob in ipairs(globs) do
-          if glob and glob ~= "" then
-            vim.list_extend(args, { "--glob", glob })
-          end
-        end
+        append_args(args, globs, { flag = "--glob" })
       end
 
-      if opts.search_hidden_files then
-        vim.list_extend(args, { "--hidden" })
-      end
+      append_args(args, "--hidden", { append = opts.search_hidden_files })
 
-      if not opts.search_ignored_files then
-        vim.list_extend(args, { "--no-ignore" }) -- don't respect .gitignore, etc.
-      end
+      -- --no-ignore means don't respect .gitignore, etc. - double negative
+      append_args(args, "--no-ignore", { append = not opts.search_ignored_files })
 
-      vim.list_extend(
-        args,
-        { "--color=never", "--no-heading", "--with-filename", "--line-number", "--column", "--smart-case" }
-      )
       return args
     end,
     entry_maker = make_entry.gen_from_vimgrep(opts),
