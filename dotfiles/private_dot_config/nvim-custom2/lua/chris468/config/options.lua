c7eb3cd872613e8d55995b4b30afdbea0b2e68a4
Chris Pawling <chris-468@outlook.com>
2025-05-24 18:27:05 -0500

    extract tool config to separate module

--- dotfiles/private_dot_config/nvim-custom2/lua/chris468/config/options.lua
+++ dotfiles/private_dot_config/nvim-custom2/lua/chris468/config/options.lua
@@ -38,60 +38,7 @@ local chris468 = {
     work = chezmoi.work,
     theme = chezmoi.theme,
   },
-  tools = {
-    ---@type string[] disable tooling on these filetypes
-    disable = { "lazy", "lazy_backdrop", "mason" },
-    ---@type { provider: "codeium"|"copilot" }
-    ai = {
-      provider = chezmoi.work and "copilot" or "codeium",
-    },
-    ---@type chris468.config.FormattersByFiletype
-    formatters = {
-      cs = { { "csharpier", install = false } },
-      lua = { "stylua" },
-      python = { "black" },
-    },
-    ---@type chris468.config.ToolsByFiletype
-    linters = {
-      ["yaml.ansible"] = { "ansible-lint" },
-      markdown = { "markdownlint-cli2" },
-    },
-    ---@type chris468.config.Lsps
-    lsps = {
-      lua_ls = {
-        config = {
-          settings = {
-            Lua = {
-              completion = {
-                callSnippet = "Replace",
-              },
-              hint = {
-                enable = true,
-                setType = false,
-                paramType = true,
-                paramName = "Disable",
-                semicolon = "Disable",
-                arrayIndex = "Disable",
-              },
-            },
-          },
-        },
-      },
-      omnisharp = {},
-      pyright = {},
-      yamlls = {
-        config = {
-          settings = {
-            yaml = {
-              format = {
-                enable = false,
-              },
-            },
-          },
-        },
-      },
-    },
-  },
+  tools = require("chris468.config.tools"),
   ui = {
     icons = {
       error = "󰅚",
---
+++ dotfiles/private_dot_config/nvim-custom2/lua/chris468/config/tools.lua
@@ -0,0 +1,56 @@
+local chezmoi = require("chris468.config.chezmoi")
+
+return {
+  ---@type string[] disable tooling on these filetypes
+  disable = { "lazy", "lazy_backdrop", "mason" },
+  ---@type { provider: "codeium"|"copilot" }
+  ai = {
+    provider = chezmoi.work and "copilot" or "codeium",
+  },
+  ---@type chris468.config.FormattersByFiletype
+  formatters = {
+    cs = { { "csharpier", install = false } },
+    lua = { "stylua" },
+    python = { "black" },
+  },
+  ---@type chris468.config.ToolsByFiletype
+  linters = {
+    ["yaml.ansible"] = { "ansible-lint" },
+    markdown = { "markdownlint-cli2" },
+  },
+  ---@type chris468.config.Lsps
+  lsps = {
+    lua_ls = {
+      config = {
+        settings = {
+          Lua = {
+            completion = {
+              callSnippet = "Replace",
+            },
+            hint = {
+              enable = true,
+              setType = false,
+              paramType = true,
+              paramName = "Disable",
+              semicolon = "Disable",
+              arrayIndex = "Disable",
+            },
+          },
+        },
+      },
+    },
+    omnisharp = {},
+    pyright = {},
+    yamlls = {
+      config = {
+        settings = {
+          yaml = {
+            format = {
+              enable = false,
+            },
+          },
+        },
+      },
+    },
+  },
+}
