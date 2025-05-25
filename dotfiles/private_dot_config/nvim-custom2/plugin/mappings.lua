28cacddfde9ec60d6d4d6790ab2ae249d8b67e0f
Chris Pawling <chris-468@outlook.com>
2025-05-25 18:52:35 -0500

    map on very lazy for real

--- dotfiles/private_dot_config/nvim-custom2/plugin/mappings.lua
+++ dotfiles/private_dot_config/nvim-custom2/plugin/mappings.lua
@@ -1,4 +1,5 @@
 vim.api.nvim_create_autocmd("User", {
+  pattern = "VeryLazy",
   group = vim.api.nvim_create_augroup("chris468.mappings", {}),
   callback = function()
     require("chris468.config.mappings")
