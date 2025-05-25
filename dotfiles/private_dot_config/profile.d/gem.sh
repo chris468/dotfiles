469c84cb5de69a47e3677a678ada92934db01c00
Chris Pawling <chris-468@outlook.com>
2025-05-25 08:23:43 -0500

    shell: fix gem dir detection for old mac version

--- dotfiles/private_dot_config/profile.d/gem.sh
+++ dotfiles/private_dot_config/profile.d/gem.sh
@@ -1,6 +1,13 @@
 if command -v gem &>/dev/null; then
-  _gem_dir="$(gem environment user_gemhome)/bin"
-  if [[ $PATH != *$(gem environment user_gemhome)/bin:* ]] && [[ -d "$_gem_dir" ]]; then
+  if ! _gem_dir="$(gem environment user_gemhome 2>/dev/null)/bin"; then
+    for __candidate in $(gem environment gempath 2>/dev/null | tr ":" "\n "); do
+      if [[ "$__candidate" == "$HOME"* ]]; then
+        _gem_dir="$__candidate/bin"
+        break
+      fi
+    done
+  fi
+  if [[ -n "$_gem_dir" ]] && [[ $PATH != *"$_gem_dir"* ]] && [[ -d "$_gem_dir" ]]; then
     export PATH="$_gem_dir:$PATH"
   fi
   unset _gem_dir
