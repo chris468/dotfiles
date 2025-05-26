54667ca55f604a0fa130e17c3a9d047efc8efc28
Chris Pawling <chris-468@outlook.com>
2025-05-26 12:07:13 -0500

    add tf, lazy, zoxide telescope extensions

--- dotfiles/private_dot_config/nvim-custom2/lua/chris468/plugins/lazy/search.lua
+++ dotfiles/private_dot_config/nvim-custom2/lua/chris468/plugins/lazy/search.lua
@@ -1,6 +1,8 @@
 local util = require("chris468.util")
 local util_lazy = require("chris468.util.lazy")
 
+local have_zoxide = vim.fn.executable("zoxide") == 1
+
 return {
   {
     "nvim-telescope/telescope.nvim",
@@ -8,30 +10,46 @@ return {
     config = function(_, opts)
       local telescope = require("telescope")
       telescope.setup(opts)
-      if util_lazy.has_plugin("noice.nvim") then
-        telescope.load_extension("noice")
+      for k, v in pairs(opts.extensions or {}) do
+        if v.auto_load ~= false then
+          pcall(telescope.load_extension, k)
+        end
       end
     end,
-    dependencies = "plenary.nvim",
-    keys = {
-      { "<leader>/", require("chris468.plugins.config.search.grep"), desc = "Live grep" },
-      { "<leader><leader>", "<cmd>Telescope find_files<CR>", desc = "Files" },
-      { "<leader>fb", "<cmd>Telescope buffers<CR>", desc = "Buffers" },
-      { "<leader>fr", "<cmd>Telescope oldfiles<CR>", desc = "Recent files" },
-      -- { "<leader>fz", "<cmd>Telescope zoxide<CR>", desc = "Zoxide" },
-      { "<leader>gs", "<cmd>Telescope git_status<CR>", desc = "Git status" },
-      { "<leader>r", "<cmd>Telescope resume<CR>", desc = "Resume last search" },
-      { "<leader>sh", "<cmd>Telescope highlights<CR>", desc = "Highlights" },
-      { "<leader>sk", "<cmd>Telescope keymaps<CR>", desc = "Key maps" },
-      { "<leader>su", require("chris468.plugins.config.search.unicode"), desc = "Unicode symbols" },
-      {
-        "<C-R><C-U>",
-        util.wrap(require("chris468.plugins.config.search.unicode"), { mode = "i" }),
-        desc = "Unicode symbols",
-        mode = "i",
-      },
-      { "<leader>uC", "<cmd>Telescope colorschemes<CR>", desc = "Change color scheme" },
+    dependencies = {
+      "plenary.nvim",
+      "noice.nvim",
+      "ANGkeith/telescope-terraform-doc.nvim",
+      "jvgrootveld/telescope-zoxide",
+      "tsakirist/telescope-lazy.nvim",
     },
+    keys = function()
+      local keys = {
+        { "<leader>/", require("chris468.plugins.config.search.grep"), desc = "Live grep" },
+        { "<leader><leader>", "<cmd>Telescope find_files<CR>", desc = "Files" },
+        { "<leader>fb", "<cmd>Telescope buffers<CR>", desc = "Buffers" },
+        { "<leader>fr", "<cmd>Telescope oldfiles<CR>", desc = "Recent files" },
+        { "<leader>gs", "<cmd>Telescope git_status<CR>", desc = "Git status" },
+        { "<leader>r", "<cmd>Telescope resume<CR>", desc = "Resume last search" },
+        { "<leader>sh", "<cmd>Telescope highlights<CR>", desc = "Highlights" },
+        { "<leader>sk", "<cmd>Telescope keymaps<CR>", desc = "Key maps" },
+        { "<leader>sT", "<cmd>Telescope terraform_doc", desc = "Terraform docs" },
+        { "<leader>su", require("chris468.plugins.config.search.unicode"), desc = "Unicode symbols" },
+        {
+          "<C-R><C-U>",
+          util.wrap(require("chris468.plugins.config.search.unicode"), { mode = "i" }),
+          desc = "Unicode symbols",
+          mode = "i",
+        },
+        { "<leader>uC", "<cmd>Telescope colorschemes<CR>", desc = "Change color scheme" },
+      }
+
+      if have_zoxide then
+        table.insert(keys, { "<leader>fz", "<cmd>Telescope zoxide list<CR>", desc = "Zoxide" })
+      end
+
+      return keys
+    end,
     opts = {
       defaults = {
         layout_config = {
@@ -40,6 +58,16 @@ return {
         },
         layout_strategy = "vertical",
         sorting_strategy = "ascending",
+      },
+      extensions = {
+        noice = {
+          enabled = util_lazy.has_plugin("noice.nvim"),
+        },
+        terraform_doc = {},
+        lazy = {},
+        zoxide = {
+          enabled = have_zoxide,
+        },
       },
     },
     version = false,
