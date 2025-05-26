19d91082d19feef4a8dc5caffa130f09f8c8d253
Chris Pawling <chris-468@outlook.com>
2025-05-25 19:43:00 -0500

    more mappings

--- dotfiles/private_dot_config/nvim-custom2/lua/chris468/config/mappings.lua
+++ dotfiles/private_dot_config/nvim-custom2/lua/chris468/config/mappings.lua
@@ -1,5 +1,6 @@
 local snacks = require("snacks")
 local whichkey = require("which-key")
+local util = require("chris468.util")
 local util_lua = require("chris468.util.lua")
 
 local mappings = {
@@ -10,6 +11,12 @@ local mappings = {
   { "<leader>bd", snacks.bufdelete.delete, desc = "Delete buffer" },
   { "<leader>bo", snacks.bufdelete.other, desc = "Delete buffer" },
   { "<leader>c", group = "Code" },
+  {
+    "<leader>cd",
+    "<cmd>Trouble diagnostics follow=true<CR>",
+    desc = "Diagnostics (Trouble)",
+  },
+  { "<leader>cl", vim.diagnostic.open_float, desc = "Line diagnostic" },
   { "<leader>f", group = "Files" },
   { "<leader>g", group = "Git" },
   { "<leader>l", group = "Lua", icon = "󰢱" },
@@ -18,6 +25,44 @@ local mappings = {
   { "<leader>u", group = "UI" },
   { "j", "gj", hidden = true },
   { "k", "gk", hidden = true },
+}
+
+local lsp_mappings = {
+  {
+    "ci",
+    "<cmd>Trouble lsp_incoming_calls follow=false<CR>",
+    desc = "Incoming calls (Trouble)",
+  },
+  {
+    "co",
+    "<cmd>Trouble lsp_outgoing_calls follow=false<CR>",
+    desc = "Outgoing calls (Trouble)",
+  },
+  {
+    "gd",
+    "<cmd>Trouble lsp_definitions follow=false<CR>",
+    desc = "Go to definition (Trouble)",
+  },
+  {
+    "gD",
+    "<cmd>Trouble lsp_declarations follow=false<CR>",
+    desc = "Go to declarations (Trouble)",
+  },
+  {
+    "gI",
+    "<cmd>Trouble lsp_implementations follow=false<CR>",
+    desc = "Go to implementation (Trouble)",
+  },
+  {
+    "gr",
+    "<cmd>Trouble lsp_references follow=false<CR>",
+    desc = "Find references (Trouble)",
+  },
+  {
+    "gy",
+    "<cmd>Trouble lsp_type_definitions follow=false<CR>",
+    desc = "Go to type definition (Trouble)",
+  },
 }
 
 whichkey.add(mappings)
@@ -36,3 +81,16 @@ snacks.toggle
     end,
   })
   :map("<leader>c<C-F>")
+
+vim.api.nvim_create_autocmd("LspAttach", {
+  group = vim.api.nvim_create_augroup("chris468.mappings.lsp", {}),
+  callback = function(args)
+    local bufnr = args.buf
+    for _, mapping in ipairs(lsp_mappings) do
+      vim.keymap.set(mapping.mode or "n", mapping[1], mapping[2], {
+        buffer = bufnr,
+        desc = mapping.desc,
+      })
+    end
+  end,
+})
