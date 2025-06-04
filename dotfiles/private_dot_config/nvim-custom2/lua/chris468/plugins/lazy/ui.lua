41b5ac6b75535f53d5a4d555b4955b33f573b722
Chris Pawling <chris-468@outlook.com>
2025-06-03 20:46:09 -0500

    status line for telescope

--- dotfiles/private_dot_config/nvim-custom2/lua/chris468/plugins/lazy/ui.lua
+++ dotfiles/private_dot_config/nvim-custom2/lua/chris468/plugins/lazy/ui.lua
@@ -65,6 +65,35 @@ return {
             },
           },
         },
+        {
+          filetypes = { "TelescopePrompt" },
+          sections = {
+            lualine_a = {
+              function()
+                local state = require("telescope.actions.state")
+                local buf = vim.api.nvim_get_current_buf()
+                local picker = state.get_current_picker(buf)
+                local name = vim.trim(picker.prompt_prefix or "")
+                name = name .. (#name and " " or "") .. (picker.prompt_title or " ")
+
+                return name
+              end,
+            },
+            lualine_b = {
+              function()
+                return string.format(" %s", vim.fn.fnamemodify(vim.fn.getcwd(), ":p:~"))
+              end,
+            },
+          },
+        },
+      },
+      inactive_sections = {
+        lualine_a = {},
+        lualine_b = {},
+        lualine_c = { "filename" },
+        lualine_x = { "location" },
+        lualine_y = {},
+        lualine_z = {},
       },
       sections = {
         lualine_a = { { "mode", fmt = config.format.mode } },
@@ -78,14 +107,6 @@ return {
           { "encoding", fmt = config.format.encoding },
         },
         lualine_z = { "progress", "location" },
-      },
-      inactive_sections = {
-        lualine_a = {},
-        lualine_b = {},
-        lualine_c = { "filename" },
-        lualine_x = { "location" },
-        lualine_y = {},
-        lualine_z = {},
       },
     },
     version = false,
