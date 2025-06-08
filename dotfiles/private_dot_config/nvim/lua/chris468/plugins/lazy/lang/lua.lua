edd89e1b4263a8e6b5ba2dc4a24c60c4a1b9c8ba
Chris Pawling <chris-468@outlook.com>
2025-06-08 12:42:52 -0500

    lua lsp to new config

--- dotfiles/private_dot_config/nvim/lua/chris468/plugins/lazy/lang/lua.lua
+++ dotfiles/private_dot_config/nvim/lua/chris468/plugins/lazy/lang/lua.lua
@@ -1,5 +1,29 @@
 return {
   {
+    "nvim-lspconfig",
+    opts = {
+      lua_ls = {
+        cfg = {
+          settings = {
+            Lua = {
+              completion = {
+                callSnippet = "Replace",
+              },
+              hint = {
+                enable = true,
+                setType = false,
+                paramType = true,
+                paramName = "Disable",
+                semicolon = "Disable",
+                arrayIndex = "Disable",
+              },
+            },
+          },
+        },
+      },
+    },
+  },
+  {
     "folke/lazydev.nvim",
     cmd = { "LazyDev" },
     ft = { "lua" },
