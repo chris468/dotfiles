c0f38822afb97b2a15094cf095199a9c03a7ac23
Chris Pawling <chris-468@outlook.com>
2025-06-08 20:30:53 -0500

    nvim: clean up a bit

--- dotfiles/private_dot_config/nvim/lua/chris468/plugins/config/tools.lua
+++ dotfiles/private_dot_config/nvim/lua/chris468/plugins/config/tools.lua
@@ -87,23 +87,65 @@ local function register_lint(linters_by_ft)
   })
 end
 
----- SIMPLIFIED (?)
+---@class chris468.config._CachedLspConfig
+---@field server_name string
+---@field filetypes { [string]: true }
+---@field public package? Package
+---@field lspconfig vim.lsp.Config
+---@field display_name string
+
+---@param opts chris468.config.LspConfig
+---@return chris468.config._CachedLspConfig
+local function cached_lspconfig_info(opts)
+  return setmetatable({}, {
+    __index = function(tbl, name)
+      local config = opts[name]
+      if not config or config.enabled == false then
+        tbl[name] = { server_name = name, filetypes = {}, lspconfig = {}, "LSP " .. name }
+      else
+        local package
+        if config.package ~= false then
+          local ok, p = pcall(require("mason-registry").get_package, name)
+          package = ok and p or nil
+        end
+
+        local server_name = config.name or vim.tbl_get(package or {}, "spec", "neovim", "lspconfig") or name
+        local filetypes =
+          util.make_set((config.lspconfig or {}).filetypes or vim.lsp.config[server_name].filetypes or {})
+        local display_name =
+          string.format("LSP %s%s", name, name == server_name and "" or string.format("(%s) ", server_name))
+
+        tbl[name] = {
+          server_name = server_name,
+          filetypes = filetypes,
+          package = package,
+          lspconfig = config.lspconfig,
+          display_name = display_name,
+        }
+      end
+
+      return tbl[name]
+    end,
+  })
+end
+
+---@param info chris468.config._CachedLspConfig
+---@param bufnr integer
+local function enable_and_install_lsp(info, bufnr)
+  vim.lsp.config(info.server_name, merge_completion_capabilities(info.lspconfig))
+  vim.lsp.enable(info.server_name)
+  util_mason.install(info.package, function()
+    raise_filetype(bufnr)
+  end, function()
+    vim.lsp.disable(info.server_name)
+  end, info.display_name)
+end
+
 --- @param opts chris468.config.LspConfig
-function M.lspconfig(opts)
-  local group = vim.api.nvim_create_augroup("chris468.tools.lsp", { clear = true })
-  register_lsp_attach(group)
-  register_dynamic_capability_handlers()
-
+--- @param group integer
+local function lazily_install_lsps_by_filetype(opts, group)
   local _ = require("lspconfig")
-  local cache = {}
-
-  local configs = vim.iter(opts):fold({}, function(result, k, v)
-    if v.enabled ~= false then
-      result[k] = v
-    end
-    return result
-  end)
-
+  local infos = cached_lspconfig_info(opts)
   local handled_filetypes = util.make_set(Chris468.disable_filetypes)
   vim.api.nvim_create_autocmd("FileType", {
     group = group,
@@ -115,38 +157,22 @@ function M.lspconfig(opts)
 
       handled_filetypes[filetype] = true
 
-      for name, config in pairs(configs) do
-        if not cache[name] then
-          local package
-          if config.package ~= false then
-            local ok, p = pcall(require("mason-registry").get_package, name)
-            package = ok and p or nil
-          end
-
-          local server_name = config.name or vim.tbl_get(package or {}, "spec", "neovim", "lspconfig") or name
-          local filetypes =
-            util.make_set((config.lspconfig or {}).filetypes or vim.lsp.config[server_name].filetypes or {})
-          cache[name] = { server_name = server_name, filetypes = filetypes, package = package }
-        end
-
-        if cache[name].filetypes[filetype] then
-          local package = cache[name].package
-          local server_name = cache[name].server_name
-          vim.lsp.config(server_name, merge_completion_capabilities(config.lspconfig))
-          vim.lsp.enable(server_name)
-          util_mason.install(package, function()
-            raise_filetype(arg.buf)
-          end, function()
-            vim.lsp.disable(server_name)
-          end, string.format(
-            "LSP %s%s",
-            name,
-            name == server_name and "" or string.format("(%s) ", server_name)
-          ))
+      for name, _ in pairs(opts) do
+        local info = infos[name]
+        if info.filetypes[filetype] then
+          enable_and_install_lsp(info, arg.buf)
         end
       end
     end,
   })
+end
+
+--- @param opts chris468.config.LspConfig
+function M.lspconfig(opts)
+  local group = vim.api.nvim_create_augroup("chris468.tools.lsp", { clear = true })
+  register_lsp_attach(group)
+  register_dynamic_capability_handlers()
+  lazily_install_lsps_by_filetype(opts, group)
 end
 
 ---@param tools_by_ft chris468.config.FormattersByFileType
@@ -171,12 +197,33 @@ local function normalize_tools_by_ft(tools_by_ft, disabled_filetypes)
   end)
 end
 
+local function cached_tool_info(tool_type)
+  local cache = {}
+  return function(config)
+    local name = config[1]
+    if not cache[name] then
+      local package
+      if config.package ~= false then
+        local ok, p = pcall(require("mason-registry").get_package, config.package or name)
+        package = ok and p or nil
+      end
+
+      local package_name = package and package.spec.name or name
+      local display_name =
+        string.format("%s %s%s", tool_type, package_name, name == package_name and "" or string.format(" (%s)", name))
+      cache[name] = { name = name, package = package, package_name = package_name, display_name = display_name }
+    end
+
+    return cache[name]
+  end
+end
+
 ---@param config_by_ft { [string]: chris468.config.Formatter[] }
 ---@param disabled_filetypes { [string]: true }
 ---@param tool_type string
 local function lazily_install_tools_by_filetype(config_by_ft, disabled_filetypes, tool_type)
   local handled_filetypes = disabled_filetypes
-  local cache = {}
+  local cache = cached_tool_info(tool_type)
 
   vim.api.nvim_create_autocmd("FileType", {
     group = vim.api.nvim_create_augroup("chris468." .. tool_type, { clear = true }),
@@ -188,26 +235,10 @@ local function lazily_install_tools_by_filetype(config_by_ft, disabled_filetypes
       handled_filetypes[filetype] = true
 
       for _, c in ipairs(config_by_ft[filetype] or {}) do
-        local name = c[1]
-        if not cache[name] then
-          local package
-          if c.package ~= false then
-            local ok, p = pcall(require("mason-registry").get_package, c.package or name)
-            package = ok and p or nil
-          end
-
-          cache[name] = { package = package, package_name = package and package.spec.name or name }
-        end
-
-        local package, package_name = cache[name].package, cache[name].package_name
-        util_mason.install(
-          package,
-          function()
-            raise_filetype(arg.buf)
-          end,
-          nil,
-          string.format("%s %s%s", tool_type, package_name, name == package_name and "" or string.format(" (%s)", name))
-        )
+        local info = cache(c)
+        util_mason.install(info.package, function()
+          raise_filetype(arg.buf)
+        end, nil, info.display_name)
       end
     end,
   })
