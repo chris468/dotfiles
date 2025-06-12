5a32684bf9071da8f640a760071ddaaa18dcdee9
Chris Pawling <chris-468@outlook.com>
2025-06-11 21:25:50 -0500

    theory

--- dotfiles/private_dot_config/nvim/tests/tool.lua
+++ dotfiles/private_dot_config/nvim/tests/tool.lua
@@ -8,31 +8,54 @@ package.loaded["mason-registry"] = registry
 
 local tool = require("chris468.plugins.config.tools.tool")
 
+local tool_factories = {
+  Tool = function(...)
+    return tool.Tool:new("test_type", ...)
+  end,
+  LspTool = function(...)
+    return tool.LspTool:new(...)
+  end,
+}
+
+---@param func fun(factory: fun(...): AbstractTool))
+local function all_tools(func)
+  for name, factory in pairs(tool_factories) do
+    describe(name, function()
+      func(factory)
+    end)
+  end
+end
+
 describe("tool", function()
-  describe("generic tool", function()
-    describe("name", function()
+  describe("name", function()
+    all_tools(function(factory)
       it("should be name when tool_name is unset", function()
-        local t = tool.Tool:new("formatter", "form")
-        assert.are.equal("form", t:name())
-      end)
-      it("should be tool_name when specified", function()
-        local t = tool.Tool:new("formatter", "form", { tool_name = "tool_name" })
-        assert.are.equal("tool_name", t:name())
+        local t = factory("test tool")
+        assert.are.equal("test tool", t:name())
       end)
     end)
 
-    describe("enabled", function()
-      it("should be enabled when enabled is unset", function()
-        local t = tool.Tool:new("formatter", "form")
-        assert.are.equal(true, t.enabled)
+    describe("Tool", function()
+      it("should be tool_name when specified", function()
+        local t = tool.Tool:new("test type", "test tool", { tool_name = "test tool name" })
+        assert.are.equal("test tool name", t:name())
       end)
-      it("should be enabled when enabled is true", function()
-        local t = tool.Tool:new("formatter", "form", { enabled = true })
-        assert.are.equal(true, t.enabled)
-      end)
-      it("should be disabled when enabled is false", function()
-        local t = tool.Tool:new("formatter", "form", { enabled = false })
-        assert.are.equal(false, t.enabled)
+    end)
+
+    all_tools(function(factory)
+      describe("enabled", function()
+        it("should be enabled when enabled is unset", function()
+          local t = factory("test tool")
+          assert.are.equal(true, t.enabled)
+        end)
+        it("should be enabled when enabled is true", function()
+          local t = factory("test tool", { enabled = true })
+          assert.are.equal(true, t.enabled)
+        end)
+        it("should be disabled when enabled is false", function()
+          local t = factory("test tool", { enabled = false })
+          assert.are.equal(false, t.enabled)
+        end)
       end)
     end)
   end)
