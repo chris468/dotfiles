1628892e6f62f7d323afabaabe01b5ee3f196fd0
Chris Pawling <chris-468@outlook.com>
2025-06-11 20:46:37 -0500

    take optional opts as a table

--- dotfiles/private_dot_config/nvim/lua/chris468/plugins/config/tools/tool.lua
+++ dotfiles/private_dot_config/nvim/lua/chris468/plugins/config/tools/tool.lua
@@ -1,25 +1,35 @@
 local AbstractTool = require("chris468.plugins.config.tools.abstract_tool").AbstractTool
 
+---@class Tool.Options : AbstractTool.Options
+---@field tool_name? string
+
 ---@class Tool : AbstractTool
 ---@field protected super AbstractTool
 ---@field private _tool_name string?
----@field new fun(self: Tool, tool_type: string, name: string, enabled?: boolean, package?: boolean, filetypes?: string[], tool_name?: string) : Tool
+---@field new fun(self: Tool, tool_type: string, name: string, opts?: Tool.Options) : Tool
 local Tool = AbstractTool:extend("Tool") --[[ @as Tool ]]
-function Tool:new(tool_type, name, enabled, package, filetypes, tool_name)
-  return Tool.super.new(self, tool_type, name, enabled, package, filetypes, { _tool_name = tool_name }) --[[ @as Tool ]]
+function Tool:new(tool_type, name, opts)
+  opts = opts or {}
+  return Tool.super.new(self, tool_type, name, opts, { _tool_name = opts.tool_name }) --[[ @as Tool ]]
 end
 
 function Tool:name()
   return self._tool_name or self._package_name
 end
 
+---@class LspTool.Options
+---@field enabled? boolean
+---@field public package? boolean
+---@field lspconfig? vim.lsp.Config
+
 ---@class LspTool : AbstractTool
 ---@field protected super AbstractTool
 ---@field lspconfig vim.lsp.Config
----@field new fun(self: LspTool, name: string, enabled?: boolean, package?: boolean, lspconfig?: vim.lsp.Config, ...: table) : LspTool
+---@field new fun(self: LspTool, name: string, opts?: LspTool.Options, ...: table) : LspTool
 local LspTool = AbstractTool:extend("LspTool") --[[ @as LspTool ]]
-function LspTool:new(name, enabled, package, lspconfig)
-  return LspTool.super.new(self, "LSP", name, enabled, package, nil, { _lspconfig = lspconfig }) --[[ @as LspTool ]]
+function LspTool:new(name, opts)
+  opts = opts or {}
+  return LspTool.super.new(self, "LSP", name, opts --[[ @as AbstractTool.Options ]], { _lspconfig = opts.lspconfig }) --[[ @as LspTool ]]
 end
 
 function LspTool:_tool_filetypes()
