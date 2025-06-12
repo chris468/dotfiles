478cbf42838f365fbe4c4301a10c5f3541c54c19
Chris Pawling <chris-468@outlook.com>
2025-06-11 20:27:02 -0500

    start some testing

---
+++ dotfiles/private_dot_config/nvim/tests/tool.lua
@@ -0,0 +1,39 @@
+local registry = {}
+
+function registry.get_package(name)
+  return {}
+end
+
+package.loaded["mason-registry"] = registry
+
+local tool = require("chris468.plugins.config.tools.tool")
+
+describe("tool", function()
+  describe("generic tool", function()
+    describe("name", function()
+      it("should be name when tool_name is unset", function()
+        local t = tool.Tool:new("formatter", "form", nil, nil, nil, nil)
+        assert.are.equal("form", t:name())
+      end)
+      it("should be tool_name when specified", function()
+        local t = tool.Tool:new("formatter", "form", nil, nil, nil, "tool_name")
+        assert.are.equal("tool_name", t:name())
+      end)
+    end)
+
+    describe("enabled", function()
+      it("should be enabled when enabled is unset", function()
+        local t = tool.Tool:new("formatter", "form", nil, nil, nil, nil)
+        assert.are.equal(true, t.enabled)
+      end)
+      it("should be enabled when enabled is true", function()
+        local t = tool.Tool:new("formatter", "form", true, nil, nil, nil)
+        assert.are.equal(true, t.enabled)
+      end)
+      it("should be disabled when enabled is false", function()
+        local t = tool.Tool:new("formatter", "form", false, nil, nil, nil)
+        assert.are.equal(false, t.enabled)
+      end)
+    end)
+  end)
+end)
