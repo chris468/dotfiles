20020a807b31a522203a7240edc8b35b0a78f849
Chris Pawling <chris-468@outlook.com>
2025-06-11 19:24:46 -0500

    no errors, lsps load, but i don't think formatters. not black at least

--- dotfiles/private_dot_config/nvim/lua/chris468/plugins/config/tools/tool.lua
+++ dotfiles/private_dot_config/nvim/lua/chris468/plugins/config/tools/tool.lua
@@ -1,10 +1,10 @@
-local AbstractTool = require("chris468.plugins.config.tools.abstract_tool")
+local AbstractTool = require("chris468.plugins.config.tools.abstract_tool").AbstractTool
 
 ---@class Tool : AbstractTool
 ---@field protected super AbstractTool
 ---@field private _tool_name string?
 ---@field new fun(self: Tool, tool_type: string, name: string, enabled?: boolean, package?: boolean, filetypes?: string[], tool_name?: string) : Tool
-local Tool = AbstractTool:extend("Tool")
+local Tool = AbstractTool:extend("Tool") --[[ @as Tool ]]
 function Tool:new(tool_type, name, enabled, package, filetypes, tool_name)
   return Tool.super.new(self, tool_type, name, enabled, package, filetypes, { _tool_name = tool_name }) --[[ @as Tool ]]
 end
@@ -17,13 +17,13 @@ end
 ---@field protected super AbstractTool
 ---@field lspconfig vim.lsp.Config
 ---@field new fun(self: LspTool, name: string, enabled?: boolean, package?: boolean, lspconfig?: vim.lsp.Config, ...: table) : LspTool
-local LspTool = AbstractTool:extend("LspTool")
+local LspTool = AbstractTool:extend("LspTool") --[[ @as LspTool ]]
 function LspTool:new(name, enabled, package, lspconfig)
   return LspTool.super.new(self, "LSP", name, enabled, package, nil, { _lspconfig = lspconfig }) --[[ @as LspTool ]]
 end
 
 function LspTool:_tool_filetypes()
-  return (self.lspconfig or {}).filetypes or (vim.config[self:name()] or {}).filetypes
+  return (self.lspconfig or {}).filetypes or (vim.lsp.config[self:name()] or {}).filetypes
 end
 
 return {
