f824af2a8fbd13494a9b4c52908760eff62ae5a7
Chris Pawling <chris-468@outlook.com>
2025-06-12 18:28:53 -0500

    pull _tool_name up

--- dotfiles/private_dot_config/nvim/lua/chris468/util/object.lua
+++ dotfiles/private_dot_config/nvim/lua/chris468/util/object.lua
@@ -1,14 +1,15 @@
 local class = require("plenary").class
 
 ---@class chris468.Object
----@field new fun(self: chris468.Object, ...: table) : chris468.Object
+---@field new fun(self: chris468.Object, state: table, ...: table) : chris468.Object
 ---@field protected super chris468.Object
 ---@field protected _abstract fun(self: chris468.Object, method: string)
 local Object = class:extend()
 Object._type = "Object"
 
-function Object:new(...)
-  return setmetatable(vim.tbl_extend("error", ...), self)
+function Object:new(state, ...)
+  local o = {}
+  return setmetatable(vim.tbl_extend("error", o, state or {}, ...), self)
 end
 
 function Object:__tostring()
