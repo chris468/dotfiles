8e9887401193709c43dbbcaaeb9543a609905306
Chris Pawling <chris-468@outlook.com>
2025-06-14 09:12:48 -0500

    naming

--- dotfiles/private_dot_config/nvim/tests/tool.lua
+++ dotfiles/private_dot_config/nvim/tests/tool.lua
@@ -14,31 +14,31 @@ local tool = require("chris468.plugins.config.tools.tool")
 
 ---@class ToolSpec
 local tool_specs = {
-  FormatterTool = {
+  Formatter = {
     display_type = "formatter",
     supports_custom_name = true,
     supports_custom_filetypes = true,
     ---@type fun(...) : Tool
     factory = function(...)
-      return tool.FormatterTool:new(...)
+      return tool.Formatter:new(...)
     end,
   },
-  LinterTool = {
+  Linter = {
     display_type = "linter",
     supports_custom_name = true,
     supports_custom_filetypes = true,
     ---@type fun(...) : Tool
     factory = function(...)
-      return tool.LinterTool:new(...)
+      return tool.Linter:new(...)
     end,
   },
-  LspTool = {
+  Lsp = {
     display_type = "LSP",
     supports_custom_name = false,
     supports_custom_filetypes = false,
     ---@type fun(...) : Tool
     factory = function(...)
-      return tool.LspTool:new(...)
+      return tool.Lsp:new(...)
     end,
   },
 }
@@ -143,7 +143,7 @@ describe("tool", function()
     end)
   end)
 
-  describe("LspTool", function()
+  describe("Lsp", function()
     local config_filetypes = { "ft1", "ft2" }
     local lsp_filetypes = { "ft3", "ft4" }
     local lsp_config = { filetypes = config_filetypes }
@@ -152,7 +152,7 @@ describe("tool", function()
     describe("name", function()
       it("should be lspconfig name when present", function()
         registry._packages.lsp = { spec = { name = "lsp", neovim = { lspconfig = "test tool" } } }
-        local t = tool_specs.LspTool.factory("lsp", { lspconfig = {} })
+        local t = tool_specs.Lsp.factory("lsp", { lspconfig = {} })
         assert.is.truthy(t:package())
         assert.are.equal("test tool", t:name())
       end)
@@ -160,24 +160,24 @@ describe("tool", function()
 
     describe("lspconfig", function()
       it("should return lspconfig", function()
-        local t = tool.LspTool:new("test tool", { lspconfig = lsp_config })
+        local t = tool.Lsp:new("test tool", { lspconfig = lsp_config })
         assert.are.equal(lsp_config, t.lspconfig)
       end)
 
       it("should return empty when unset", function()
-        local t = tool.LspTool:new("test tool", {})
+        local t = tool.Lsp:new("test tool", {})
         assert.are.same({}, t.lspconfig)
       end)
     end)
 
     describe("filetypes", function()
       it("should take filetypes from tool", function()
-        local t = tool.LspTool:new("test tool", { lspconfig = lsp_config })
+        local t = tool.Lsp:new("test tool", { lspconfig = lsp_config })
         assert.are.equal(config_filetypes, t:filetypes())
       end)
 
       it("should take filetypes from lsp", function()
-        local t = tool.LspTool:new("test tool", { lspconfig = {} })
+        local t = tool.Lsp:new("test tool", { lspconfig = {} })
         assert.are.equal(lsp_filetypes, t:filetypes())
       end)
     end)
