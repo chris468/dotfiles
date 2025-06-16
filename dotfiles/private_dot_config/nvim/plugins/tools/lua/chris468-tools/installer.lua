f35c611030704dd1d930b6c83a5f99547d4bf081
Chris Pawling <chris-468@outlook.com>
2025-06-15 20:23:48 -0500

    add callbacks to tool

--- dotfiles/private_dot_config/nvim/plugins/tools/lua/chris468-tools/installer.lua
+++ dotfiles/private_dot_config/nvim/plugins/tools/lua/chris468-tools/installer.lua
@@ -1,5 +1,9 @@
 local M = {}
 
+local function notify(...)
+  vim.schedule_wrap(vim.notify)(...)
+end
+
 ---@generic TConfig
 ---@generic TTool : chris468.tools.Tool
 ---@param opts  { [string]: { [string]: TConfig } }
@@ -26,6 +30,40 @@ function M.map_tools_by_filetype(opts, Tool)
   end
 
   return tools_by_ft, names_by_ft
+end
+
+---@module "mason-core.package"
+
+---@param tool chris468.tools.Tool The tool to install.
+---@param bufnr integer buffer that triggered the install
+local function install(tool, bufnr)
+  local package = tool:package()
+  if package and not package:is_installed() then
+    local display = tool:display_name()
+    notify(string.format("Installing %s...", display))
+    package
+      :once("install:success", function()
+        notify(string.format("Successfully installed %s.", display))
+        tool:on_installed(bufnr)
+      end)
+      :once("install:failed", function()
+        notify(string.format("Error installing %s.", display), vim.log.levels.WARN)
+        tool:on_install_failed(bufnr)
+      end)
+    package:install()
+  else
+    tool:on_installed(bufnr)
+  end
+end
+
+---@param tools chris468.tools.Tool[]
+---@param bufnr integer buffer that triggered the install
+function M.install(tools, bufnr)
+  for _, tool in ipairs(tools) do
+    if tool:enabled() then
+      install(tool, bufnr)
+    end
+  end
 end
 
 return M
