15131bdeb2cfa1587e51b1333dd71f9fbbb47420
Chris Pawling <chris-468@outlook.com>
2025-06-18 19:15:38 -0500

    linter and formatter too

--- dotfiles/private_dot_config/nvim/plugins/tools/lua/chris468-tools/linter.lua
+++ dotfiles/private_dot_config/nvim/plugins/tools/lua/chris468-tools/linter.lua
@@ -2,14 +2,12 @@ local Tool = require("chris468-tools.tool")
 local util = require("chris468-tools._util")
 local installer = require("chris468-tools.installer")
 
-local M = {}
-
 ---@class chris468.tools.Linter : chris468.tools.Tool
 ---@field protected super chris468.tools.Tool
 ---@field new fun(self: chris468.tools.Linter, name: string, opts?: chris468.tools.Tool.Options) : chris468.tools.Linter
-M.Linter = Tool:extend() --[[ @as chris468.tools.Linter ]]
-M.Linter.type = "linter"
-function M.Linter:new(name, opts)
+Linter = Tool:extend() --[[ @as chris468.tools.Linter ]]
+Linter.type = "linter"
+function Linter:new(name, opts)
   opts = opts or {}
   return self:_new(name, opts) --[[ @as chris468.tools.Linter ]]
 end
@@ -38,11 +36,11 @@ local function register_lint(linters_by_ft)
   })
 end
 
-function M.setup(opts)
-  local tools_by_ft, names_by_ft = installer.map_tools_by_ft(opts.linters, M.Linter, opts.disabled_filetypes)
+function Linter.setup(opts)
+  local tools_by_ft, names_by_ft = installer.map_tools_by_ft(opts.linters, Linter, opts.disabled_filetypes)
   require("lint").linters_by_ft = names_by_ft
   register_lint(tools_by_ft)
   installer.install_on_filetype(tools_by_ft, vim.api.nvim_create_augroup("chris468-tools.formatter", { clear = true }))
 end
 
-return M
+return Linter
