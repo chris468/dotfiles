127385b29f4c53464c97fd895954b9cfc15e9da9
Chris Pawling <chris-468@outlook.com>
2025-06-21 17:25:02 -0500

    switch the rest to the mason registry

--- dotfiles/private_dot_config/nvim/plugins/tools/tests/formatter_spec.lua
+++ dotfiles/private_dot_config/nvim/plugins/tools/tests/formatter_spec.lua
@@ -1,18 +1,6 @@
-local FakeRegistry = require("tests.utils.fake_registry")
-
 local Formatter = require("chris468-tools.formatter")
 
 describe("Formatter", function()
-  local registry = FakeRegistry()
-
-  before_each(function()
-    registry:register()
-  end)
-
-  after_each(function()
-    registry:unregister()
-  end)
-
   it("should have type formatter", function()
     assert.are.equal("formatter", Formatter.type)
   end)
