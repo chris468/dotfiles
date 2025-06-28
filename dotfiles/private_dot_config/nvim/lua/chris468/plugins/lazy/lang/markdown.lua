cd8eebda500d044511a695fb453f0bf53a9ee67b
Chris Pawling <chris-468@outlook.com>
2025-06-28 12:28:13 -0500

    linters

--- dotfiles/private_dot_config/nvim/lua/chris468/plugins/lazy/lang/markdown.lua
+++ dotfiles/private_dot_config/nvim/lua/chris468/plugins/lazy/lang/markdown.lua
@@ -8,15 +8,9 @@ return {
           prettier = { filetypes = { "markdown", "markdown.mdx" } },
         },
       },
-    },
-  },
-  {
-    "nvim-lint",
-    opts = {
-      linters_by_ft = {
+      linters = {
         markdown = {
-          markdown = { "markdownlint-cli2" },
-          ["markdown.mdx"] = { "markdownlint-cli2" },
+          ["markdownlint-cli2"] = { filetypes = { "markdown", "markdown.mdx" } },
         },
       },
     },
