397c484517ae3e5ae28a7c640e2b5a6e1e44e3c7
Chris Pawling <chris-468@outlook.com>
2025-07-03 13:21:40 -0500

    better fold and statuscol behavior

--- dotfiles/private_dot_config/nvim/lua/chris468/plugins/lazy/editor.lua
+++ dotfiles/private_dot_config/nvim/lua/chris468/plugins/lazy/editor.lua
@@ -72,4 +72,52 @@ return {
       indent = {},
     },
   },
+  {
+    "luukvbaal/statuscol.nvim",
+    event = "BufWinEnter",
+    opts = function(_, opts)
+      local builtin = require("statuscol.builtin")
+      return vim.tbl_deep_extend("keep", opts or {}, {
+        segments = {
+          { text = { builtin.foldfunc } },
+          {
+            sign = {
+              name = { ".*" },
+              text = { ".*" },
+              colwidth = 1,
+            },
+          },
+          { text = { builtin.lnumfunc } },
+          {
+            sign = {
+              namespace = { "gitsigns" },
+              colwidth = 1,
+            },
+          },
+        },
+      })
+    end,
+  },
+  {
+    "kevinhwang91/nvim-ufo",
+    dependencies = { "kevinhwang91/promise-async" },
+    event = "BufWinEnter",
+    keys = {
+      {
+        "zR",
+        function()
+          require("ufo").openAllFolds()
+        end,
+        desc = "Open all folds",
+      },
+      {
+        "zM",
+        function()
+          require("ufo").closeAllFolds()
+        end,
+        desc = "Close all folds",
+      },
+    },
+    opts = {},
+  },
 }
