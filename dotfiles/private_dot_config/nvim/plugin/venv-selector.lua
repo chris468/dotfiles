dad7f8b2706472c74408caae7947196962a5b7d5
Chris Pawling <chris-468@outlook.com>
2025-09-30 19:33:55 -0500

    nvim: always try and load cached venv
    Retrieve now schedules the venv activiation and it does not finish
    before the ansible lsp tries to call ansible. Load it on VimEnter,
    regardless of filetype, so that it is active by the time the LSP
    attaches. Also on DirChanged to try and keep it up to date.

--- dotfiles/private_dot_config/nvim/plugin/venv-selector.lua
+++ dotfiles/private_dot_config/nvim/plugin/venv-selector.lua
@@ -1,11 +1,8 @@
-if Chris468.venv.additional_filetypes and #Chris468.venv.additional_filetypes then
-  vim.api.nvim_create_autocmd("FileType", {
-    pattern = Chris468.venv.additional_filetypes,
-    callback = function()
-      if require("chris468.util.lazy").has_plugin("venv-selector.nvim") then
-        local cache = require("venv-selector.cached_venv")
-        cache.retrieve()
-      end
-    end,
-  })
-end
+vim.api.nvim_create_autocmd({ "VimEnter", "DirChanged" }, {
+  callback = function()
+    if require("chris468.util.lazy").has_plugin("venv-selector.nvim") then
+      local cache = require("venv-selector.cached_venv")
+      cache.retrieve()
+    end
+  end,
+})
