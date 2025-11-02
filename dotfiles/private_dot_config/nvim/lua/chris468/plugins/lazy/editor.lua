89527d98d415a11f6101007632255fb7c543b775
Chris Pawling <chris-468@outlook.com>
2025-11-02 12:23:27 -0600

    extract helpers

--- dotfiles/private_dot_config/nvim/lua/chris468/plugins/lazy/editor.lua
+++ dotfiles/private_dot_config/nvim/lua/chris468/plugins/lazy/editor.lua
@@ -1,84 +1,4 @@
----@param show_hidden boolean
-local function mini_files_filter(show_hidden)
-  return function(fs_entry)
-    return show_hidden or not vim.startswith(fs_entry.name, ".")
-  end
-end
-
-local function update_keymaps(buf)
-  buf = buf or 0
-  if vim.bo[buf].filetype ~= "minifiles" then
-    return
-  end
-
-  local arrow_navigation = vim.g.chris468_mini_files_arrow_navigation or false
-  local b = vim.b[buf]
-  if b.chris468_mini_files_arrow_navigation == arrow_navigation then
-    return
-  end
-
-  local mini_files_action = {
-    right = function()
-      MiniFiles.go_in({ close_on_file = true })
-    end,
-    shift_right = MiniFiles.go_in,
-    left = MiniFiles.go_out,
-    shift_left = function()
-      for _ = 1, vim.v.count1 do
-        MiniFiles.go_out()
-      end
-      MiniFiles.trim_right()
-    end,
-  }
-
-  local mini_files_navigation = {
-    [false] = {
-      ["h"] = mini_files_action.left,
-      ["H"] = mini_files_action.shift_left,
-      ["l"] = mini_files_action.right,
-      ["L"] = mini_files_action.shift_right,
-    },
-    [true] = {
-      ["<Left>"] = mini_files_action.left,
-      ["<S-Left>"] = mini_files_action.shift_left,
-      ["<Right>"] = mini_files_action.right,
-      ["<S-Right>"] = mini_files_action.shift_right,
-    },
-  }
-
-  b.chris468_mini_files_arrow_navigation = arrow_navigation
-  for key, action in pairs(mini_files_navigation[arrow_navigation]) do
-    vim.keymap.set("n", key, action, { buffer = buf })
-  end
-  for key, _ in pairs(mini_files_navigation[not arrow_navigation]) do
-    vim.keymap.set("n", key, key, { buffer = buf, remap = false })
-  end
-end
-
-local mini_files_split = function(direction)
-  return function()
-    local fs_entry = MiniFiles.get_fs_entry() or {}
-    if fs_entry.fs_type ~= "file" then
-      return
-    end
-
-    local cur_target = MiniFiles.get_explorer_state().target_window
-    local new_target = vim.api.nvim_win_call(cur_target, function()
-      vim.cmd(direction .. " split")
-      return vim.api.nvim_get_current_win()
-    end)
-
-    MiniFiles.set_target_window(new_target)
-    MiniFiles.go_in({ close_on_file = true })
-  end
-end
-
-vim.api.nvim_create_autocmd("BufEnter", {
-  group = vim.api.nvim_create_augroup("chris468_mini_files_toggle_navigation", { clear = true }),
-  callback = function(args)
-    update_keymaps(args.buf)
-  end,
-})
+local mf = require("chris468.plugins.config.minifiles")
 
 return {
   {
@@ -164,37 +84,31 @@ return {
       },
       {
         "g.",
-        function()
-          vim.g.chris468_mini_files_show_hidden = not vim.g.chris468_mini_files_show_hidden
-          MiniFiles.refresh({ content = { filter = mini_files_filter(vim.g.chris468_mini_files_show_hidden) } })
-        end,
+        mf.toggle_show_hidden,
         desc = "Toggle hidden files",
         ft = "minifiles",
       },
       {
         "g<C-N>",
-        function()
-          vim.g.chris468_mini_files_arrow_navigation = not vim.g.chris468_mini_files_arrow_navigation
-          update_keymaps()
-        end,
+        mf.toggle_arrow_navigation,
         desc = "Toggle arrow navigation",
         ft = "minifiles",
       },
       {
         "<C-X>",
-        mini_files_split("horizontal"),
+        mf.mini_files_split("horizontal"),
         desc = "Open in horizontal split",
         ft = "minifiles",
       },
       {
         "<C-V>",
-        mini_files_split("vertical"),
+        mf.mini_files_split("vertical"),
         desc = "Open in vertical split",
         ft = "minifiles",
       },
       {
         "<C-T>",
-        mini_files_split("tab"),
+        mf.mini_files_split("tab"),
         desc = "Open in new tab",
         ft = "minifiles",
       },
@@ -202,7 +116,7 @@ return {
     lazy = false,
     opts = {
       content = {
-        filter = mini_files_filter(vim.g.chris468_mini_files_show_hidden or false),
+        filter = mf.mini_files_filter(),
       },
       options = {
         use_as_default_explorer = true,
