2923466bac1531abb143b29e2bcd611671bb5ffe
Chris Pawling <chris-468@outlook.com>
2025-11-05 18:12:03 -0600

    get at the toggle term

--- dotfiles/private_dot_config/nvim/lua/chris468/util/terminal/toggleterm.lua
+++ dotfiles/private_dot_config/nvim/lua/chris468/util/terminal/toggleterm.lua
@@ -1,10 +1,11 @@
-local toggleterm = require("toggleterm")
+local ToggleTerm = require("toggleterm.terminal")
 local Terminal = require("chris468.util.terminal._terminal"):extend()
 
 function Terminal.toggle(opts)
   opts = opts or { count = 1 }
   opts.name = opts.name or ("Terminal %s"):format(opts.count)
-  toggleterm.toggle(opts.count, nil, nil, nil, opts.name)
+  local t = ToggleTerm.get_or_create_term(opts.count, nil, nil, opts.name)
+  t:toggle()
 end
 
 return Terminal
