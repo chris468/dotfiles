aabe0c87b85f1e9e741acc34c912fdee4a84f3ba
Chris Pawling <chris-468@outlook.com>
2025-11-22 13:44:44 -0600

    chezmoi

--- dotfiles/private_dot_config/lazyvim/lua/config/keymaps.lua
+++ dotfiles/private_dot_config/lazyvim/lua/config/keymaps.lua
@@ -4,3 +4,30 @@ vim.keymap.del({ "n", "i", "v" }, "<A-k>")
 
 -- I never increment on purpose
 vim.keymap.set("n", "<C-a>", "<nop>")
+
+vim.keymap.set("n", "<leader>za", function()
+  require("util.chezmoi").apply("all")
+end, { desc = "Apply" })
+
+vim.keymap.set("n", "<leader>zd", function()
+  require("util.chezmoi").apply("current_dir")
+end, { desc = "Apply current source dir" })
+
+vim.keymap.set("n", "<leader>zf", function()
+  require("util.chezmoi").apply("current_file")
+end, { desc = "Apply current source file" })
+
+vim.keymap.set("n", "<leader>zu", function()
+  require("util.chezmoi").update(false)
+end, { desc = "Update" })
+
+vim.keymap.set("n", "<leader>zU", function()
+  require("util.chezmoi").update(true)
+end, { desc = "Update and apply" })
+
+---@module 'lazyvim'
+LazyVim.on_load("which-key.nvim", function()
+  require("which-key").add({
+    { "<leader>z", group = "Chezmoi" },
+  })
+end)
---
+++ dotfiles/private_dot_config/lazyvim/lua/util/chezmoi.lua
@@ -0,0 +1,62 @@
+local M = {}
+
+---@param source_path? string Source path to apply, or nil to apply all
+---@return string
+local function chezmoi_apply_command(source_path)
+  local command = "chezmoi apply --no-tty --color=false"
+  if source_path then
+    command = string.format('%s "$(chezmoi target-path "%s")"', command, source_path)
+  end
+  return command
+end
+
+local function notify_if_modified_buffers()
+  local buffers = vim.api.nvim_list_bufs()
+  for _, buffer in ipairs(buffers) do
+    if vim.bo[buffer].modified then
+      vim.notify("Unsaved files will not be applied", vim.log.levels.WARN)
+    end
+  end
+end
+
+---@param what "all"|"current_file"|"current_dir"
+function M.apply(what)
+  local path, display_name
+  if what == "current_file" then
+    local buf_name = vim.api.nvim_buf_get_name(0)
+    if not buf_name or buf_name == "" then
+      vim.notify("Can't chezmoi apply file never saved", vim.log.levels.ERROR)
+      return
+    end
+
+    -- warn if the file was modified
+    if vim.bo.modified then
+      vim.notify("File has not been saved", vim.log.levels.WARN)
+    end
+
+    path = vim.fn.fnamemodify(buf_name, ":p")
+    display_name = vim.fn.fnamemodify(buf_name, ":t")
+  elseif what == "current_dir" then
+    path = vim.fn.fnamemodify(vim.fn.getcwd(), ":p")
+    display_name = #path > 30 and path:sub(-30) or path
+    notify_if_modified_buffers()
+  else
+    notify_if_modified_buffers()
+  end
+
+  ---@module 'snacks'
+  Snacks.terminal(
+    chezmoi_apply_command(path),
+    { win = { title = ("Chezmoi apply%s%s"):format(display_name and " " or "", display_name or "") } }
+  )
+end
+
+---@param apply? boolean
+function M.update(apply)
+  local command = "chezmoi update --no-tty --color=false" .. (apply and " --init --apply" or "")
+  local title = "Chezmoi update" .. (apply and " and apply" or "")
+  print(command)
+  Snacks.terminal(command, { win = { title = { title } } })
+end
+
+return M
