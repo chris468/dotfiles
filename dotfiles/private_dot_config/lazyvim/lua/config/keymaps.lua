691bf1f41a2f0b0fbe2f0f54c2c1b3a5a3e541e5
Chris Pawling <chris-468@outlook.com>
2025-11-22 18:52:47 -0600

    organize mappings

--- dotfiles/private_dot_config/lazyvim/lua/config/keymaps.lua
+++ dotfiles/private_dot_config/lazyvim/lua/config/keymaps.lua
@@ -1,40 +1,46 @@
--- Esc followed by j/k were moving lines instead of navigating
-vim.keymap.del({ "n", "i", "v" }, "<A-j>")
-vim.keymap.del({ "n", "i", "v" }, "<A-k>")
+do -- defaults I don't want
+  -- Esc followed by j/k were moving lines instead of navigating
+  vim.keymap.del({ "n", "i", "v" }, "<A-j>")
+  vim.keymap.del({ "n", "i", "v" }, "<A-k>")
 
--- I never increment on purpose
-vim.keymap.set("n", "<C-a>", "<nop>")
+  -- I never increment on purpose
+  vim.keymap.set("n", "<C-a>", "<nop>")
 
--- Default keymaps I don't, remapm or use for something else
-vim.keymap.del("n", "<leader>l") -- Lazy
+  vim.keymap.del("n", "<leader>l") -- Lazy
+end
 
-vim.keymap.set("n", "<leader>za", function()
-  require("util.chezmoi").apply("all")
-end, { desc = "Apply" })
+do -- chezmoi
+  vim.keymap.set("n", "<leader>za", function()
+    require("util.chezmoi").apply("all")
+  end, { desc = "Apply" })
 
-vim.keymap.set("n", "<leader>zd", function()
-  require("util.chezmoi").apply("current_dir")
-end, { desc = "Apply current source dir" })
+  vim.keymap.set("n", "<leader>zd", function()
+    require("util.chezmoi").apply("current_dir")
+  end, { desc = "Apply current source dir" })
 
-vim.keymap.set("n", "<leader>zf", function()
-  require("util.chezmoi").apply("current_file")
-end, { desc = "Apply current source file" })
+  vim.keymap.set("n", "<leader>zf", function()
+    require("util.chezmoi").apply("current_file")
+  end, { desc = "Apply current source file" })
 
-vim.keymap.set("n", "<leader>zu", function()
-  require("util.chezmoi").update(false)
-end, { desc = "Update" })
+  vim.keymap.set("n", "<leader>zu", function()
+    require("util.chezmoi").update(false)
+  end, { desc = "Update" })
 
-vim.keymap.set("n", "<leader>zU", function()
-  require("util.chezmoi").update(true)
-end, { desc = "Update and apply" })
+  vim.keymap.set("n", "<leader>zU", function()
+    require("util.chezmoi").update(true)
+  end, { desc = "Update and apply" })
+end
 
-vim.keymap.set("n", "<leader>pl", "<cmd>Lazy<CR>", { desc = "Lazy" })
-vim.keymap.set("n", "<leader>pL", function()
-  LazyVim.news.changelog()
-end, { desc = "LazyVim changelog" })
-vim.keymap.set("n", "<leader>px", "<cmd>LazyExtras<CR>", { desc = "LazyVim extras" })
-vim.keymap.set("n", "<leader>pm", "<cmd>Mason<CR>", { desc = "Mason" })
+do -- package management
+  vim.keymap.set("n", "<leader>pl", "<cmd>Lazy<CR>", { desc = "Lazy" })
+  vim.keymap.set("n", "<leader>pL", function()
+    LazyVim.news.changelog()
+  end, { desc = "LazyVim changelog" })
+  vim.keymap.set("n", "<leader>px", "<cmd>LazyExtras<CR>", { desc = "LazyVim extras" })
+  vim.keymap.set("n", "<leader>pm", "<cmd>Mason<CR>", { desc = "Mason" })
+end
 
+-- group names / icons
 ---@module 'lazyvim'
 LazyVim.on_load("which-key.nvim", function()
   require("which-key").add({
