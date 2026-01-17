cc3a3f310f6d5db743eee9588a15bb75ae646a72
Chris Pawling <chris-468@outlook.com>
2026-01-17 13:12:52 -0600

    chezmoi: remove externals for tools, add install script for windows
    Something is wrong with chezmoi reference in winget manifest, otherwise
    working.

---
+++ dotfiles/dot_local/bin/executable_install-essentials.ps1
@@ -0,0 +1,8 @@
+winget import `
+  --import-file "$env:XDG_DATA_HOME/chris468/essential-tools/win/winget.json" `
+  --accept-package-agreements `
+  --accept-source-agreements
+
+Set-PSRepository PSGallery https://www.powershellgallery.com/api/v2 -InstallationPolicy Trusted
+$modules = @(Get-Content "$env:XDG_DATA_HOME/chris468/essential-tools/win/ps-modules.json" | ConvertFrom-Json)
+Install-Module $modules
