94be294114be7d59486d2714b3308dbb86aa709a
Chris Pawling <chris-468@outlook.com>
2026-01-17 10:52:16 -0600

    nvim: failed attempt to get pwsh lsp working

---
+++ dotfiles/private_dot_config/nvim/lua/plugins/lang/powershell.lua
@@ -0,0 +1,18 @@
+return {
+  -- {
+  --   "TheLeoP/powershell.nvim",
+  --   ft = "ps1",
+  --   opts = {
+  --     bundle_path = LazyVim.get_pkg_path("powershell-editor-services", nil, { warn = false }),
+  --     lsp_log_level = "Debug",
+  --   },
+  -- },
+  -- {
+  --   "nvim-lspconfig",
+  --   opts = {
+  --     servers = {
+  --       powershell_es = {},
+  --     },
+  --   },
+  -- },
+}
