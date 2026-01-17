8f491f0d9da029bb2b432888e95a85dd176f51e7
Chris Pawling <chris-468@outlook.com>
2026-01-17 17:35:21 -0600

    tools: rename in preparation for cli args

--- dotfiles/dot_local/bin/executable_install-tools.ps1
+++ dotfiles/dot_local/bin/executable_install-tools.ps1
@@ -1,8 +1,8 @@
 winget import `
-  --import-file "$env:XDG_DATA_HOME/chris468/essential-tools/win/winget.json" `
+  --import-file "$env:XDG_DATA_HOME/chris468/tools/essential/win/winget.json" `
   --accept-package-agreements `
   --accept-source-agreements
 
 Set-PSRepository PSGallery https://www.powershellgallery.com/api/v2 -InstallationPolicy Trusted
-$modules = @(Get-Content "$env:XDG_DATA_HOME/chris468/essential-tools/win/ps-modules.json" | ConvertFrom-Json)
+$modules = @(Get-Content "$env:XDG_DATA_HOME/chris468/tools/essential/win/ps-modules.json" | ConvertFrom-Json)
 Install-Module $modules
