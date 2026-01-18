4b71947495be048451f8b7307fefea6a4be6dd83
Chris Pawling <chris-468@outlook.com>
2026-01-17 18:31:37 -0600

    tools: add additional tools and categories

--- dotfiles/dot_local/bin/executable_install-tools.ps1
+++ dotfiles/dot_local/bin/executable_install-tools.ps1
@@ -1,8 +1,42 @@
-winget import `
-  --import-file "$env:XDG_DATA_HOME/chris468/tools/essential/win/winget.json" `
-  --accept-package-agreements `
-  --accept-source-agreements
+[CmdletBinding()]
+param(
+  [switch]$All,
+  [Switch]$Aws,
+  [Switch]$Azure,
+  [Switch]$Container,
+  [switch]$Essential,
+  [Switch]$Kubernetes
+)
+$AllCategories = @(
+  "Essential",
+  "Container",
+  "Kubernetes",
+  "Aws",
+  "Azure"
+)
 
-Set-PSRepository PSGallery https://www.powershellgallery.com/api/v2 -InstallationPolicy Trusted
-$modules = @(Get-Content "$env:XDG_DATA_HOME/chris468/tools/essential/win/ps-modules.json" | ConvertFrom-Json)
-Install-Module $modules
+$categories = @(foreach ($category in $AllCategories) {
+  if ($All -or (Get-Variable -Scope Script -Name $category).Value.IsPresent) {
+    $category.ToLower()
+  }
+})
+$categories = if ($categories) { $categories } else { @("Essential") }
+
+foreach ($category in $categories) {
+  $WingetPackages = "$env:XDG_DATA_HOME/chris468/tools/$category/win/winget.json"
+  if (Test-Path $WingetPackages) {
+    Write-Host "`nInstalling $category tool winget packages..." -ForegroundColor Green
+    winget import `
+      --import-file $WingetPackages `
+      --accept-package-agreements `
+      --accept-source-agreements
+  }
+
+  $PowerShellModules = "$env:XDG_DATA_HOME/chris468/tools/$category/win/ps-modules.json"
+  if (Test-Path $PowerShellModules) {
+    Write-Host "`nInstalling $category tool powershell modules..." -ForegroundColor Green
+    Set-PSRepository PSGallery https://www.powershellgallery.com/api/v2 -InstallationPolicy Trusted
+    $modules = @(Get-Content $PowerShellModules | ConvertFrom-Json)
+    Install-Module $modules
+  }
+}
