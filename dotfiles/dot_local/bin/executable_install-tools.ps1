0147140e96cdd5a90124484a8f86f09608d6de91
Chris Pawling <chris-468@outlook.com>
2026-01-18 10:39:25 -0600

    tools/win: install nerd fonts

--- dotfiles/dot_local/bin/executable_install-tools.ps1
+++ dotfiles/dot_local/bin/executable_install-tools.ps1
@@ -1,6 +1,9 @@
 [CmdletBinding()]
 param(
   [switch]$All,
+  [Switch]$NoFonts,
+
+  # Categories. Each must have a folder in .local/shared/chris468/tools
   [Switch]$Aws,
   [Switch]$Azure,
   [Switch]$Container,
@@ -15,6 +18,8 @@ $AllCategories = @(
   "Azure"
 )
 
+$ToolsRoot = "$env:XDG_DATA_HOME/chris468/tools"
+
 $categories = @(foreach ($category in $AllCategories) {
   if ($All -or (Get-Variable -Scope Script -Name $category).Value.IsPresent) {
     $category.ToLower()
@@ -23,7 +28,10 @@ $categories = @(foreach ($category in $AllCategories) {
 $categories = if ($categories) { $categories } else { @("Essential") }
 
 foreach ($category in $categories) {
-  $WingetPackages = "$env:XDG_DATA_HOME/chris468/tools/$category/win/winget.json"
+  $CategoryRoot = "$ToolsRoot/$category"
+  $CategoryOSRoot = "$CategoryRoot/win"
+
+  $WingetPackages = "$CategoryOSRoot/winget.json"
   if (Test-Path $WingetPackages) {
     Write-Host "`nInstalling $category tool winget packages..." -ForegroundColor Green
     winget import `
@@ -32,11 +40,19 @@ foreach ($category in $categories) {
       --accept-source-agreements
   }
 
-  $PowerShellModules = "$env:XDG_DATA_HOME/chris468/tools/$category/win/ps-modules.json"
+  $PowerShellModules = "$CategoryOSRoot/ps-modules.json"
   if (Test-Path $PowerShellModules) {
     Write-Host "`nInstalling $category tool powershell modules..." -ForegroundColor Green
     Set-PSRepository PSGallery https://www.powershellgallery.com/api/v2 -InstallationPolicy Trusted
     $modules = @(Get-Content $PowerShellModules | ConvertFrom-Json)
     Install-Module $modules
+  }
+
+  $NerdFonts = "$CategoryRoot/nerdfonts.txt"
+  if (!$NoFonts -and (Test-Path $NerdFonts)) {
+    Write-Host "`nInstalling $category nerdfonts..." -ForegroundColor Green
+    foreach ($font in (Get-Content $NerdFonts)) {
+      oh-my-posh font install "$font"
+    }
   }
 }
