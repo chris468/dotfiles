238e0998f32eb82ef11f2cb76d8a32aabb7cd0c5
Chris Pawling <chris-468@outlook.com>
2026-01-19 16:43:49 -0600

    zsh: move almost everything from .local/opt to .local/chris468/share/externals

--- dotfiles/private_dot_config/zsh/completion-menu.zsh
+++ dotfiles/private_dot_config/zsh/completion-menu.zsh
@@ -3,7 +3,10 @@ zstyle ':completion:*' completer _expand _complete _correct _approximate
 zstyle ':completion:*:*:*:*:corrections' format '%B%F{yellow}%d%f%b'
 zstyle ':completion:*:*:*:*:descriptions' format '%B%F{blue}%d%f%b'
 zstyle ':completion:*' group-name ''
-zstyle ':completion:*' menu select=2
+zstyle ':completion:*:*:*:*:*' list-style menu
+zstyle ':completion:*' list-style menu
+zstyle ':completion:*:*:*:*:*' menu yes select
+zstyle ':completion:*' menu yes select
 
 eval $(/usr/bin/env bash ~/.config/dircolors/dircolors.sh)
 
@@ -11,10 +14,12 @@ zstyle ':completion:*:default' list-colors ${(s.:.)LS_COLORS}
 zstyle ':completion:*' list-colors ''
 zstyle ':completion:*' list-prompt %SAt %p: Hit TAB for more, or the character to insert%s
 zstyle ':completion:*' matcher-list '' 'm:{a-z}={A-Z}' 'm:{a-zA-Z}={A-Za-z}' 'r:|[._-]=* r:|=* l:|=*'
-zstyle ':completion:*' menu select=long
 zstyle ':completion:*' select-prompt %SScrolling active: current selection at %p%s
 zstyle ':completion:*' use-compctl false
 zstyle ':completion:*' verbose true
 
 zstyle ':completion:*:*:kill:*:processes' list-colors '=(#b) #([0-9]#)*=0=01;31'
 zstyle ':completion:*:kill:*' command 'ps -u $USER -o pid,%cpu,tty,cputime,cmd'
+
+bindkey -M menuselect '\e' send-break
+bindkey -M isearch '\e' send-break
