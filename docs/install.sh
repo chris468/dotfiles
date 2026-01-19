bee2f7fecec57a168bc6499cdff97f8075825048
Chris Pawling <chris-468@outlook.com>
2026-01-19 08:17:25 -0600

    install: download chezmoi to /tmp, let it be installed by install-tools --essential

--- docs/install.sh
+++ docs/install.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/bash
 
 if ! command -v git >/dev/null 2>&1; then
-	echo "error: could not find git in PATH" >&2
-	exit 1
+  echo "error: could not find git in PATH" >&2
+  exit 1
 fi
 
 sh -c "$(curl -fsLS get.chezmoi.io)" -- \
-	-b $HOME/.local/bin \
-	init --apply chris468 \
-	"$@"
+  -b /tmp \
+  init --apply chris468 \
+  "$@"
